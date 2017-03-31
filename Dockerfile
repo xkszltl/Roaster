a23cff5de2846cf18aa74615466c8c768997ca64
Tongliang Liao <xkszltl@gmail.com>
2017-03-31 01:45:14 -0500

    Retry

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM centos
 ENV container docker
 SHELL ["/bin/bash", "-c"]
-ADD ["setup.sh"]
-RUN rm -f /etc/systemd/system/*.wants/* /lib/systemd/system/{{multi-user,local-fs,basic,anaconda}.target.wants/*,sockets.target.wants/*{udev,initctl}*,sysinit.target.wants/systemd-tmpfiles-setup.service}
-RUN setup.sh
+ADD ["setup.sh", "/tmp/"]
+RUN chmod +x /tmp/setup.sh && /tmp/setup.sh && rm -f /tmp/setup.sh
 RUN rm -f /etc/systemd/system/*.wants/* /lib/systemd/system/{{multi-user,local-fs,basic,anaconda}.target.wants/*,sockets.target.wants/*{udev,initctl}*,sysinit.target.wants/systemd-tmpfiles-setup.service}
 VOLUME ["/sys/fs/cgroup"]
 CMD ["/usr/sbin/init"]
--- setup.sh
+++ setup.sh
@@ -1,89 +1,114 @@
 #!/bin/bash
 
+set -e
+
 # ================================================================
 # YUM Configuration
 # ================================================================
 
 yum-config-manager --setopt=tsflags= --save
-yum update -y --skip-broken
-yum update -y
-yum install -y curl
+until yum update -y --skip-broken; do echo 'Retrying'; done
+yum update -y || true
+until yum install -y curl; do echo 'Retrying'; done
 rpm -i "http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/`curl -s http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/ | sed -n 's/.*\(cuda-repo-rhel7-.*\.x86_64\.rpm\).*/\1/p' | sort | tail -n 1`"
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.rpm.sh | bash
-yum install -y epel-release
-# yum install -y centos-release-scl{,-rh}
-yum update -y --skip-broken
-yum update -y
+until yum install -y epel-release; do echo 'Retrying'; done
+# until yum install -y centos-release-scl{,-rh}; do echo 'Retrying'; done
+until yum update -y --skip-broken; do echo 'Retrying'; done
+yum update -y || true
 yum-config-manager --enable extras centosplus {base,epel}-debuginfo
 # yum-config-manager --enable centos-sclo-{sclo,rh}-debuginfo
-yum update -y --skip-broken
-yum update -y
+until yum update -y --skip-broken; do echo 'Retrying'; done
+yum update -y || true
 
-yum install -y yum-{axelget,plugin-priorities}
+until yum install -y yum-{axelget,plugin-priorities}; do echo 'Retrying'; done
 
 # ================================================================
 # Install Packages
 # ================================================================
 
-yum remove -y compat-qpid-cpp-client{,-*}
-yum install -y qpid-cpp-client{,-*}
+until yum remove -y                                     \
+                                                        \
+compat-qpid-cpp-client{,-*}                             \
 
-yum install -y {gcc,distcc,ccache}{,-*}
-yum install -y --skip-broken perl{,-*}
-yum install -y --skip-broken {python{,34},anaconda}{,-*}
-yum install -y --skip-broken ruby{,-*}
-yum install -y java-1.8.0-openjdk{,-*}
-yum install -y texlive{,-*}
-yum install -y {gdb,valgrind,perf,{l,s}trace}{,-*}
-yum install -y {make,cmake{,3},autoconf,libtool,ant,maven}{,-*}
-yum install -y {git,subversion,mercurial}{,-*}
-yum install -y doxygen{,-*}
+do echo 'Retrying'; done
 
-yum install -y vim{,-*}
-yum install -y dos2unix{,-*}
+# ----------------------------------------------------------------
 
-yum install -y {bash,fish,zsh,mosh,tmux}{,-*}
-yum install -y {telnet,tftp,rsh}{,-debuginfo}
-yum install -y {htop,glances}{,-*}
-yum install -y {wget,axel,curl}{,-*}
-yum install -y man{,-*}
-yum install -y {f,tc,dhc,libo,io}ping{,-*}
-yum install -y hping3{,-*}
-yum install -y {traceroute,rsync,tcpdump}{,-*}
-yum install -y net-snmp{,-*}
-yum install -y GeoIP{,-*}
-yum install -y dstat{,-*}
-yum install -y lm_sensors{,-*}
-yum install -y {{e2fs,btrfs-,xfs,ntfs}progs,xfsdump,nfs-utils}{,-*}
-yum install -y dd{,_}rescue{,-*}
-yum install -y docker{,-*}
+until yum install -y                                    \
+                                                        \
+qpid-cpp-client{,-*}                                    \
+{gcc,distcc,ccache}{,-*}                                \
+java-1.8.0-openjdk{,-*}                                 \
+texlive{,-*}                                            \
+{gdb,valgrind,perf,{l,s}trace}{,-*}                     \
+{make,cmake{,3},autoconf,libtool,ant,maven}{,-*}        \
+{git,subversion,mercurial}{,-*}                         \
+doxygen{,-*}                                            \
+swig{,-*}                                               \
+                                                        \
+vim{,-*}                                                \
+dos2unix{,-*}                                           \
+                                                        \
+{bash,fish,zsh,mosh,tmux}{,-*}                          \
+{telnet,tftp,rsh}{,-debuginfo}                          \
+{htop,glances}{,-*}                                     \
+{wget,axel,curl,net-tools}{,-*}                         \
+man{,-*}                                                \
+{f,tc,dhc,libo,io}ping{,-*}                             \
+hping3{,-*}                                             \
+{traceroute,rsync,tcpdump}{,-*}                         \
+{more,elf,ib}utils{,-*}                                 \
+cyrus-imapd{,-*}                                        \
+net-snmp{,-*}                                           \
+GeoIP{,-*}                                              \
+dstat{,-*}                                              \
+lm_sensors{,-*}                                         \
+{{e2fs,btrfs-,xfs,ntfs}progs,xfsdump,nfs-utils}{,-*}    \
+dd{,_}rescue{,-*}                                       \
+docker{,-*}                                             \
+                                                        \
+ncurses{,-*}                                            \
+hwloc{,-*}                                              \
+icu{,-*}                                                \
+{gmp,mpfr,libmpc}{,-*}                                  \
+lib{jpeg-turbo,tiff,png}{,-*}                           \
+{zlib,libzip,{,p}xz,snappy}{,-*}                        \
+lib{telnet,ssh{,2},curl,aio,ffi,edit,icu}{,-*}          \
+boost{,-*}                                              \
+{flex,cups,bison,antlr}{,-*}                            \
+open{blas,cv,ssl,ssh,ldap}{,-*}                         \
+{gflags,glog,protobuf}{,-*}                             \
+ImageMagick{,-*}                                        \
+cuda                                                    \
+                                                        \
+{hdf5}{,-*}                                             \
+{leveldb,lmdb}{,-*}                                     \
+{mariadb,postgresql}{,-*}                               \
+                                                        \
+{fio,filebench}{,-*}                                    \
+                                                        \
+{sudo,nss,sssd}{,-*}                                    \
+                                                        \
+gitlab-ci-multi-runner                                  \
 
-yum install -y ncurses{,-*}
-yum install -y hwloc{,-*}
-yum install -y icu{,-*}
-yum install -y {gmp,mpfr,libmpc}{,-*}
-yum install -y lib{jpeg-turbo,tiff,png}{,-*}
-yum install -y {zlib,libzip,{,p}xz,snappy}{,-*}
-yum install -y lib{telnet,ssh{,2},curl,aio,ffi,edit,icu}{,-*}
-yum install -y boost{,-*}
-yum install -y open{blas,cv,ssl,ssh,ldap}{,-*}
-yum install -y {gflags,glog,protobuf}{,-*}
-yum install -y ImageMagick{,-*}
-yum install -y --skip-broken qt5{,-*}
-yum install -y cuda
+do echo 'Retrying'; done
 
-yum install -y {hdf5}{,-*}
-yum install -y {leveldb,lmdb}{,-*}
-yum install -y {mariadb,postgresql}{,-*}
+# ----------------------------------------------------------------
 
-yum install -y {fio,filebench}{,-*}
+until yum install -y --skip-broken                      \
+                                                        \
+perl{,-*}                                               \
+{python{,34},anaconda}{,-*}                             \
+ruby{,-*}                                               \
+qt5{,-*}                                                \
 
-yum install -y {nss,sssd}{,-*}
+do echo 'Retrying'; done
 
-yum install -y gitlab-ci-multi-runner
+# ----------------------------------------------------------------
 
-yum update -y --skip-broken
-yum update -y
+until yum update -y --skip-broken; do echo 'Retrying'; done
+yum update -y || true
 
 # ================================================================
 # Account Configuration
@@ -96,6 +121,8 @@ rm -rf id_{ecdsa,rsa}{,.pub}
 ssh-keygen -N '' -f id_ecdsa -qt ecdsa -b 521
 ssh-keygen -N '' -f id_rsa -qt rsa -b 8192
 cd
+
+# ----------------------------------------------------------------
 
 cd /etc/openldap
 for i in 'BASE' 'URI' 'TLS_CACERT' 'TLS_REQCERT'; do
@@ -115,6 +142,21 @@ mv -f .ldap.conf ldap.conf
 cd
 
 authconfig --enablesssd --enablesssdauth --enablecachecreds --enableldap --enableldapauth --enablemkhomedir --ldapserver=ldap://ldap.codingcafe.org --ldapbasedn=dc=codingcafe,dc=org --enablelocauthorize --enableldaptls --update
+
+# ================================================================
+# Enable Services
+# ================================================================
+
+systemctl enable sssd
+
+# ================================================================
+# Personalize
+# ================================================================
+
+git config --global user.name 'Tongliang Liao'
+git config --global user.email 'xkszltl@gmail.com'
+git config --global push.default matching
+git config --global core.editor 'vim'
 
 # ================================================================
 # Compile LLVM
@@ -163,13 +205,25 @@ cd ../..
 wait
 mkdir -p build/Release
 cd build/Release
+
+# ----------------------------------------------------------------
+
 cmake3 -DCMAKE_BUILD_TYPE=Release -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DLLVM_CCACHE_BUILD=ON -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLLVM_ENABLE_LIBCXX=ON -DLIBCLANG_BUILD_STATIC=ON -DLIBCXX_CONFIGURE_IDE=ON -DLIBOMP_OMPT_SUPPORT=ON -DLIBOMP_STATS=OFF -DLIBOMP_TSAN_SUPPORT=ON -DLIBOMP_USE_HWLOC=ON -DLIBOMP_USE_STDCPPLIB=ON -DLIBUNWIND_ENABLE_CROSS_UNWINDING=ON -DLLDB_DISABLE_PYTHON=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_ENABLE_CXX1Y=ON -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_FFI=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_LINK_LLVM_DYLIB=OFF -DLLVM_OPTIMIZED_TABLEGEN=ON -DPOLLY_ENABLE_GPGPU_CODEGEN=ON ../..
 time VERBOSE=1 make -j`nproc --all` install
 rm -rf *
+
+# ----------------------------------------------------------------
+
 echo /usr/local/lib > /etc/ld.so.conf.d/libc++-x86_64.conf
 ldconfig
+
+# ----------------------------------------------------------------
+
 CC='clang -fPIC -fuse-ld=lld' CXX='clang++ -stdlib=libc++ -lc++abi -fPIC -fuse-ld=lld' LD='lld' cmake3 -DCMAKE_BUILD_TYPE=Release -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DENABLE_X86_RELAX_RELOCATIONS=ON -DLLVM_CCACHE_BUILD=ON -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLLVM_ENABLE_LIBCXX=ON -DLIBCLANG_BUILD_STATIC=ON -DLIBCXX_CONFIGURE_IDE=ON -DLIBOMP_OMPT_SUPPORT=ON -DLIBOMP_STATS=OFF -DLIBOMP_TSAN_SUPPORT=ON -DLIBOMP_USE_HWLOC=ON -DLIBOMP_USE_STDCPPLIB=ON -DLIBUNWIND_ENABLE_CROSS_UNWINDING=ON -DLLDB_DISABLE_PYTHON=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_ENABLE_CXX1Y=ON -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_FFI=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_LINK_LLVM_DYLIB=OFF -DLLVM_OPTIMIZED_TABLEGEN=ON -DPOLLY_ENABLE_GPGPU_CODEGEN=ON ../..
 time VERBOSE=1 make -j`nproc --all` install
+
+# ----------------------------------------------------------------
+
 ldconfig
 cd /tmp
 rm -rf LLVM
@@ -186,6 +240,10 @@ cd boost*/
 ./bootstrap.sh --with-icu
 # LD=lld ./b2 cxxflags="-std=c++1z -stdlib=libc++ -fuse-ld=lld" linkflags="-stdlib=libc++" -aj`nproc --all` install
 ./b2 -aj`nproc --all` install
+
+# ----------------------------------------------------------------
+
+ldconfig
 cd /tmp
 rm -rf boost*
 
