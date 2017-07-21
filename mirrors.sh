3035f34640440d35fb483899a392d13cee7e218c
Tongliang Liao <xkszltl@gmail.com>
2017-07-21 03:49:48 -0700

    Mirror shadowsocks.

--- mirrors.sh
+++ mirrors.sh
@@ -11,4 +11,4 @@ if [ ! -d $ROOT/{} ]; then mkdir -p $ROOT/$(dirname {}) && cd $ROOT/$(dirname {}
 else cd $ROOT/{} && git fetch --all
 fi &&
 git remote set-url --push origin git@git.codingcafe.org:Mirrors/{} && git push --mirror
-'"'" ::: {llvm-mirror/{ll{vm,d,db,go},clang{,-tools-extra},polly,compiler-rt,openmp,lib{unwind,cxx{,abi}},test-suite},jemalloc/jemalloc,aws/aws-{cli,sdk-{cpp,go,java,js,net,php,ruby}}}.git
+'"'" ::: {llvm-mirror/{ll{vm,d,db,go},clang{,-tools-extra},polly,compiler-rt,openmp,lib{unwind,cxx{,abi}},test-suite},jemalloc/jemalloc,aws/aws-{cli,sdk-{cpp,go,java,js,net,php,ruby}},shadowsocks/shadowsocks}.git
