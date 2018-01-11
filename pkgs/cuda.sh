5df1c1d3f42b5a205e0043781bc79f43b9180771
Tongliang Liao <xkszltl@gmail.com>
2018-01-12 06:21:45 +0800

    Skip old files in NCCL untar.

--- pkgs/cuda.sh
+++ pkgs/cuda.sh
@@ -17,7 +17,7 @@
 
         curl -sSL https://developer.download.nvidia.com/compute/redist/$CUDNN_REPO/cudnn-9.1-linux-x64-v7.tgz
     fi | tar -zxvf - -C /usr/local/ --no-overwrite-dir
-    curl -sSL https://repo.codingcafe.org/nvidia/nccl/$(curl -sSL https://repo.codingcafe.org/nvidia/nccl | sed -n 's/.*href="\(.*x86_64.*\)".*/\1/p' | sort | tail -n1) | tar -Jxvf - --strip-components=1 -C /usr/local/cuda --no-overwrite-dir
+    curl -sSL https://repo.codingcafe.org/nvidia/nccl/$(curl -sSL https://repo.codingcafe.org/nvidia/nccl | sed -n 's/.*href="\(.*x86_64.*\)".*/\1/p' | sort | tail -n1) | tar -Jxvf - --strip-components=1 -C /usr/local/cuda --no-overwrite-dir --skip-old-files
     ldconfig
 
     cd $(dirname $(which nvcc))/../samples
