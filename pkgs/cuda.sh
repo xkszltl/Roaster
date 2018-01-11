8adaff82834195d9f20cf07c21fe886647a17fb2
Tongliang Liao <xkszltl@gmail.com>
2018-01-12 06:06:31 +0800

    Create script for extra cuda packages.

---
+++ pkgs/cuda.sh
@@ -0,0 +1,27 @@
+# ================================================================
+# Install Extra CUDA Packages
+# ================================================================
+
+[ -e $STAGE/cuda ] && ( set -e
+    cd $SCRATCH
+
+    export CUDNN_REPO=cudnn/v7.0.5
+
+    if [ $GIT_MIRROR == $GIT_MIRROR_CODINGCAFE ]; then
+        curl -sSL https://repo.codingcafe.org/nvidia/$CUDNN_REPO/$(curl -sSL https://repo.codingcafe.org/nvidia/cudnn | sed -n 's/.*href="\(.*linux-x64.*\)".*/\1/p' | sort | tail -n1)
+    else
+        export HTTP_PROXY=proxy.codingcafe.org:8118
+        [ $HTTP_PROXY ] && export HTTPS_PROXY=$HTTP_PROXY
+        [ $HTTP_PROXY ] && export http_proxy=$HTTP_PROXY
+        [ $HTTPS_PROXY ] && export https_proxy=$HTTPS_PROXY
+
+        curl -sSL https://developer.download.nvidia.com/compute/redist/$CUDNN_REPO/cudnn-9.1-linux-x64-v7.tgz
+    fi | tar -zxvf - -C /usr/local/ --no-overwrite-dir
+    curl -sSL https://repo.codingcafe.org/nvidia/nccl/$(curl -sSL https://repo.codingcafe.org/nvidia/nccl | sed -n 's/.*href="\(.*x86_64.*\)".*/\1/p' | sort | tail -n1) | tar -Jxvf - --strip-components=1 -C /usr/local/cuda --no-overwrite-dir
+    ldconfig
+
+    cd $(dirname $(which nvcc))/../samples
+    . scl_source enable devtoolset-6
+    VERBOSE=1 time make -j$(nproc)
+) && rm -rvf $STAGE/cuda
+sync || true
--- setup.sh
+++ setup.sh
@@ -66,7 +66,7 @@ cd $SCRATCH
     rm -rvf $STAGE
     mkdir -p $(dirname $STAGE)/.$(basename $STAGE)
     cd $_
-    [ $# -gt 0 ] && touch $@ || touch repo pkg auth slurm ompi nagios ss tex cmake llvm boost jemalloc gflags glog protobuf leveldb opencv rocksdb caffe caffe2
+    [ $# -gt 0 ] && touch $@ || touch repo pkg auth cuda slurm ompi nagios ss tex cmake llvm boost jemalloc gflags glog protobuf leveldb opencv rocksdb caffe caffe2
     sync || true
     cd $SCRATCH
     mv -vf $(dirname $STAGE)/.$(basename $STAGE) $STAGE
@@ -291,27 +291,6 @@ echo '----------------------------------------------------------------'
 
     # ------------------------------------------------------------
 
-    ( set -e
-        export CUDNN_REPO=cudnn/v7.0.5
-
-        if [ $GIT_MIRROR == $GIT_MIRROR_CODINGCAFE ]; then
-            export HTTP_PROXY=proxy.codingcafe.org:8118
-            [ $HTTP_PROXY ] && export HTTPS_PROXY=$HTTP_PROXY
-            [ $HTTP_PROXY ] && export http_proxy=$HTTP_PROXY
-            [ $HTTPS_PROXY ] && export https_proxy=$HTTPS_PROXY
-
-            curl -sSL https://repo.codingcafe.org/nvidia/$CUDNN_REPO/$(curl -sSL https://repo.codingcafe.org/nvidia/cudnn | sed -n 's/.*href="\(.*linux-x64.*\)".*/\1/p' | sort | tail -n1)
-        else
-            curl -sSL https://developer.download.nvidia.com/compute/redist/$CUDNN_REPO/cudnn-9.1-linux-x64-v7.tgz
-        fi | tar -zxvf - -C /usr/local/ --no-overwrite-dir
-        curl -sSL https://repo.codingcafe.org/nvidia/nccl/$(curl -sSL https://repo.codingcafe.org/nvidia/nccl | sed -n 's/.*href="\(.*x86_64.*\)".*/\1/p' | sort | tail -n1) | tar -Jxvf - --strip-components=1 -C /usr/local/cuda --no-overwrite-dir
-        ldconfig
-
-        cd $(dirname $(which nvcc))/../samples
-        . scl_source enable devtoolset-6
-        VERBOSE=1 time make -j
-    )
-
     until yum update -y --skip-broken; do echo 'Retrying'; done
     yum update -y || true
 
@@ -389,6 +368,7 @@ git config --global core.editor     'vim'
 sync || true
 
 for i in $(echo "
+    cuda
     slurm
     openmpi
     nagios
