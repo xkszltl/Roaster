34fbfc694d5a3755233903ae1cf3d163bdc9408f
Tongliang Liao <xkszltl@gmail.com>
2018-01-12 05:18:43 +0800

    Bugfix for CMake mirror.

--- pkgs/cmake.sh
+++ pkgs/cmake.sh
@@ -5,20 +5,18 @@
 [ -e $STAGE/cmake ] && ( set -e
     cd $SCRATCH
 
-    [ $GIT_MIRROR = $GIT_MIRROR_CODINGCAFE ] && export CMAKE_MIRROR=$GIT_MIRROR || export CMAKE_MIRROR=https://gitlab.kitware.com
-
-    until git clone $CMAKE_MIRROR/cmake/cmake.git; do echo 'Retrying'; done
-    cd cmake
+    until git clone $GIT_MIRROR/Kitware/CMake.git; do echo 'Retrying'; done
+    cd CMake
     # git checkout $(git tag | sed -n '/^[0-9\.]*$/p' | sort -V | tail -n1)
     git checkout release
 
-    . scl_source enable devtoolset-6
+    . scl_source enable devtoolset-7
 
     ./bootstrap --prefix=/usr --parallel=$(nproc)
     VERBOSE=1 time make -j$(nproc)
     VERBOSE=1 time make -j install
 
     cd
-    rm -rf $SCRATCH/cmake
+    rm -rf $SCRATCH/CMake
 ) && rm -rvf $STAGE/cmake
 sync || true
