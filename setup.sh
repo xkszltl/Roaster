e19ae2e01abcb4ab885ada6a76125691e65f6faa
Tongliang Liao <xkszltl@gmail.com>
2018-01-19 11:30:37 +0800

    Add repo and pkg stages.

--- setup.sh
+++ setup.sh
@@ -150,118 +150,120 @@ echo '----------------------------------------------------------------'
 [ -e $STAGE/pkg ] && ( set -e
     export RPM_CACHE_ARGS=$([ -f $RPM_CACHE_REPO ] && echo "--disableplugin=axelget,fastestmirror")
 
-    until yum install -y --nogpgcheck $RPM_CACHE_ARGS           \
-                                                                \
-    qpid-cpp-client{,-*}                                        \
-    {gcc,distcc,ccache}{,-*}                                    \
-    {openmpi,mpich-3.{0,2}}{,-devel,-doc,-debuginfo}            \
-    java-1.8.0-openjdk{,-*}                                     \
-    lua{,-*}                                                    \
-    octave{,-*}                                                 \
-    {gdb,valgrind,perf,{l,s}trace}{,-*}                         \
-    {make,ninja-build,cmake{,3},autoconf,libtool}{,-*}          \
-    {ant,maven}{,-*}                                            \
-    {git,subversion,mercurial}{,-*}                             \
-    doxygen{,-*}                                                \
-    swig{,-*}                                                   \
-                                                                \
-    vim{,-*}                                                    \
-    dos2unix{,-*}                                               \
-                                                                \
-    {bash,fish,zsh,mosh,tmux}{,-*}                              \
-    {bc,sed,man,pv,which}{,-*}                                  \
-    {parallel,jq}{,-*}                                          \
-    {tree,whereami,mlocate,lsof}{,-*}                           \
-    {telnet,tftp,rsh}{,-debuginfo}                              \
-    {f,h,if,io,latency,power,tip}top{,-*}                       \
-    procps-ng{,-*}                                              \
-    glances{,-*}                                                \
-    {wget,axel,curl,net-tools}{,-*}                             \
-    {f,tc,dhc,libo,io}ping{,-*}                                 \
-    hping3{,-*}                                                 \
-    {traceroute,mtr,rsync,tcpdump,whois,net-snmp}{,-*}          \
-    torsocks{,-*}                                               \
-    {bridge-,core,crypto-,elf,find,ib,ip,yum-}utils{,-*}        \
-    moreutils{,-debuginfo}                                      \
-    cyrus-imapd{,-*}                                            \
-    GeoIP{,-*}                                                  \
-    {device-mapper,lvm2}{,-*}                                   \
-    {d,sys}stat{,-*}                                            \
-    {lm_sensors,hddtemp}{,-*}                                   \
-    {{e2fs,btrfs-,xfs,ntfs}progs,xfsdump,nfs-utils}{,-*}        \
-    fuse{,-devel,-libs}                                         \
-    dd{,_}rescue{,-*}                                           \
-    {docker-ce,container-selinux}{,-*}                          \
-    createrepo{,_c}{,-*}                                        \
-    environment-modules{,-*}                                    \
-    fpm2{,-*}                                                   \
-    munge{,-*}                                                  \
-                                                                \
-    scl-utils{,-*}                                              \
-                                                                \
-    ncurses{,-*}                                                \
-    hwloc{,-*}                                                  \
-    icu{,-*}                                                    \
-    {glibc{,-devel},libgcc}{,.i686}                             \
-    {gmp,mpfr,libmpc}{,-*}                                      \
-    gperftools{,-*}                                             \
-    lib{asan{,3},tsan,ubsan}{,-*}                               \
-    lib{exif,jpeg-turbo,tiff,png,gomp,gphoto2}{,-*}             \
-    OpenEXR{,-*}                                                \
-    {libv4l,v4l-utils}{,-*}                                     \
-    libunicap{,gtk}{,-*}                                        \
-    libglvnd{,-*}                                               \
-    tbb{,-*}                                                    \
-    {bzip2,zlib,libzip,{,lib}zstd,lz4,{,p}xz,snappy}{,-*}       \
-    lib{telnet,ssh{,2},curl,aio,ffi,edit,icu,xslt}{,-*}         \
-    boost{,-*}                                                  \
-    {flex,cups,bison,antlr}{,-*}                                \
-    open{blas,cv,ldap,ni,ssh,ssl}{,-*}                          \
-    {atlas,eigen3}{,-*}                                         \
-    lapack{,64}{,-*}                                            \
-    {libsodium,mbedtls}{,-*}                                    \
-    libev{,-devel,-source,-debuginfo}                           \
-    {asciidoc,gettext,xmlto,c-ares,pcre{,2}}{,-*}               \
-    librados2{,-*}                                              \
-    {gflags,glog,gmock,gtest,protobuf}{,-*}                     \
-    {redis,hiredis}{,-*}                                        \
-    ImageMagick{,-*}                                            \
-    docbook{,5,2X}{,-*}                                         \
-    nagios{,selinux,devel,debuginfo,-plugins-all}               \
-    {nrpe,nsca}                                                 \
-    {collectd,rrdtool,pnp4nagios}{,-*}                          \
-    cuda                                                        \
-    nvidia-docker2                                              \
-                                                                \
-    hdf5{,-*}                                                   \
-    {leveldb,lmdb}{,-*}                                         \
-    {mariadb,postgresql}{,-*}                                   \
-                                                                \
-    {fio,{file,sys}bench}{,-*}                                  \
-                                                                \
-    {,pam_}krb5{,-*}                                            \
-    {sudo,nss,sssd,authconfig}{,-*}                             \
-                                                                \
-    gitlab-ci-multi-runner                                      \
-                                                                \
-    youtube-dl                                                  \
-                                                                \
-    privoxy{,-*}                                                \
-                                                                \
-    wine                                                        \
-                                                                \
-    libselinux{,-*}                                             \
-    policycoreutils{,-*}                                        \
-    se{troubleshoot,tools}{,-*}                                 \
-    selinux-policy{,-*}                                         \
-                                                                \
-    mod_authnz_*                                                \
-                                                                \
-    cabextract{,-*}                                             \
-                                                                \
-    devtoolset-{3,4,6,7}                                        \
+    for i in --skip-broken --nogpgcheck; do
+        until yum install -y --nogpgcheck $RPM_CACHE_ARGS           \
+                                                                    \
+        qpid-cpp-client{,-*}                                        \
+        {gcc,distcc,ccache}{,-*}                                    \
+        {openmpi,mpich-3.{0,2}}{,-devel,-doc,-debuginfo}            \
+        java-1.8.0-openjdk{,-*}                                     \
+        lua{,-*}                                                    \
+        octave{,-*}                                                 \
+        {gdb,valgrind,perf,{l,s}trace}{,-*}                         \
+        {make,ninja-build,cmake{,3},autoconf,libtool}{,-*}          \
+        {ant,maven}{,-*}                                            \
+        {git,subversion,mercurial}{,-*}                             \
+        doxygen{,-*}                                                \
+        swig{,-*}                                                   \
+                                                                    \
+        vim{,-*}                                                    \
+        dos2unix{,-*}                                               \
+                                                                    \
+        {bash,fish,zsh,mosh,tmux}{,-*}                              \
+        {bc,sed,man,pv,which}{,-*}                                  \
+        {parallel,jq}{,-*}                                          \
+        {tree,whereami,mlocate,lsof}{,-*}                           \
+        {telnet,tftp,rsh}{,-debuginfo}                              \
+        {f,h,if,io,latency,power,tip}top{,-*}                       \
+        procps-ng{,-*}                                              \
+        glances{,-*}                                                \
+        {wget,axel,curl,net-tools}{,-*}                             \
+        {f,tc,dhc,libo,io}ping{,-*}                                 \
+        hping3{,-*}                                                 \
+        {traceroute,mtr,rsync,tcpdump,whois,net-snmp}{,-*}          \
+        torsocks{,-*}                                               \
+        {bridge-,core,crypto-,elf,find,ib,ip,yum-}utils{,-*}        \
+        moreutils{,-debuginfo}                                      \
+        cyrus-imapd{,-*}                                            \
+        GeoIP{,-*}                                                  \
+        {device-mapper,lvm2}{,-*}                                   \
+        {d,sys}stat{,-*}                                            \
+        {lm_sensors,hddtemp}{,-*}                                   \
+        {{e2fs,btrfs-,xfs,ntfs}progs,xfsdump,nfs-utils}{,-*}        \
+        fuse{,-devel,-libs}                                         \
+        dd{,_}rescue{,-*}                                           \
+        {docker-ce,container-selinux}{,-*}                          \
+        createrepo{,_c}{,-*}                                        \
+        environment-modules{,-*}                                    \
+        fpm2{,-*}                                                   \
+        munge{,-*}                                                  \
+                                                                    \
+        scl-utils{,-*}                                              \
+                                                                    \
+        ncurses{,-*}                                                \
+        hwloc{,-*}                                                  \
+        icu{,-*}                                                    \
+        {glibc{,-devel},libgcc}{,.i686}                             \
+        {gmp,mpfr,libmpc}{,-*}                                      \
+        gperftools{,-*}                                             \
+        lib{asan{,3},tsan,ubsan}{,-*}                               \
+        lib{exif,jpeg-turbo,tiff,png,gomp,gphoto2}{,-*}             \
+        OpenEXR{,-*}                                                \
+        {libv4l,v4l-utils}{,-*}                                     \
+        libunicap{,gtk}{,-*}                                        \
+        libglvnd{,-*}                                               \
+        tbb{,-*}                                                    \
+        {bzip2,zlib,libzip,{,lib}zstd,lz4,{,p}xz,snappy}{,-*}       \
+        lib{telnet,ssh{,2},curl,aio,ffi,edit,icu,xslt}{,-*}         \
+        boost{,-*}                                                  \
+        {flex,cups,bison,antlr}{,-*}                                \
+        open{blas,cv,ldap,ni,ssh,ssl}{,-*}                          \
+        {atlas,eigen3}{,-*}                                         \
+        lapack{,64}{,-*}                                            \
+        {libsodium,mbedtls}{,-*}                                    \
+        libev{,-devel,-source,-debuginfo}                           \
+        {asciidoc,gettext,xmlto,c-ares,pcre{,2}}{,-*}               \
+        librados2{,-*}                                              \
+        {gflags,glog,gmock,gtest,protobuf}{,-*}                     \
+        {redis,hiredis}{,-*}                                        \
+        ImageMagick{,-*}                                            \
+        docbook{,5,2X}{,-*}                                         \
+        nagios{,selinux,devel,debuginfo,-plugins-all}               \
+        {nrpe,nsca}                                                 \
+        {collectd,rrdtool,pnp4nagios}{,-*}                          \
+        cuda                                                        \
+        nvidia-docker2                                              \
+                                                                    \
+        hdf5{,-*}                                                   \
+        {leveldb,lmdb}{,-*}                                         \
+        {mariadb,postgresql}{,-*}                                   \
+                                                                    \
+        {fio,{file,sys}bench}{,-*}                                  \
+                                                                    \
+        {,pam_}krb5{,-*}                                            \
+        {sudo,nss,sssd,authconfig}{,-*}                             \
+                                                                    \
+        gitlab-ci-multi-runner                                      \
+                                                                    \
+        youtube-dl                                                  \
+                                                                    \
+        privoxy{,-*}                                                \
+                                                                    \
+        wine                                                        \
+                                                                    \
+        libselinux{,-*}                                             \
+        policycoreutils{,-*}                                        \
+        se{troubleshoot,tools}{,-*}                                 \
+        selinux-policy{,-*}                                         \
+                                                                    \
+        mod_authnz_*                                                \
+                                                                    \
+        cabextract{,-*}                                             \
+                                                                    \
+        devtoolset-{3,4,6,7}                                        \
 
-    do echo 'Retrying'; done
+        do echo 'Retrying'; done
+    done
 
     # TODO: Fix the following issue:
     #       LLVM may select the wrong gcc toolchain without libgcc_s integrated.
