cbc663a3d8f3c9f961a3a197f4fbd3d8eb7a6dd3
Tongliang Liao <xkszltl@gmail.com>
2018-01-25 17:02:43 +0800

    Use CUDA_ARCH_NAME to fix compile error "Unsupported gpu architecture 'compute_20'" in Caffe.

--- pkgs/caffe.sh
+++ pkgs/caffe.sh
@@ -15,12 +15,15 @@
     ( set -e
         . scl_source enable devtoolset-6 || true
 
+        # CUDA 9.x has removed Fermi (2.x) support.
+        # Use CUDA_ARCH_NAME to fix compile error "Unsupported gpu architecture 'compute_20'".
         cmake                                   \
             -G"Unix Makefiles"                  \
             -DCMAKE_BUILD_TYPE=Release          \
             -DCMAKE_C_FLAGS="-g"                \
             -DCMAKE_CXX_FLAGS="-g"              \
             -DCMAKE_VERBOSE_MAKEFILE=ON         \
+            -DCUDA_ARCH_NAME=Pascal             \
             -DBLAS=Open                         \
             -DUSE_NCCL=ON                       \
             ..
