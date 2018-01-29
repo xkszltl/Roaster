dd75a597d5fd4bdc5465ebbd5ede284e9fce6fe7
Tongliang Liao <xkszltl@gmail.com>
2018-01-30 03:39:55 +0800

    Remove redundant deployment of scripts.

--- gitlab-ci/build_stage.sh
+++ gitlab-ci/build_stage.sh
@@ -10,10 +10,10 @@ if [ $GITLAB_CI ]; then
     <<<$CI_REGISTRY_PASSWORD
 
 	if [ $(echo $CI_COMMIT_REF_NAME | sed 's/^[^\-]*-//') = $CI_JOB_STAGE ]; then
-		cat stage/$CI_JOB_STAGE > Dockerfile
+		cat stage/$CI_JOB_STAGE
 	else
-		grep -v ADD stage/$CI_JOB_STAGE > Dockerfile
-	fi
+		grep -v '"/etc/codingcafe/scripts"' stage/$CI_JOB_STAGE
+	fi > Dockerfile
 	docker build                                        \
         --cpu-shares 128                                \
         --no-cache                                      \
