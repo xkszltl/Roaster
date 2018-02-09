e1954921408317acff00b5d6ddb93d8f7fb351b5
Tongliang Liao <xkszltl@gmail.com>
2018-02-09 08:43:28 +0800

    Use JSON task descriptor and "parallel" for repo mirroring.

--- repo-cache.sh
+++ repo-cache.sh
@@ -9,11 +9,14 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 # ----------------------------------------------------------------
 
+export DRY=false
+export DRY_RSYNC=$($DRY && echo --dry-run)
+export DRY_WGET=$($DRY && echo --spider)
+
 export REPOSYNC='reposync
     --cachedir=$(mktemp -d)
     --download-metadata
     --downloadcomps
-    --gpgcheck
     --norepopath
     --source
     -r
@@ -25,14 +28,16 @@ export CREATEREPO='createrepo_c
     --compress-type=xz
     --database
     $([ -f comps.xml ] && echo --groupfile=comps.xml)
+    --keep-all-metadata
     --pretty
     --workers $(nproc)
+    --update
     $(pwd)
 '
 
 export ROUTE='10.0.0.$([ $(expr $RANDOM % 15) -lt 10 ] && echo 12 || echo 11)'
 
-export MAX_ATTEMPT=4
+export MAX_ATTEMPT=3
 
 mkdir -p /var/www/repos
 cd $_
@@ -41,13 +46,13 @@ cd $_
 
 if false; then :
 elif ping -nfc 10 mirrors.tuna.tsinghua.edu.cn -I 10.0.0.11; then
-    rsync -avPz --delete --address 10.0.0.11 rsync://mirrors.tuna.tsinghua.edu.cn/CTAN/ CTAN &
+    $DRY || rsync $DRY_RSYNC -avPz --delete --address 10.0.0.11 rsync://mirrors.tuna.tsinghua.edu.cn/CTAN/ CTAN &
 elif ping -nfc 10 rsync.mirrors.ustc.edu.cn -I 10.0.0.12; then
-    rsync -avPz --delete --address 10.0.0.12 rsync://rsync.mirrors.ustc.edu.cn/CTAN/ CTAN &
+    $DRY || rsync $DRY_RSYNC -avPz --delete --address 10.0.0.12 rsync://rsync.mirrors.ustc.edu.cn/CTAN/ CTAN &
 elif ping -nfc 10 mirrors.tuna.tsinghua.edu.cn -I 10.0.0.12; then
-    rsync -avPz --delete --address 10.0.0.12 rsync://mirrors.tuna.tsinghua.edu.cn/CTAN/ CTAN &
+    $DRY || rsync $DRY_RSYNC -avPz --delete --address 10.0.0.12 rsync://mirrors.tuna.tsinghua.edu.cn/CTAN/ CTAN &
 elif ping -nfc 10 rsync.mirrors.ustc.edu.cn -I 10.0.0.11; then
-    rsync -avPz --delete --address 10.0.0.11 rsync://rsync.mirrors.ustc.edu.cn/CTAN/ CTAN &
+    $DRY || rsync $DRY_RSYNC -avPz --delete --address 10.0.0.11 rsync://rsync.mirrors.ustc.edu.cn/CTAN/ CTAN &
 else
     echo "No mirror to try for CTAN"
     exit 1
@@ -63,7 +68,7 @@ fi
     export INTEL_URL="http://registrationcenter-download.intel.com/akdlm/irc_nas/tec"
 
     parallel --line-buffer --bar 'bash -c '"'"'
-        wget -cq --bind-address='$ROUTE' $INTEL_URL/{}
+        $DRY || wget $DRY_WGET -cq --bind-address='$ROUTE' $INTEL_URL/{}
     '"'" :::   \
         12414/l_daal_2018.1.163.tgz     \
         12414/l_mkl_2018.1.163.tgz      \
@@ -86,18 +91,23 @@ fi
 
 (
     set -e
-    mkdir -p nvidia/cudnn
-    cd $_
 
-    for i in v7.0.{4,5}; do
-        mkdir -p $i
-        pushd $_
-        for j in 9.{0,1,2,3,4,5,6,7,8,9}-{{linux,osx}-%s.tgz,windows10-%s.zip}; do
-            wget -cq --bind-address=$(eval 'echo '$ROUTE) https://developer.download.nvidia.com/compute/redist/cudnn/$(basename $(pwd))/cudnn-$(printf $j x64-$(basename $(pwd) | sed 's/\..*//')) &
-        done
-        popd
-    done
+    export CUDNN_URL="https://developer.download.nvidia.com/compute/redist/cudnn"
+
+    parallel --line-buffer --bar 'bash -c '"'"'
+        export lhs=$(sed "s/=.*//" <<< "{}")
+        export rhs=$(sed "s/.*=//" <<< "{}")
+        mkdir -p nvidia/cudnn/$lhs
+        $DRY || wget $DRY_WGET -cq --bind-address='$ROUTE' $CUDNN_URL/$(basename $lhs)/cudnn-$(printf $rhs x64-$(basename $lhs | cut -f1 -d.)) -P $_
+    '"'" :::    \
+        v7.0.{4,5}=9.{0,1,2,3,4,5,6,7,8,9}-{{linux,osx}-%s.tgz,windows10-%s.zip}    \
+    &
 )
+
+# ----------------------------------------------------------------
+
+yum makecache fast -y || true
+rm -rf $(find . -name .repodata -type d)
 
 # ----------------------------------------------------------------
 
@@ -108,128 +118,147 @@ export HTTP_PROXY=proxy.codingcafe.org:8118
 
 # ----------------------------------------------------------------
 
-yum makecache fast -y || true
-rm -rf $(find . -name .repodata -type d)
+export TASKS=''
 
-# ----------------------------------------------------------------
-
-for i in base updates extras centosplus; do :
-for j in =$(uname -i) -source=Source $([ $i = base ] && echo -debuginfo=debug/$(uname -i)); do :
-(
-    set -e
-    mkdir -p centos/7/$i/$(sed 's/.*=//' <<< $j)
-    cd $_
-    for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC $i$(sed 's/=.*//' <<< $j) && break
-        echo "Retry \"$i$(sed 's/=.*//' <<< $j)\""
-    done
-    eval $CREATEREPO
-) &
+for i in base updates extras centosplus; do
+for j in =$(uname -i) -source=Source $([ $i = base ] && echo -debuginfo=debug/$(uname -i)); do
+    export lhs=$(sed 's/=.*//' <<< $j)
+    export rhs=$(sed 's/.*=//' <<< $j)
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"$i$lhs\",
+        \"path\": \"centos/7/$i/$rhs\"
+    },"
 done
 done
 
 # ----------------------------------------------------------------
 
-for i in {=,-debuginfo=debug/}$(uname -i) -source=SRPMS; do :
-(
-    set -e
-    mkdir -p epel/7/$(sed 's/.*=//' <<< $i)
-    cd $_
-    for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC epel$(sed 's/=.*//' <<< $i) && break
-        echo "Retry \"epel$(sed 's/=.*//' <<< $i)\""
-    done
-    eval $CREATEREPO
-) &
+for i in {=,-debuginfo=debug/}$(uname -i) -source=SRPMS; do
+    export lhs=$(sed 's/=.*//' <<< $i)
+    export rhs=$(sed 's/.*=//' <<< $i)
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"epel$lhs\",
+        \"path\": \"epel/7/$rhs\"
+    },"
 done
 
 # ----------------------------------------------------------------
 
-for i in sclo rh; do :
-for j in =$(uname -i)/$i -testing=$(uname -i)/$i/testing -source=Source/$i -debuginfo=$(uname -i)/$i/debuginfo; do :
-(
-    set -e
-    mkdir -p centos/7/sclo/$(sed 's/.*=//' <<< $j)
-    cd $_
-    for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC centos-sclo-$i$(sed 's/=.*//' <<< $j) && break
-        echo "Retry \"centos-sclo-$i$(sed 's/=.*//' <<< $j)\""
-    done
-    eval $CREATEREPO
-) &
+for i in sclo rh; do
+for j in =$(uname -i)/$i -testing=$(uname -i)/$i/testing -source=Source/$i -debuginfo=$(uname -i)/$i/debuginfo; do
+    export lhs=$(sed 's/=.*//' <<< $j)
+    export rhs=$(sed 's/.*=//' <<< $j)
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"centos-sclo-$i$lhs\",
+        \"path\": \"centos/7/sclo/$rhs\"
+    },"
 done
 done
 
 # ----------------------------------------------------------------
 
-for i in elrepo{,-testing,-kernel,-extras}; do :
-(
-    set -e
-    mkdir -p $(sed 's/-/\//' <<< $i)/el7
-    cd $_
-    for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC $i && break
-        echo "Retry \"$i\""
-    done
-    eval $CREATEREPO
-) &
+for i in elrepo{,-testing,-kernel,-extras}; do
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"$i\",
+        \"path\": \"$(sed 's/-/\//' <<< $i)/el7\"
+    },"
 done
 
 # ----------------------------------------------------------------
 
 (
     set -e
+
     mkdir -p cuda/rhel7/$(uname -i)
     cd $_
-    rpm --import https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/7fa2af80.pub
-    for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC cuda && break
-        echo "Retry \"cuda\""
-    done
-    eval $CREATEREPO
-) &
-
-# ----------------------------------------------------------------
-
-(
-    set -e
-    mkdir -p docker/linux/centos
-    cd $_
-    wget -cq https://download.docker.com/linux/centos/gpg
-    rpm --import gpg
+    $DRY || wget $DRY_WGET -cq https://developer.download.nvidia.com/compute/cuda/repos/rhel7/$(uname -i)/7fa2af80.pub
+    $DRY || rpm --import 7fa2af80.pub
 )
 
-for i in {=,-debuginfo=debug-}$(uname -i) -source=source; do :
-for j in stable edge test; do :
+export REPO_TASKS="$REPO_TASKS
+{
+    \"repo\": \"cuda\",
+    \"path\": \"cuda/rhel7/$(uname -i)\"
+},"
+
+# ----------------------------------------------------------------
+
 (
     set -e
-    mkdir -p docker/linux/centos/7/$(sed 's/.*=//' <<< $i)/$j
+
+    mkdir -p docker/linux/centos
     cd $_
-    for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC docker-ce-$j$(sed 's/=.*//' <<< $i) && break
-        echo "Retry \"docker-ce-$j$(sed 's/=.*//' <<< $i)\""
-    done
-    eval $CREATEREPO
-) &
+    $DRY || wget $DRY_WGET -cq https://download.docker.com/linux/centos/gpg
+    $DRY || rpm --import gpg
+)
+
+for i in stable edge test; do :
+for j in {=,-debuginfo=debug-}$(uname -i) -source=source; do
+    export lhs=$(sed 's/=.*//' <<< $j)
+    export rhs=$(sed 's/.*=//' <<< $j)
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"docker-ce-$i$lhs\",
+        \"path\": \"docker/linux/centos/7/$rhs/$i\"
+    },"
 done
 done
 
 # ----------------------------------------------------------------
 
-for i in gitlab=gitlab-ce runner=gitlab-ci-multi-runner; do :
-for j in =$(uname -i) -source=SRPMS; do :
-(
-    set -e
-    mkdir -p gitlab/$(sed 's/.*=//' <<< $i)/el/7/$(sed 's/.*=//' <<< $j)
+for i in =$(uname -i) -source=SRPMS; do
+    export lhs=$(sed 's/=.*//' <<< $i)
+    export rhs=$(sed 's/.*=//' <<< $i)
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"gitlab_gitlab-ce$lhs\",
+        \"path\": \"gitlab/gitlab-ce/el/7/$rhs\",
+        \"sync_args\": \"--newest-only\"
+    },"
+done
+
+# ----------------------------------------------------------------
+
+for j in =$(uname -i) -source=SRPMS; do
+    export lhs=$(sed 's/=.*//' <<< $i)
+    export rhs=$(sed 's/.*=//' <<< $i)
+    export REPO_TASKS="$REPO_TASKS
+    {
+        \"repo\": \"runner_gitlab-ci-multi-runner$lhs\",
+        \"path\": \"gitlab/gitlab-ci-multi-runner/el/7/$rhs\",
+        \"sync_args\": \"--newest-only\"
+    },"
+done
+
+# ----------------------------------------------------------------
+
+export REPO_TASKS=$(sed 's/,[[:space:]]*\(\]\)/\1/g' <<< "
+{
+    \"repo_tasks\": [ $REPO_TASKS ]
+}")
+
+parallel --line-buffer --bar 'bash -c '"'"'
+    export JSON_OBJ=$(jq ".repo_tasks | .[{}]" <<< "$REPO_TASKS")
+    echo "Execute task $JSON_OBJ"
+
+    export repo=$(jq -r ".repo" <<< "$JSON_OBJ")
+    export path=$(jq -r ".path" <<< "$JSON_OBJ")
+    jq -e ".sync_args" <<< "$JSON_OBJ" > /dev/null && export sync_args=$(jq -r ".sync_args" <<< "$JSON_OBJ")
+
+    mkdir -p $path
     cd $_
     for attempt in $(seq $MAX_ATTEMPT); do
-        eval $REPOSYNC $(sed 's/=.*//' <<< $i)_$(sed 's/.*=//' <<< $i)$(sed 's/=.*//' <<< $j) $([ $(sed 's/=.*//' <<< $i) = gitlab ] && echo '--newest-only') && break
-        echo "Retry \"$(sed 's/=.*//' <<< $i)_$(sed 's/.*=//' <<< $i)$(sed 's/=.*//' <<< $j)\""
+        eval $REPOSYNC $repo $sync_args && break
+        echo "Retry \"$repo\""
     done
     eval $CREATEREPO
-) &
-done
-done
+'"'" :::    \
+    $(seq 0 $(expr $(jq '.repo_tasks | length' <<< "$REPO_TASKS") - 1)) \
+&
 
 # ----------------------------------------------------------------
 
