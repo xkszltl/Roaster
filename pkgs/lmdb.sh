dfcaaa982afbafc3d7ee8b20e5296454a76adef2
Tongliang Liao <xkszltl@gmail.com>
2018-02-11 06:14:55 +0800

    Split cd for LMDB when using --no-checkout.

--- pkgs/lmdb.sh
+++ pkgs/lmdb.sh
@@ -6,8 +6,9 @@
     cd $SCRATCH
 
     until git clone --depth 1 --no-checkout --no-single-branch $GIT_MIRROR/LMDB/lmdb.git; do echo 'Retrying'; done
-    cd lmdb/libraries/liblmdb
+    cd lmdb
     git checkout $(git tag | sed -n '/^LMDB_[0-9\.]*$/p' | sort -V | tail -n1)
+    cd libraries/liblmdb
 
     . scl_source enable devtoolset-7 || true
 
