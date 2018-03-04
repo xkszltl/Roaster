466162fb2458e28b792626a006a7b7c98692d4dc
Tongliang Liao <xkszltl@gmail.com>
2018-03-05 04:27:19 +0800

    Add script for docker configuration.

---
+++ docker_conf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+pushd /etc/docker
+
+echo '========================================'
+echo '| Before'
+echo '========================================'
+
+cat 'daemon.json' | tee 'daemon.json.bak'
+
+echo '========================================'
+echo '| After'
+echo '========================================'
+
+cat 'daemon.json.bak'                                                                               \
+| jq '. |= . + {"storage_driver":"devicemapper"}'                                                   \
+| jq '. |= . + {"storage_opts":[]}'                                                                 \
+| jq '.storage_opts[.storage_opts | length] |= . + "dm.thinpooldev=/dev/mapper/Mocha-docker--pool"' \
+| jq '.storage_opts[.storage_opts | length] |= . + dm.use_deferred_removal=true"'                   \
+| jq '.storage_opts[.storage_opts | length] |= . + dm.use_deferred_deletion=true"'                  \
+| tee 'daemon.json'
