21ec38ed02293e698caa95fa7a88c62281b5ac38
Tongliang Liao <xkszltl@gmail.com>
2018-03-05 05:02:55 +0800

    Add quotation mark around key with '-'.

--- docker_conf.sh
+++ docker_conf.sh
@@ -14,13 +14,13 @@ echo '========================================'
 echo '| After'
 echo '========================================'
 
-cat 'daemon.json.bak'                                                                                   \
-| jq -e '. |= . + {"storage-driver":"devicemapper"}'                                                    \
-| jq -e '. |= . + {"storage-opts":[]}'                                                                  \
-| jq -e '.storage-opts[.storage-opts | length] |= . + "dm.thinpooldev=/dev/mapper/Mocha-docker--pool"'  \
-| jq -e '.storage-opts[.storage-opts | length] |= . + "dm.use_deferred_removal=true"'                   \
-| jq -e '.storage-opts[.storage-opts | length] |= . + "dm.use_deferred_deletion=true"'                  \
-| tee 'daemon.json' | jq -e '.'                                                                         \
+cat 'daemon.json.bak'                                                                                       \
+| jq -e '. |= . + {"storage-driver":"devicemapper"}'                                                        \
+| jq -e '. |= . + {"storage-opts":[]}'                                                                      \
+| jq -e '."storage-opts"[."storage-opts" | length] |= . + "dm.thinpooldev=/dev/mapper/Mocha-docker--pool"'  \
+| jq -e '."storage-opts"[."storage-opts" | length] |= . + "dm.use_deferred_removal=true"'                   \
+| jq -e '."storage-opts"[."storage-opts" | length] |= . + "dm.use_deferred_deletion=true"'                  \
+| tee 'daemon.json' | jq -e '.'                                                                             \
 || ( set -e
     cat 'daemon.json.bak' > 'daemon.json'
     false
