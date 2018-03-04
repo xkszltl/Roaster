3fab077ed196d389bdd89ed0307da45d7a8247ef
Tongliang Liao <xkszltl@gmail.com>
2018-03-05 05:00:02 +0800

    Fail jq with non-zero exit code.

--- docker_conf.sh
+++ docker_conf.sh
@@ -8,19 +8,19 @@ echo '========================================'
 echo '| Before'
 echo '========================================'
 
-cat 'daemon.json' | tee 'daemon.json.bak' | jq '.'
+cat 'daemon.json' | tee 'daemon.json.bak' | jq -e '.'
 
 echo '========================================'
 echo '| After'
 echo '========================================'
 
-cat 'daemon.json.bak'                                                                               \
-| jq '. |= . + {"storage-driver":"devicemapper"}'                                                   \
-| jq '. |= . + {"storage-opts":[]}'                                                                 \
-| jq '.storage-opts[.storage-opts | length] |= . + "dm.thinpooldev=/dev/mapper/Mocha-docker--pool"' \
-| jq '.storage-opts[.storage-opts | length] |= . + "dm.use_deferred_removal=true"'                  \
-| jq '.storage-opts[.storage-opts | length] |= . + "dm.use_deferred_deletion=true"'                 \
-| tee 'daemon.json' | jq '.'                                                                        \
+cat 'daemon.json.bak'                                                                                   \
+| jq -e '. |= . + {"storage-driver":"devicemapper"}'                                                    \
+| jq -e '. |= . + {"storage-opts":[]}'                                                                  \
+| jq -e '.storage-opts[.storage-opts | length] |= . + "dm.thinpooldev=/dev/mapper/Mocha-docker--pool"'  \
+| jq -e '.storage-opts[.storage-opts | length] |= . + "dm.use_deferred_removal=true"'                   \
+| jq -e '.storage-opts[.storage-opts | length] |= . + "dm.use_deferred_deletion=true"'                  \
+| tee 'daemon.json' | jq -e '.'                                                                         \
 || ( set -e
     cat 'daemon.json.bak' > 'daemon.json'
     false
