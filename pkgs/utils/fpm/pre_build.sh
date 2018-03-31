2e78e523e83700efd55914d8762f359ab8d5105a
Tongliang Liao <xkszltl@gmail.com>
2018-03-31 02:38:34 -0700

    Remove content of package "filesystem" from screen.

--- pkgs/utils/fpm/pre_build.sh
+++ pkgs/utils/fpm/pre_build.sh
@@ -6,4 +6,4 @@ export INSTALL_REL="$INSTALL_ROOT/$INSTALL_PREFIX"
 mkdir -p "$INSTALL_REL"
 export INSTALL_ABS=$(readlink -e "$INSTALL_REL")
 
-[ "$FPM_EXCLUDE" ] || ! rpm -q filesystem || export FPM_EXCLUDE="$(repoquery -l filesystem)" &
+[ "$FPM_EXCLUDE" ] || ! rpm -q filesystem || export FPM_EXCLUDE="$(repoquery -l filesystem)" > /dev/null &
