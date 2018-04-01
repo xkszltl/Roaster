9bc1141d0cb419bb7570009ae3ecfe546c542f41
Tongliang Liao <xkszltl@gmail.com>
2018-03-31 23:05:37 -0700

    Bugfix for ld.so.conf.d path.

--- pkgs/utils/fpm/post_build.sh
+++ pkgs/utils/fpm/post_build.sh
@@ -2,8 +2,10 @@
 
 pushd "$INSTALL_ROOT"
 
-mkdir -p "etc/codingcafe.conf.d"
-find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*' | sed 's/\.//' | sed 's/\/\/*/\//g' > "etc/codingcafe.conf.d/$(basename $(dirname $(pwd))).conf"
+export PKG_LD_CONF_DIR="/etc/ld.so.conf.d/codingcafe.conf.d"
+
+mkdir -p "./$PKG_LD_CONF_DIR"
+find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*' | sed 's/\.//' | sed 's/\/\/*/\//g' > "./$PKG_LD_CONF_DIR/$(basename $(dirname $(pwd))).conf"
 
 popd
 
