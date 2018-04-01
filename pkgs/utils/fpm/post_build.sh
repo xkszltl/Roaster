8a32bdd9bea62500becd61c14c44585c80f34968
Tongliang Liao <xkszltl@gmail.com>
2018-04-01 03:08:53 -0700

    Bugfix about path.

--- pkgs/utils/fpm/post_build.sh
+++ pkgs/utils/fpm/post_build.sh
@@ -13,7 +13,7 @@ pushd "$INSTALL_ROOT"
 export PKG_LD_CONF_DIR="/etc/ld.so.conf.d/codingcafe.conf.d"
 
 mkdir -p "./$PKG_LD_CONF_DIR"
-find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*' | sed 's/\.//' | sed 's/\/\/*/\//g' > "./$PKG_LD_CONF_DIR/$(basename $(dirname $(pwd))).conf"
+find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*' | sed 's/\.//' | sed 's/\/\/*/\//g' > "./$PKG_LD_CONF_DIR/$(basename $(cd ../.. && pwd)).conf"
 
 popd
 
