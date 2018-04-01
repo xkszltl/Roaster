6643a4ec98202eec7603d9e7d76f626ec92f2677
Tongliang Liao <xkszltl@gmail.com>
2018-04-01 02:14:12 -0700

    Add fpm packaging for glog.

--- pkgs/glog.sh
+++ pkgs/glog.sh
@@ -9,28 +9,51 @@
     cd glog
     git checkout $(git tag | sed -n '/^v[0-9\.]*$/p' | sort -V | tail -n1)
 
-    # . scl_source enable devtoolset-7 || true
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
 
-    # Downgrade to gcc-5 to solve "unrecognized relocation" error in caffe2.
-    . scl_source enable devtoolset-4 || true
+    (
+        # . scl_source enable devtoolset-7 || true
 
-    mkdir -p build
-    cd $_
+        # Downgrade to gcc-5 to solve "unrecognized relocation" error in caffe2.
+        . scl_source enable devtoolset-4 || true
 
-    cmake                                               \
-        -G"Ninja"                                       \
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo               \
-        -DCMAKE_C_COMPILER=gcc                          \
-        -DCMAKE_CXX_COMPILER=g++                        \
-        -DCMAKE_INSTALL_PREFIX=/usr/local               \
-        ..
+        set -e
 
-    time cmake --build .
-    time cmake --build . --target test
-    time cmake --build . --target install
+        mkdir -p build
+        cd $_
 
-    ldconfig &
-    $IS_CONTAINER && ccache -C &
+        cmake                                               \
+            -G"Ninja"                                       \
+            -DCMAKE_BUILD_TYPE=RelWithDebInfo               \
+            -DCMAKE_C_COMPILER=gcc                          \
+            -DCMAKE_CXX_COMPILER=g++                        \
+            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"           \
+            ..
+
+        time cmake --build .
+        time cmake --build . --target test
+        time cmake --build . --target install
+    )
+
+    . "$ROOT_DIR/pkgs/utils/fpm/post_build.sh"
+
+    fpm                                                             \
+        --after-install "$ROOT_DIR/pkgs/utils/fpm/post_install.sh"  \
+        --after-remove "$ROOT_DIR/pkgs/utils/fpm/post_install.sh"   \
+        --chdir "$INSTALL_ROOT"                                     \
+        --exclude-file "$INSTALL_ROOT/../exclude.conf"              \
+        --input-type dir                                            \
+        --iteration "$(git log -n1 --format="%h")"                  \
+        --name "codingcafe-$(basename $(pwd))"                      \
+        --output-type rpm                                           \
+        --package "$INSTALL_ROOT/.."                                \
+        --rpm-compression xz                                        \
+        --rpm-digest sha512                                         \
+        --vendor "CodingCafe"                                       \
+        --version "$(git describe --tags | sed 's/[^0-9\.]//g')"
+
+    "$ROOT_DIR/pkgs/utils/fpm/install.sh"
+
     cd
     rm -rf $SCRATCH/glog
     wait
