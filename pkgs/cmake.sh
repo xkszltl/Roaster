1598a4db45115551389a3108c512643aac4f3659
Tongliang Liao <xkszltl@gmail.com>
2018-04-02 02:41:21 -0700

    Use fpm for cmake.

--- pkgs/cmake.sh
+++ pkgs/cmake.sh
@@ -4,16 +4,30 @@
 
 [ -e $STAGE/cmake ] && ( set -xe
     cd $SCRATCH
+    
+    # ------------------------------------------------------------
 
     until git clone --depth 1 --branch release $GIT_MIRROR/Kitware/CMake.git; do echo 'Retrying'; done
     cd CMake
-    # git checkout $(git tag | sed -n '/^[0-9\.]*$/p' | sort -V | tail -n1)
+    git checkout $(git tag | sed -n '/^[0-9\.]*$/p' | sort -V | tail -n1)
 
-    . scl_source enable devtoolset-7 || true
+    # ------------------------------------------------------------
 
-    ./bootstrap --prefix=/usr --parallel=$(nproc)
-    VERBOSE=1 time make -j$(nproc)
-    VERBOSE=1 time make -j install
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
+
+    (
+        set +x
+        . scl_source enable devtoolset-7 || true
+        set -xe
+
+        ./bootstrap --prefix="$INSTALL_ABS" --parallel=$(nproc)
+        VERBOSE=1 time make -j$(nproc)
+        VERBOSE=1 time make -j install
+    )
+
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+
+    # ------------------------------------------------------------
 
     cd
     rm -rf $SCRATCH/CMake
