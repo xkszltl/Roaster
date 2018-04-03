03fea80e67f098549efdff99193f502dec519c24
Tongliang Liao <xkszltl@gmail.com>
2018-04-02 21:25:28 -0700

    Use fpm for caffe.

--- pkgs/caffe.sh
+++ pkgs/caffe.sh
@@ -9,35 +9,47 @@
     cd caffe
 
     # ------------------------------------------------------------
+    # Dummy version assignment
+    # ------------------------------------------------------------
 
-    mkdir -p build
-    cd $_
-    ( set -xe
+    git tag '0.0.0'
+
+    # ------------------------------------------------------------
+
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
+
+    (
+        set +x
         . scl_source enable devtoolset-6 || true
+        set -xe
+
+        mkdir -p build
+        cd $_
 
         # CUDA 9.x has removed Fermi (2.x) support.
         # Use CUDA_ARCH_NAME to fix compile error "Unsupported gpu architecture 'compute_20'".
-        cmake                                   \
-            -G"Ninja"                           \
-            -DCMAKE_BUILD_TYPE=Release          \
-            -DCMAKE_C{,XX}_FLAGS="-g"           \
-            -DCMAKE_INSTALL_PREFIX="/usr"       \
-            -DCMAKE_VERBOSE_MAKEFILE=ON         \
-            -DCUDA_ARCH_NAME=Pascal             \
-            -DBLAS=MKL                          \
-            -DUSE_NCCL=ON                       \
+        cmake                                       \
+            -G"Ninja"                               \
+            -DCMAKE_BUILD_TYPE=Release              \
+            -DCMAKE_C{,XX}_FLAGS="-g"               \
+            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
+            -DCMAKE_VERBOSE_MAKEFILE=ON             \
+            -DCUDA_ARCH_NAME=Pascal                 \
+            -DBLAS=MKL                              \
+            -DUSE_NCCL=ON                           \
             ..
 
         time cmake --build .
-        nvidia-smi && time cmake --build . --target runtest
+        time cmake --build . --target runtest || ! nvidia-smi
         time cmake --build . --target install
     )
 
-    ldconfig &
-    $IS_CONTAINER && ccache -C &
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+    
+    # ------------------------------------------------------------
+
     cd
     rm -rf $SCRATCH/caffe
-    wait
 )
 sudo rm -vf $STAGE/caffe
 sync || true
