a7a1c18a1ee15d1b4c13a846e7088df211d2e2cf
Tongliang Liao <xkszltl@gmail.com>
2018-04-08 22:11:14 -0700

    Use fpm for LMDB.

--- pkgs/lmdb.sh
+++ pkgs/lmdb.sh
@@ -8,19 +8,27 @@
     until git clone --depth 1 --no-checkout --no-single-branch $GIT_MIRROR/LMDB/lmdb.git; do echo 'Retrying'; done
     cd lmdb
     git checkout $(git tag | sed -n '/^LMDB_[0-9\.]*$/p' | sort -V | tail -n1)
-    cd libraries/liblmdb
 
-    . scl_source enable devtoolset-7 || true
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
 
-    make -j$(nproc)
-    make test
-    make install -j
+    (
+        set +x
+        . scl_source enable devtoolset-7 || true
+        set -xe
 
-    ldconfig &
-    $IS_CONTAINER && ccache -C &
+        . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
+
+        cd libraries/liblmdb
+
+        make -j$(nproc)
+        make test
+        make PREFIX="$INSTALL_ABS" install -j
+    )
+
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+
     cd
     rm -rf $SCRATCH/lmdb
-    wait
 )
 rm -rvf $STAGE/lmdb
 sync || true
