490fdc9124ec86046b3e5929836c360c90adf9e2
Tongliang Liao <xkszltl@gmail.com>
2018-04-15 13:44:23 +0800

    Append 0 to zombie check.

--- pkgs/utils/sudo_ping_daemon.sh
+++ pkgs/utils/sudo_ping_daemon.sh
@@ -13,14 +13,18 @@ if [ $PPID -le 1 ]; then
 	exit 1
 fi
 
-SUDO_PING_PID="$("$SHELL" -c 'echo $PPID' | xargs ps -o ppid= -p)"
-
 # ----------------------------------------------------------------
 
-while [ $(ps -o ppid= -p "$SUDO_PING_PID") -eq $PPID ]; do
-    SUDO_PING_DDL="$(expr "$(date +%s)" + "$SUDO_PING_HEARTBEAT_SEC" - "$SUDO_PING_SLA_SEC")"
-    sudo -n true
-    while [ $(ps -o ppid= -p "$SUDO_PING_PID") -eq $PPID ] && [ "$(date +%s)" -lt "$SUDO_PING_DDL" ]; do
-    	sleep "$SUDO_PING_SLA_SEC"
+(
+    set -e
+    export SUDO_PING_PID="$("$SHELL" -c 'echo $PPID' | xargs ps -o ppid= -p | xargs ps -o ppid= -p)"
+
+    # Append '0' because ps may not be able to found the process itself as zombie.
+    while [ "$(ps -o ppid= -p "$SUDO_PING_PID")"0 -eq "$PPID"0 ]; do
+        SUDO_PING_DDL="$(expr "$(date +%s)" + "$SUDO_PING_HEARTBEAT_SEC" - "$SUDO_PING_SLA_SEC")"
+        sudo -n true
+        while [ "$(ps -o ppid= -p "$SUDO_PING_PID")"0 -eq "$PPID" ] && [ "$(date +%s)" -lt "$SUDO_PING_DDL" ]; do
+    	    sleep "$SUDO_PING_SLA_SEC"
+        done
     done
-done &
+) &
