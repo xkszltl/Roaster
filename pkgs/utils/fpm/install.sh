46a4b159828758b33d4fe9e456a61c5487f74a26
Tongliang Liao <xkszltl@gmail.com>
2018-04-16 15:29:29 +0800

    Reorder install commands.

--- pkgs/utils/fpm/install.sh
+++ pkgs/utils/fpm/install.sh
@@ -21,13 +21,13 @@ rm -rf "$INSTALL_ABS"
 export PKG_PATH="$(find "$INSTALL_ROOT/.." -maxdepth 1 -type f -name "$PKG_NAME-*.rpm" | xargs readlink -e)"
 
 if [ ! "$PKG_PATH" ]; then
-    echo "No package file found for \"$PKG_NAME\"."
-    echo "    Might have error occured during packaging."
+    echo "[ERROR] No package file found for \"$PKG_NAME\"."
+    echo "[ERROR]     Might have error occured during packaging."
     exit 1
 elif [ $(wc -l <<<"$PKG_NAME") -gt 1 ]; then
-    echo "Multiple candidates detected:"
-    sed 's/^/    /' <<<"$PKG_PATH"
-    echo "Please update the search condition to narrow it down."
+    echo "[ERROR] Multiple candidates detected:"
+    sed 's/^/[ERROR]     /' <<<"$PKG_PATH"
+    echo "[ERROR] Please update the search condition to narrow it down."
     exit 1
 fi
 
@@ -38,12 +38,14 @@ rpm -qlp "$PKG_PATH"
 # ----------------------------------------------------------------
 
 if rpm -q "$PKG_NAME"; then
-    export PKG_YUM_SEQ="update reinstall downgrade"
+    export PKG_YUM_SEQ="reinstall update downgrade"
 else
     export PKG_YUM_SEQ="install"
 fi
 
 for i in $PKG_YUM_SEQ _; do
     [ "$i" != '_' ]
+    echo "[INFO] Trying with \"yum $i\"."
     sudo yum "$i" -y "$PKG_PATH" && break
+    echo "[INFO] Does not succeed with \"yum $i\"."
 done
