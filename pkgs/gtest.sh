26a14bb469dbefec6a2bc124b63c47a073c5787c
Tongliang Liao <xkszltl@gmail.com>
2018-04-19 14:33:49 +0800

    Add gtest/gmock.

---
+++ pkgs/gtest.sh
@@ -0,0 +1,55 @@
+# ================================================================
+# Compile Protobuf
+# ================================================================
+
+[ -e $STAGE/gtest ] && ( set -xe
+    cd $SCRATCH
+
+    # ------------------------------------------------------------
+
+    until git clone --depth 1 --single-branch -b "$(git ls-remote --tags "$GIT_MIRROR/google/googletest.git" | sed -n 's/.*[[:space:]]refs\/tags\/\(release-[0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1)" "$GIT_MIRROR/google/googletest.git" gtest; do echo 'Retrying'; done
+    # until git clone --depth 1 --single-branch "$GIT_MIRROR/google/googletest.git" gtest; do echo 'Retrying'; done
+    cd gtest
+    # git tag "$(git ls-remote --tags "$GIT_MIRROR/google/googletest.git" | sed -n 's/.*[[:space:]]refs\/tags\/\(release-[0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1).1"
+
+    # ------------------------------------------------------------
+
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
+
+    (
+        set +x
+        . scl_source enable devtoolset-7 || true
+        set -xe
+
+        mkdir -p build
+        cd $_
+
+        cmake                                       \
+            -DBUILD_GMOCK=ON                        \
+            -DBUILD_GTEST=ON                        \
+            -DBUILD_SHARED_LIBS=ON                  \
+            -DCMAKE_BUILD_TYPE=Release              \
+            -DCMAKE_C_COMPILER_LAUNCHER=ccache      \
+            -DCMAKE_C{,XX}_FLAGS="-g"               \
+            -DCMAKE_CXX_COMPILER_LAUNCHER=ccache    \
+            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
+            -Dgmock_build_tests=ON                  \
+            -Dgtest_build_samples=ON                \
+            -Dgtest_build_tests=ON                  \
+            -G"Ninja"                               \
+            ..
+
+        time cmake --build .
+        time cmake --build . --target test || true
+        time cmake --build . --target install
+    )
+
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+
+    # ------------------------------------------------------------
+
+    cd
+    rm -rf $SCRATCH/gtest
+)
+sudo rm -vf $STAGE/gtest
+sync || true
--- setup.sh
+++ setup.sh
@@ -97,7 +97,7 @@ cd $SCRATCH
     sudo rm -rvf $STAGE
     sudo mkdir -p $(dirname $STAGE)/.$(basename $STAGE)
     cd $_
-    [ $# -gt 0 ] && sudo touch $@ || sudo touch repo font pkg-{skip,all} intel auth ompi cuda slurm nagios ss tex cmake llvm-{gcc,clang} boost jemalloc openblas opencv gflags glog snappy protobuf leveldb rocksdb lmdb caffe caffe2
+    [ $# -gt 0 ] && sudo touch $@ || sudo touch repo font pkg-{skip,all} intel auth ompi cuda slurm nagios ss tex cmake llvm-{gcc,clang} boost jemalloc openblas opencv gtest gflags glog snappy protobuf leveldb rocksdb lmdb caffe caffe2
     sync || true
     cd $SCRATCH
     sudo mv -vf $(dirname $STAGE)/.$(basename $STAGE) $STAGE
@@ -123,6 +123,7 @@ for i in $(echo "
     jemalloc
     openblas
     opencv
+    gtest
     gflags
     glog
     snappy
