99b3e11f660bf9b18d6e84ab4a9d003f64e24866
Tongliang Liao <xkszltl@gmail.com>
2018-04-20 12:03:00 +0800

    Use master branch of eigen for CUDA 9.1.

--- pkgs/eigen.sh
+++ pkgs/eigen.sh
@@ -5,8 +5,11 @@
 [ -e $STAGE/eigen ] && ( set -xe
     cd $SCRATCH
 
-    until git clone --depth 1 --single-branch -b "$(git ls-remote --tags "$GIT_MIRROR/eigenteam/eigen-git-mirror.git" | sed -n 's/.*[[:space:]]refs\/tags\/\([0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1)" "$GIT_MIRROR/eigenteam/eigen-git-mirror.git" "eigen"; do echo 'Retrying'; done
+    # until git clone --depth 1 --single-branch -b "$(git ls-remote --tags "$GIT_MIRROR/eigenteam/eigen-git-mirror.git" | sed -n 's/.*[[:space:]]refs\/tags\/\([0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1)" "$GIT_MIRROR/eigenteam/eigen-git-mirror.git" "eigen"; do echo 'Retrying'; done
+    # Release 3.3.4 is not compatible with CUDA 9.1.
+    until git clone --depth 1 --single-branch -b "master" "$GIT_MIRROR/eigenteam/eigen-git-mirror.git" "eigen"; do echo 'Retrying'; done
     cd eigen
+    git tag "$(git ls-remote --tags "$GIT_MIRROR/eigenteam/eigen-git-mirror.git" | sed -n 's/.*[[:space:]]refs\/tags\/\([0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1).1"
 
     # ------------------------------------------------------------
 
