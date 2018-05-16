28e44ab03d2ed3edc115c8528acf94fde8c630e5
Tongliang Liao <xkszltl@gmail.com>
2018-05-17 03:11:52 +0800

    Update rpm blacklist.

--- pkgs/env-pkg.sh
+++ pkgs/env-pkg.sh
@@ -9,15 +9,22 @@ export RPM_MAX_ATTEMPT=10
 #       The correct choice is x86_64-redhat-linux instead of x86_64-linux-gnu.
 #
 #       libasan2 seems to contain libasan.so.3.0.0 and causing a conflict with libasan3
+#
+#       python-lexicon conflict with python2-lexicon
+#       file /usr/lib/python2.7/site-packages/test/__init__.pyc conflicts between attempted installs of python2-ansible-runner-1.0.1-1.el7.noarch and python2-neomodel-3.2.8-1.el7.noarch
+#       
+#       Python issues from CentOS 7.5:
+#           python2-azure-sdk conflict with python-azure-sdk
+#           python2-boto3 conflict with python-boto3
+#           python2-s3transfer conflict with python-s3transfer
 export RPM_BLACKLIST=$(echo "
     *-debuginfo
     gcc-x86_64-linux-gnu
     libasan2
     python-lexicon
-    python-qpid-common
+    python2-ansible-runner
     python2-azure-sdk
     python2-boto3
-    python2-paramiko
     python2-s3transfer
 " | sed -n 's/^[[:space:]]*\([^[:space:]][^[:space:]]*\).*/--exclude \1/p' | paste -s - | xargs)
 
