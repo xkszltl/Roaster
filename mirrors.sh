3b7daeba717b79dee1fb8e12322f85b4ce70a3c0
Tongliang Liao <xkszltl@gmail.com>
2018-06-07 17:46:02 +0800

    Adding LFS support.

--- mirrors.sh
+++ mirrors.sh
@@ -14,7 +14,7 @@ cd $_
 
 [ $# -ge 1 ] && export PATTERN=$1
 
-parallel -j 10 --line-buffer --bar 'bash -c '"'"'
+parallel --bar --group -j 10 'bash -c '"'"'
 set -e
 [ $(xargs -n1 <<<{} | wc -l) -ne 2 ] && exit 0
 export SRC_SITE=$(xargs -n1 <<<{} 2>/dev/null | head -n1)
@@ -24,19 +24,30 @@ export DST_SITE=git@git.codingcafe.org:Mirrors/
 export DST_DIR=$SRC_DIR
 export DST=$DST_SITE$DST_DIR.git
 export LOCAL=$(pwd)/$DST_DIR.git
-if [ ! $PATTERN ] || grep $PATTERN <<<$SRC_DIR; then
+
+echo "[\"$DST_DIR\"]"
+
+if [ ! '"$PATTERN"' ] || grep '"$PATTERN"' <<<$SRC_DIR; then
     mkdir -p $(dirname $LOCAL)
     cd $(dirname $LOCAL)
-    [ -d $LOCAL ] || git lfs clone --mirror $SRC "$LOCAL" || git lfs clone --mirror "$SRC" "$LOCAL"
+    [ -d $LOCAL ] || git clone --mirror $SRC "$LOCAL" 2>&1 || git clone --mirror "$SRC" "$LOCAL" 2>&1
     cd $LOCAL
-    git remote set-url origin $DST
-    git lfs fetch --all || true
-    git remote set-url origin $SRC
-    git lfs fetch --prune --all
-    git gc --auto
-    git remote set-url origin $DST
-    git push --mirror
-    git lfs push --all origin
+    git remote set-url origin $DST 2>&1
+    git fetch --all 2>&1 || true
+    if [ $(git lfs ls-files | wc -l) -gt 0 ]; then
+        git lfs fetch --all 2>&1 || true
+    fi
+    git remote set-url origin $SRC 2>&1
+    git fetch --prune --all 2>&1
+    if [ $(git lfs ls-files | wc -l) -gt 0 ]; then
+        git lfs fetch --prune --all 2>&1
+    fi
+    git gc --auto 2>&1
+    git remote set-url origin $DST 2>&1
+    if [ $(git lfs ls-files | wc -l) -gt 0 ]; then
+        git lfs push --all origin 2>&1 || true
+    fi
+    git push --mirror origin 2>&1
 fi
 '"'" ::: {\
 https://github.com/\ {\
