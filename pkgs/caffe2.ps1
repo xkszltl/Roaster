a2afe07d89bfc065e21b340dff8a1edfc10f5c3f
Tongliang Liao <tolia@microsoft.com>
2018-06-27 13:46:59 -0700

    Comment out "ignore env|ENV" from python gitignore. Trying to add MKL vars to Caffe2 build.

--- pkgs/caffe2.ps1
+++ pkgs/caffe2.ps1
@@ -35,6 +35,12 @@ git pull patch cmake-public
 mkdir build
 pushd build
 
+# Copy MKL's environment variables from ".bat" file to PowerShell.
+Invoke-Expression $($(cmd /C "`"${Env:ProgramFiles(x86)}/IntelSWTools/compilers_and_libraries/windows/mkl/bin/mklvars.bat`" intel64 vs2015 & env") -Match '^MKL(_|ROOT)' -Replace '^','${Env:' -Replace '=','}="' -Replace '$','"' | Out-String)
+Invoke-Expression $($(cmd /C "`"${Env:ProgramFiles(x86)}/IntelSWTools/compilers_and_libraries/windows/mkl/bin/mklvars.bat`" intel64 vs2015 & env") -Match '^LIB' -Replace '^','${Env:' -Replace '=','}="' -Replace '$','"' | Out-String)
+Invoke-Expression $($(cmd /C "`"${Env:ProgramFiles(x86)}/IntelSWTools/compilers_and_libraries/windows/mkl/bin/mklvars.bat`" intel64 vs2015 & env") -Match '^CPATH' -Replace '^','${Env:' -Replace '=','}="' -Replace '$','"' | Out-String)
+Invoke-Expression $($(cmd /C "`"${Env:ProgramFiles(x86)}/IntelSWTools/compilers_and_libraries/windows/mkl/bin/mklvars.bat`" intel64 vs2015 & env") -Match '^INCLUDE' -Replace '^','${Env:' -Replace '=','}="' -Replace '$','"' | Out-String)
+
 $gtest_silent_warning="/D_SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS /D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING /w"
 $gflags_dll="/DGFLAGS_IS_A_DLL=1"
 $protobuf_dll="/DPROTOBUF_USE_DLLS"
---
+++ pkgs/env/mirror.ps1
@@ -0,0 +1,5 @@
+#Requires -RunAsAdministrator
+
+$Env:GIT_MIRROR_GITHUB="https://github.com"
+$Env:GIT_MIRROR_GITLAB="https://gitlab.com"
+$Env:GIT_MIRROR=${Env:GIT_MIRROR_GITHUB}
