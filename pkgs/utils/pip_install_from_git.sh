07591d5a267f4a688ce9ff0df18ad319b6188c95
Tongliang Liao <xkszltl@gmail.com>
2018-08-17 15:16:30 +0800

    Add wheel.

--- pkgs/utils/pip_install_from_git.sh
+++ pkgs/utils/pip_install_from_git.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
-sudo pip3 install -U setuptools
+sudo
 
-for i in pypa/pip, $@; do
+sudo pip install -U setuptools
+
+for i in pypa/{pip,wheel}, $@; do
     REPO="$(cut -d, -f1 <<< "$i")"
     TAG="$(cut -d, -f2 <<< "$i")"
-    sudo pip3 install -U "git+$GIT_MIRROR/$REPO.git@$(git ls-remote --tags "$GIT_MIRROR/$REPO.git" | sed -n 's/.*[[:space:]]refs\/tags\/\('"$TAG"'[0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1)"
+    sudo pip install -U "git+$GIT_MIRROR/$REPO.git@$(git ls-remote --tags "$GIT_MIRROR/$REPO.git" | sed -n 's/.*[[:space:]]refs\/tags\/\('"$TAG"'[0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1)"
 done
