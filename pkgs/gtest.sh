3af5410bcb47f3290a6dec030d4fca770a8883d4
Tongliang Liao <xkszltl@gmail.com>
2018-08-27 09:34:23 +0800

    Bugfix: missing gtest directory name.

--- pkgs/gtest.sh
+++ pkgs/gtest.sh
@@ -8,7 +8,7 @@
     # ------------------------------------------------------------
 
     . "$ROOT_DIR/pkgs/utils/git/version.sh" google/googletest,release-
-    until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
+    until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO" gtest; do echo 'Retrying'; done
     cd gtest
 
     # ------------------------------------------------------------
