87459c30157ff6820ca22384b75fecdafc7e50a4
Tongliang Liao <xkszltl@gmail.com>
2018-08-27 16:44:58 +0800

    Lower-case package name.

--- pkgs/utils/fpm/install_from_git.sh
+++ pkgs/utils/fpm/install_from_git.sh
@@ -10,19 +10,19 @@ set -xe
 
 sudo -v
 
-time fpm                                                        \
-    --after-install "$ROOT_DIR/pkgs/utils/fpm/post_install.sh"  \
-    --after-remove "$ROOT_DIR/pkgs/utils/fpm/post_install.sh"   \
-    --chdir "$INSTALL_ROOT"                                     \
-    --exclude-file "$INSTALL_ROOT/../exclude.conf"              \
-    --input-type dir                                            \
-    --iteration "$(git log -n1 --format="%h")"                  \
-    --name "codingcafe-$(basename $(pwd))"                      \
-    --output-type rpm                                           \
-    --package "$INSTALL_ROOT/.."                                \
-    --rpm-compression "$(false && echo xzmt || echo none)"      \
-    --rpm-digest sha512                                         \
-    --vendor "CodingCafe"                                       \
+time fpm                                                                \
+    --after-install "$ROOT_DIR/pkgs/utils/fpm/post_install.sh"          \
+    --after-remove "$ROOT_DIR/pkgs/utils/fpm/post_install.sh"           \
+    --chdir "$INSTALL_ROOT"                                             \
+    --exclude-file "$INSTALL_ROOT/../exclude.conf"                      \
+    --input-type dir                                                    \
+    --iteration "$(git log -n1 --format="%h")"                          \
+    --name "codingcafe-$(basename $(pwd) | tr '[:upper:]' '[:lower:]')" \
+    --output-type rpm                                                   \
+    --package "$INSTALL_ROOT/.."                                        \
+    --rpm-compression "$(false && echo xzmt || echo none)"              \
+    --rpm-digest sha512                                                 \
+    --vendor "CodingCafe"                                               \
     --version "$((git describe --tags || echo 0.0) | sed 's/[_\-]/\./g' | sed 's/[^0-9\.]//g')"
 
 "$ROOT_DIR/pkgs/utils/fpm/install.sh"
