74abe0412dea9f7e4a018f391cf5dd7a51ae7498
Tongliang Liao <xkszltl@gmail.com>
2018-08-27 18:19:24 +0800

    Add resume functionality for docker build.

--- gitlab-ci/build_stage.sh
+++ gitlab-ci/build_stage.sh
@@ -9,18 +9,38 @@ if [ $GITLAB_CI ]; then
         $CI_REGISTRY                    \
     <<< "$CI_REGISTRY_PASSWORD"
 
-	if [ $(echo $CI_COMMIT_REF_NAME | sed 's/^[^\-]*-//') = $CI_JOB_STAGE ]; then
-		cat stage/$CI_JOB_STAGE
-	else
-		grep -v '"/etc/codingcafe/scripts"' stage/$CI_JOB_STAGE
-	fi > Dockerfile
+    cmd="$(sed 's/-.*//' <<< "$CI_COMMIT_REF_NAME")";
+    stage="$(sed 's/^[^\-]*-//' <<< "$CI_COMMIT_REF_NAME")";
+
+	if [ "_$cmd" = "_build" ]; then
+        echo "Build stage \"$CI_JOB_STAGE\"."
+		cat stage/$CI_JOB_STAGE > 'Dockerfile'
+    elif [ "_$cmd" = "_resume" ]; then
+        echo "Resume stage \"$CI_JOB_STAGE\"."
+        cat stage/resume > 'Dockerfile'
+    else
+        echo "Unknown command \"$cmd\"."
+	fi
+
+    cat 'Dockerfile' > "stage/$CI_JOB_STAGE"
+
+	if [ "_$stage" = "_$CI_JOB_STAGE" ]; then
+		cat "stage/$CI_JOB_STAGE" > 'Dockerfile'
+    else
+		grep -v '"/etc/codingcafe/scripts"' "stage/$CI_JOB_STAGE" > 'Dockerfile'
+	fi
+
 	time docker build                                   \
         --cpu-shares 128                                \
         --no-cache                                      \
         --pull                                          \
         --squash                                        \
-        --tag $CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE    \
+        --tag "$CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE"  \
         .
+    if [ "$?" -ne 0 ]; then
+        echo 'Docker build failed. Save breakpoint snapshot.'
+        time docker commit "$(docker ps -alq)" "$CI_REGISTRY_IMAGE:breakpoint"
+    fi
 
     # time docker push $CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE
 fi
