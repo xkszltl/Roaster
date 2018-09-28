068dad95eb5be4376f8bf6392d44bddf22f5e084
Tongliang Liao <xkszltl@gmail.com>
2018-09-28 17:31:11 +0800

    Change org in protobuf script.

--- pkgs/protobuf.sh
+++ pkgs/protobuf.sh
@@ -7,7 +7,7 @@
 
     # ------------------------------------------------------------
 
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" google/protobuf,v
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" protocolbuffers/protobuf,v
     until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd protobuf
 
