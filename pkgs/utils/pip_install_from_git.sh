c75b9a55db60ff5b153109d069227683cc5e6ad0
Tongliang Liao <xkszltl@gmail.com>
2018-12-13 18:50:43 +0800

    Support using pip for local repo.

--- pkgs/utils/pip_install_from_git.sh
+++ pkgs/utils/pip_install_from_git.sh
@@ -11,24 +11,36 @@ if [ ! "$ROOT_DIR" ]; then
     echo 'Set $ROOT_DIR to "'"$ROOT_DIR"'".'
 fi
 
+CACHE_VALID=false
+
 for i in pypa/setuptools,v pypa/{pip,wheel} $@; do
-    PKG="$(basename "$(cut -d, -f1 <<< "$i,")")"
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" "$i"
-    URL="git+$GIT_REPO@$GIT_TAG"
-    if grep 'pypa/setuptools' <<< "$i" > /dev/null; then
+    PKG_PATH="$(cut -d, -f1 <<< "$i,")"
+    if grep '^[[:alnum:]]' <<< "$PKG_PATH" > /dev/null; then
+        . "$ROOT_DIR/pkgs/utils/git/version.sh" "$i"
+        URL="git+$GIT_REPO@$GIT_TAG"
+    else
+        URL="$(readlink -e $PKG_PATH)"
+        [ -d "$URL" ]
+        USE_LOCAL_GIT=true
+    fi
+    PKG="$(basename "$PKG_PATH")"
+    if grep '/setuptools' <<< "/$i" > /dev/null; then
         echo "Cannot build $PKG from source. Install it from wheel instead."
         URL="$PKG"
     fi
-    if grep '/protobuf' <<< "$i" > /dev/null; then
+    if grep '/protobuf' <<< "/$i" > /dev/null; then
         echo "Cannot build $PKG from source. Install it from wheel instead."
         URL="$PKG"
     fi
     for py in $(which python{,3}); do
         # Not exactly correct since the actual package name is defined by "setup.py".
-        if [ "_$("$py" -m pip freeze --all | tr '[:upper:]' '[:lower:]' | sed -n "s/^$(tr '[:upper:]' '[:lower:]' <<< "$PKG")==//p")" = "_$GIT_TAG_VER" ]; then
+        "$CACHE_VALID" || CACHED_LIST="$("$py" -m pip freeze --all | tr '[:upper:]' '[:lower:]')"
+        CACHE_VALID=true
+        if [ ! "$USE_LOCAL_GIT" ] && [ "_$(sed -n "s/^$(tr '[:upper:]' '[:lower:]' <<< "$PKG")==//p" <<< "$CACHED_LIST")" = "_$GIT_TAG_VER" ]; then
             echo "Package \"$PKG\" for \"$py\" is already up-to-date ($GIT_TAG_VER). Skip."
             continue
         fi
         sudo "$py" -m pip install -U "$URL" || sudo "$py" -m pip install -IU "$URL"
+        CACHE_VALID=false
     done
 done
