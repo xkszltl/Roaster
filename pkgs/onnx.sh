c6f4d60a16677c0e01eef5f09286e937e4edb9a6
Tongliang Liao <xkszltl@gmail.com>
2018-12-13 18:51:07 +0800

    Build ONNX pip package locally, with shared libraries.

--- pkgs/onnx.sh
+++ pkgs/onnx.sh
@@ -5,22 +5,25 @@
 [ -e $STAGE/onnx ] && ( set -xe
     cd $SCRATCH
 
-    "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" cython/cython numpy/numpy,v protocolbuffers/protobuf,v benjaminp/six onnx/onnx,master
+    "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" cython/cython numpy/numpy,v protocolbuffers/protobuf,v benjaminp/six
     "$ROOT_DIR/pkgs/utils/pip_install_from_wheel.sh" future
 
     # ------------------------------------------------------------
 
     . "$ROOT_DIR/pkgs/utils/git/version.sh" onnx/onnx,master
-    until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
+    until git clone -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
 
     cd onnx
 
     . "$ROOT_DIR/pkgs/utils/git/submodule.sh"
 
-    pushd third_party
-    rm -rf pybind11
-    cp -rf /usr/local/src/pybind11 pybind11
+    pushd third_party/pybind11
+    git checkout master
+    # rm -rf pybind11
+    # cp -rf /usr/local/src/pybind11 pybind11
     popd
+
+    git commit -am "Update submodule \"pybind11\"."
 
     # ------------------------------------------------------------
 
@@ -33,12 +36,22 @@
 
         . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
 
+        CMAKE_ARGS="
+            -DBUILD_SHARED_LIBS=ON
+            -DCMAKE_BUILD_TYPE=Release
+            -DCMAKE_C_COMPILER=gcc
+            -DCMAKE_CXX_COMPILER=g++
+            -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache
+            -DONNX_GEN_PB_TYPE_STUBS=ON
+        " "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" .
+
         mkdir -p build
         cd $_
 
         cmake                                       \
             -DBENCHMARK_ENABLE_LTO=ON               \
             -DBUILD_ONNX_PYTHON=ON                  \
+            -DBUILD_SHARED_LIBS=ON                  \
             -DCMAKE_BUILD_TYPE=Release              \
             -DCMAKE_C_COMPILER=gcc                  \
             -DCMAKE_CXX_COMPILER=g++                \
