f236278f6e3e03364df10a9fc322ae99ecb7e0dc
Tongliang Liao <tolia@microsoft.com>
2018-12-27 00:29:54 -0800

    Add /GL /LTCG:inc for protobuf.

--- pkgs/protobuf.ps1
+++ pkgs/protobuf.ps1
@@ -15,68 +15,39 @@ if (Test-Path "$root")
     Exit 1
 }
 
-# ================================================================================
-# Known issues:
-# - Release 3.5.2 has compatibility issue with CUDA 9.2, patched.
-# - Release 3.6.1 has the same and one more harder-to-patch issue about:
-#     Initialize "static constexpr" variable in-place.
-# - Master only has the second issue.
-# ================================================================================
 $latest_ver='v' + $($(git ls-remote --tags "$repo") -match '.*refs/tags/v[0-9\.]*$' -replace '.*refs/tags/v','' | sort {[Version]$_})[-1]
-$latest_ver='v' + $($(git ls-remote --tags "$repo") -match '.*refs/tags/v3.5[0-9\.]*$' -replace '.*refs/tags/v','' | sort {[Version]$_})[-1]
-# $latest_ver="master"
 git clone --depth 1 --recursive --single-branch -b "$latest_ver" -j8 "$repo"
 pushd "$root"
 
-$repo_gtest="${Env:GIT_MIRROR}/google/googletest.git"
-$latest_ver_gtest='release-' + $($(git ls-remote --tags "$repo_gtest") -match '.*refs/tags/release-[0-9\.]*$' -replace '.*refs/tags/release-','' | sort {[Version]$_})[-1]
-rm -Force -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue "$root/gmock"
-if (Test-Path "$root/gmock")
-{
-    echo "Failed to remove `"$root/gmock`""
-    Exit 1
-}
-
-git clone --depth 1 --recursive --single-branch -b "$latest_ver_gtest" -j8 "$repo_gtest" "$root/gmock"
-mv -Force "$root/gmock/googlemock/*" "$root/gmock/"
-mv -Force "$root/gmock/googletest" "$root/gmock/gtest"
-
-# ================================================================================
-# Dirty temporary patches:
-# - Fix DLL symbols of nested class.
-# ================================================================================
-pushd src/google/protobuf
-echo "Patch for `"${latest_ver}`" about DLL."
-$(cat io/gzip_stream.h) -Replace '(struct )(Options)','${1}LIBPROTOBUF_EXPORT ${2}' > io/.gzip_stream.h
-mv -Force io/.gzip_stream.h io/gzip_stream.h
-popd
-git diff
-
+# Repo contains file "BUILD" for Bazel and it will conflict with "build" on NTFS.
 mkdir build-win
 pushd build-win
 
 # - Avoid DLLs due symbol export issues with inline functions.
 #   Keep happening and no good fix/test coverage from Google so far.
-cmake                                   `
-    -DBUILD_SHARED_LIBS=ON              `
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo   `
-    -DCMAKE_C_FLAGS="/MP"               `
-    -DCMAKE_CXX_FLAGS="/MP"             `
-    -DCMAKE_EXE_LINKER_FLAGS="/LTCG"    `
-    -DCMAKE_SHARED_LINKER_FLAGS="/LTCG" `
-    -DCMAKE_STATIC_LINKER_FLAGS="/LTCG" `
-    -Dprotobuf_BUILD_EXAMPLES=ON        `
-    -Dprotobuf_BUILD_SHARED_LIBS=ON     `
-    -Dprotobuf_INSTALL_EXAMPLES=ON      `
-    -G"Visual Studio 15 2017 Win64"     `
+cmake                                                               `
+    -DBUILD_SHARED_LIBS=ON                                          `
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo                               `
+    -DCMAKE_C_FLAGS="/GL /MP"                                       `
+    -DCMAKE_CXX_FLAGS="/EHsc /GL /MP"                               `
+    -DCMAKE_EXE_LINKER_FLAGS="/LTCG:incremental"                    `
+    -DCMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO="/INCREMENTAL:NO"       `
+    -DCMAKE_GENERATOR_PLATFORM=x64                                  `
+    -DCMAKE_SHARED_LINKER_FLAGS="/LTCG:incremental"                 `
+    -DCMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO="/INCREMENTAL:NO"    `
+    -DCMAKE_STATIC_LINKER_FLAGS="/LTCG:incremental"                 `
+    -Dprotobuf_BUILD_EXAMPLES=ON                                    `
+    -Dprotobuf_BUILD_SHARED_LIBS=ON                                 `
+    -Dprotobuf_INSTALL_EXAMPLES=ON                                  `
+    -G"Visual Studio 15 2017"                                       `
     ../cmake
 
 # Multi-process build is not ready.
 # Conflict (permission denied) while multiple protoc generating the same file.
-cmake --build . --config RelWithDebInfo
+cmake --build . --config RelWithDebInfo -- -maxcpucount
 
 $ErrorActionPreference="SilentlyContinue"
-cmake --build . --config RelWithDebInfo --target check
+cmake --build . --config RelWithDebInfo --target check -- -maxcpucount
 if (-Not $?)
 {
     echo "Check failed but we temporarily bypass it."
@@ -86,6 +57,7 @@ $ErrorActionPreference="Stop"
 rm -Force -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue "${Env:ProgramFiles}/protobuf"
 cmake --build . --config RelWithDebInfo --target install
 Get-ChildItem "${Env:ProgramFiles}/protobuf" -Filter *.dll -Recurse | Foreach-Object { New-Item -Force -ItemType SymbolicLink -Path "${Env:SystemRoot}\System32\$_" -Value $_.FullName }
+Get-ChildItem "${Env:ProgramFiles}/protobuf" -Filter *.exe -Recurse | Foreach-Object { New-Item -Force -ItemType SymbolicLink -Path "${Env:SystemRoot}\System32\$_" -Value $_.FullName }
 
 popd
 popd
