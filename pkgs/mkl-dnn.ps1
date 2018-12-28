893c7db3ef591af00a447b23caf57da45c02b17d
Tongliang Liao <tolia@microsoft.com>
2018-12-27 19:38:28 -0800

    Update mkl-dnn build flags and install system-wide.

--- pkgs/mkl-dnn.ps1
+++ pkgs/mkl-dnn.ps1
@@ -29,23 +29,28 @@ Invoke-Expression $($(cmd /C "`"${Env:ProgramFiles(x86)}/IntelSWTools/compilers_
 mkdir build
 pushd build
 
-cmake                                   `
-    -DCMAKE_BUILD_TYPE=Release          `
-    -DCMAKE_C_FLAGS="/MP /Zi"           `
-    -DCMAKE_CXX_FLAGS="/EHsc /MP /Zi"   `
-    -DCMAKE_EXE_LINKER_FLAGS="/LTCG"    `
-    -DCMAKE_SHARED_LINKER_FLAGS="/LTCG" `
-    -DCMAKE_STATIC_LINKER_FLAGS="/LTCG" `
-    -G"Visual Studio 15 2017 Win64"     `
-    -T"host=x64"                        `
+cmake                                                               `
+    -A x64                                                          `
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo                               `
+    -DCMAKE_C_FLAGS="/GL /MP"                                       `
+    -DCMAKE_CXX_FLAGS="/EHsc /GL /MP"                               `
+    -DCMAKE_EXE_LINKER_FLAGS="/LTCG:incremental"                    `
+    -DCMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO="/INCREMENTAL:NO"       `
+    -DCMAKE_INSTALL_PREFIX="${Env:ProgramFiles}/Intel(R) MKL-DNN"   `
+    -DCMAKE_SHARED_LINKER_FLAGS="/LTCG:incremental"                 `
+    -DCMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO="/INCREMENTAL:NO"    `
+    -DCMAKE_STATIC_LINKER_FLAGS="/LTCG:incremental"                 `
+    -G"Visual Studio 15 2017"                                       `
+    -T"host=x64"                                                    `
     ..
 
 cmake --build . --config Release -- -maxcpucount
 
-cmake --build . --config Release --target run_tests -- -maxcpucount
+# cmake --build . --config Release --target run_tests -- -maxcpucount
 
 rm -Force -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue "${Env:ProgramFiles}/Intel(R) MKL-DNN"
 cmake --build . --config Release --target install -- -maxcpucount
+Get-ChildItem "${Env:ProgramFiles}/Intel(R) MKL-DNN" -Filter '*.dll' -Recurse | Foreach-Object { New-Item -Force -ItemType SymbolicLink -Path "${Env:SystemRoot}\System32\$_" -Value $_.FullName }
 
 popd
 popd
