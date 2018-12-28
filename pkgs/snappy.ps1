b9363a48f6e98e540cf6cdc437396187ebc7906e
Tongliang Liao <tolia@microsoft.com>
2018-12-27 23:27:15 -0800

    Update Snappy.

--- pkgs/snappy.ps1
+++ pkgs/snappy.ps1
@@ -21,15 +21,20 @@ pushd "$root"
 mkdir build
 pushd build
 
-cmake                                   `
-    -DBUILD_SHARED_LIBS=ON              `
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo   `
-    -DCMAKE_C_FLAGS="/MP"               `
-    -DCMAKE_CXX_FLAGS="/EHsc /MP"       `
-    -DCMAKE_EXE_LINKER_FLAGS="/LTCG"    `
-    -DCMAKE_SHARED_LINKER_FLAGS="/LTCG" `
-    -DCMAKE_STATIC_LINKER_FLAGS="/LTCG" `
-    -G"Visual Studio 15 2017 Win64"     `
+cmake                                                               `
+    -A x64                                                          `
+    -DBUILD_SHARED_LIBS=ON                                          `
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo                               `
+    -DCMAKE_C_FLAGS="/MP"                                           `
+    -DCMAKE_CXX_FLAGS="/EHsc /MP"                                   `
+    -DCMAKE_EXE_LINKER_FLAGS="/LTCG:incremental"                    `
+    -DCMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO="/INCREMENTAL:NO"       `
+    -DCMAKE_INSTALL_PREFIX="${Env:ProgramFiles}/Snappy"             `
+    -DCMAKE_SHARED_LINKER_FLAGS="/LTCG:incremental"                 `
+    -DCMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO="/INCREMENTAL:NO"    `
+    -DCMAKE_STATIC_LINKER_FLAGS="/LTCG:incremental"                 `
+    -G"Visual Studio 15 2017"                                       `
+    -T"host=x64"                                                    `
     ..
 
 cmake --build . --config RelWithDebInfo -- -maxcpucount
