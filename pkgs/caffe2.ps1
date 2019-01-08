8fdb7e6b2129798be3332865705eee032755c802
Tongliang Liao <tolia@microsoft.com>
2019-01-08 03:14:08 -0800

    Use AVX2 for Caffe2.

--- pkgs/caffe2.ps1
+++ pkgs/caffe2.ps1
@@ -77,9 +77,9 @@ cmake                                                                           
     -DBUILD_SHARED_LIBS=ON                                                          `
     -DBUILD_TEST=ON                                                                 `
     -DCMAKE_BUILD_TYPE=Release                                                      `
-    -DCMAKE_C_FLAGS="/FS /GL /MP /Z7 ${dep_dll}"                                    `
+    -DCMAKE_C_FLAGS="/FS /GL /MP /Z7 /arch:AVX2 ${dep_dll}"                                    `
     -DCMAKE_CUDA_SEPARABLE_COMPILATION=ON                                           `
-    -DCMAKE_CXX_FLAGS="/EHsc /FS /GL /MP /Z7 ${dep_dll} ${gtest_silent_warning}"    `
+    -DCMAKE_CXX_FLAGS="/EHsc /FS /GL /MP /Z7 /arch:AVX2 ${dep_dll} ${gtest_silent_warning}"    `
     -DCMAKE_EXE_LINKER_FLAGS="/LTCG:incremental"                                    `
     -DCMAKE_INSTALL_PREFIX="${Env:ProgramFiles}/Caffe2"                             `
     -DCMAKE_SHARED_LINKER_FLAGS="/LTCG:incremental"                                 `
