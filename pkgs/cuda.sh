6385f4fa4b7dd471b14375476e87d3ead943f2dd
Tongliang Liao <xkszltl@gmail.com>
2019-03-20 20:58:56 +0800

    Install TensorRT from nested repo.

--- pkgs/cuda.sh
+++ pkgs/cuda.sh
@@ -11,6 +11,23 @@
     export CUDA_VER_MINOR="$(cut -d'.' -f2 <<< "$CUDA_VER")"
     export CUDA_VER_BUILD="$(cut -d'.' -f3 <<< "$CUDA_VER")"
 
+    # TensorRT
+    REPO_URL='https://repo.codingcafe.org/nvidia/tensorrt'
+    TRT_URL="$(curl -sSL "$REPO_URL"                \
+        | sed -n 's/^.*href="\([^"]*\)".*$/\1/p'    \
+        | grep 'rhel7'                              \
+        | grep 'repo'                               \
+        | grep '\.rpm$'                             \
+        | sort -V                                   \
+        | tail -n1)"
+    [ "$TRT_URL" ]
+    TRT_URL="$REPO_URL/$TRT_URL"
+    sudo yum remove -y --skip-broken 'nv-tensorrt-repo-*' || true
+    $RPM_INSTALL "$TRT_URL"
+    $RPM_INSTALL "tensorrt" || $RPM_UPDATE "tensorrt" || $RPM_REINSTALL "tensorrt"
+    $IS_CONTAINER && sudo yum remove -y --skip-broken 'nv-tensorrt-repo-*'
+
+    # CUDA Samples
     cd /usr/local/cuda/samples
     . scl_source enable devtoolset-6 || true
     export MPI_HOME=/usr/local/openmpi
