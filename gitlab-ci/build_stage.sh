e8f9b857fe188b3bf59e80e6abd82d593769abc7
Tongliang Liao <xkszltl@gmail.com>
2019-04-18 16:45:41 +0800

    Allow non-GitLab CI.

--- gitlab-ci/build_stage.sh
+++ gitlab-ci/build_stage.sh
@@ -2,44 +2,51 @@
 
 set -xe
 
-if [ $GITLAB_CI ]; then
-	docker login                        \
+# [ "$GITLAB_CI" ]
+[ "$CI_COMMIT_REF_NAME" ]
+[ "$CI_JOB_STAGE" ]
+[ "$CI_REGISTRY_IMAGE" ]
+
+set +x
+if [ "$CI_REGISTRY" ] && [ "$CI_REGISTRY_USER" ] && [ "$CI_REGISTRY_PASSWORD" ]; then
+    docker login                        \
         --password-stdin                \
-        --username $CI_REGISTRY_USER    \
-        $CI_REGISTRY                    \
+        --username "$CI_REGISTRY_USER"  \
+        "$CI_REGISTRY"                  \
     <<< "$CI_REGISTRY_PASSWORD"
+fi
+set -x
 
-    cmd="$(sed 's/-.*//' <<< "$CI_COMMIT_REF_NAME")";
-    stage="$(sed 's/^[^\-]*-//' <<< "$CI_COMMIT_REF_NAME")";
+cmd="$(sed 's/-.*//' <<< "$CI_COMMIT_REF_NAME")";
+stage="$(sed 's/^[^\-]*-//' <<< "$CI_COMMIT_REF_NAME")";
 
-    if [ "_$cmd" = "_resume" ] && [ "_$stage" == "_$CI_JOB_STAGE" ]; then
-        echo "Resume stage \"$CI_JOB_STAGE\"."
-        cat stage/resume > 'Dockerfile'
-    else
-        echo "Build stage \"$CI_JOB_STAGE\"."
-		cat stage/$CI_JOB_STAGE > 'Dockerfile'
-	fi
+if [ "_$cmd" = "_resume" ] && [ "_$stage" == "_$CI_JOB_STAGE" ]; then
+    echo "Resume stage \"$CI_JOB_STAGE\"."
+    cat stage/resume > 'Dockerfile'
+else
+    echo "Build stage \"$CI_JOB_STAGE\"."
+    cat stage/$CI_JOB_STAGE > 'Dockerfile'
+fi
 
-    sed -i "s/^FROM docker\.codingcafe\.org\/.*:/FROM $(sed 's/\([\\\/\.\-]\)/\\\1/g' <<< "$CI_REGISTRY_IMAGE"):/" 'Dockerfile'
-    cat 'Dockerfile' > "stage/$CI_JOB_STAGE"
+sed -i "s/^FROM docker\.codingcafe\.org\/.*:/FROM $(sed 's/\([\\\/\.\-]\)/\\\1/g' <<< "$CI_REGISTRY_IMAGE"):/" 'Dockerfile'
+cat 'Dockerfile' > "stage/$CI_JOB_STAGE"
 
-	if [ "_$stage" = "_$CI_JOB_STAGE" ]; then
-		cat "stage/$CI_JOB_STAGE" > 'Dockerfile'
-    else
-		grep -v '"/etc/codingcafe/scripts"' "stage/$CI_JOB_STAGE" > 'Dockerfile'
-	fi
+if [ "_$stage" = "_$CI_JOB_STAGE" ]; then
+    cat "stage/$CI_JOB_STAGE" > 'Dockerfile'
+else
+    grep -v '"/etc/codingcafe/scripts"' "stage/$CI_JOB_STAGE" > 'Dockerfile'
+fi
 
-	if time docker build                                \
-        --cpu-shares 128                                \
-        --no-cache                                      \
-        --pull                                          \
-        --tag "$CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE"  \
-        .; then
-        time docker push "$CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE"
-    else
-        echo 'Docker build failed. Save breakpoint snapshot.' 1>&2
-        time docker commit "$(docker ps -alq)" "$CI_REGISTRY_IMAGE:breakpoint"
-        time docker push "$_"
-        exit 1
-    fi
+if time docker build                                \
+    --cpu-shares 128                                \
+    --no-cache                                      \
+    --pull                                          \
+    --tag "$CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE"  \
+    .; then
+    time docker push "$CI_REGISTRY_IMAGE:stage-$CI_JOB_STAGE"
+else
+    echo 'Docker build failed. Save breakpoint snapshot.' 1>&2
+    time docker commit "$(docker ps -alq)" "$CI_REGISTRY_IMAGE:breakpoint"
+    time docker push "$_"
+    exit 1
 fi
