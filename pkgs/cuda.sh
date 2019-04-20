016c18b4fbb354b18c976580180e4926979de3f7
Tongliang Liao <xkszltl@gmail.com>
2019-04-19 17:52:37 -0700

    Download TensorRT outside of CodingCafe with hard-coded path.

--- pkgs/cuda.sh
+++ pkgs/cuda.sh
@@ -11,28 +11,51 @@
     export CUDA_VER_MINOR="$(cut -d'.' -f2 <<< "$CUDA_VER")"
     export CUDA_VER_BUILD="$(cut -d'.' -f3 <<< "$CUDA_VER")"
 
+    # ============================================================
     # TensorRT
-    REPO_URL='https://repo.codingcafe.org/nvidia/tensorrt'
-    TRT_URL="$(curl -sSL "$REPO_URL"                \
-        | sed -n 's/^.*href="\([^"]*\)".*$/\1/p'    \
-        | grep 'rhel7'                              \
-        | grep 'repo'                               \
-        | grep '\.rpm$'                             \
-        | sort -V                                   \
-        | tail -n1)"
-    [ "$TRT_URL" ]
-    TRT_URL="$REPO_URL/$TRT_URL"
-    sudo yum remove -y --skip-broken 'nv-tensorrt-repo-*' || true
-    $RPM_INSTALL "$TRT_URL"
-    $RPM_INSTALL "tensorrt" || $RPM_UPDATE "tensorrt" || $RPM_REINSTALL "tensorrt"
-    $IS_CONTAINER && sudo yum remove -y --skip-broken 'nv-tensorrt-repo-*'
+    #     TODO: Detect newest version outside of CodingCafe.
+    # ============================================================
+    (
+        set -e
 
+        TRT_REPO_URL='https://developer.nvidia.com/compute/machine-learning/tensorrt/5.1/rc/local_repos'
+        TRT_FILENAME="nv-tensorrt-repo-rhel7-cuda$CUDA_VER_MAJOR.$CUDA_VER_MINOR-trt5.1.2.2-rc-20190227-1-1.x86_64.rpm"
+        if [ "_$GIT_MIRROR" == "_$GIT_MIRROR_CODINGCAFE" ]; then
+            TRT_REPO_URL_CODINGCAFE='https://repo.codingcafe.org/nvidia/tensorrt'
+            TRT_FILENAME_CODINGCAFE="$(curl -sSL "$TRT_REPO_URL"           \
+                | sed -n 's/^.*href="\([^"]*\)".*$/\1/p'    \
+                | grep 'rhel7'                              \
+                | grep 'repo'                               \
+                | grep '\.rpm$'                             \
+                | sort -V                                   \
+                | tail -n1)"
+            if [ "$TRT_FILENAME_CODINGCAFE" ]; then
+                TRT_REPO_URL="$TRT_REPO_URL_CODINGCAFE"
+                TRT_FILENAME="$TRT_FILENAME_CODINGCAFE"
+            else
+                echo "[Warning] TensorRT image not found in CodingCafe repo."
+            fi
+        fi
+        TRT_URL="$TRT_REPO_URL/$TRT_FILENAME"
+
+        sudo yum remove -y --skip-broken 'nv-tensorrt-repo-*' || true
+        $RPM_INSTALL "$TRT_URL"
+        $RPM_INSTALL "tensorrt" || $RPM_UPDATE "tensorrt" || $RPM_REINSTALL "tensorrt"
+        $IS_CONTAINER && sudo yum remove -y --skip-broken 'nv-tensorrt-repo-*'
+    )
+
+    # ============================================================
     # CUDA Samples
-    cd /usr/local/cuda/samples
-    . scl_source enable devtoolset-6 || true
-    export MPI_HOME=/usr/local/openmpi
-    sudo make -j clean
-    VERBOSE=1 time sudo make -j$(nproc)
+    # ============================================================
+    (
+        set -xe
+
+        cd /usr/local/cuda/samples
+        . scl_source enable devtoolset-6 || true
+        export MPI_HOME=/usr/local/openmpi
+        sudo make -j clean
+        VERBOSE=1 time sudo make -j$(nproc)
+    )
 )
 sudo rm -vf $STAGE/cuda
 sync || true
