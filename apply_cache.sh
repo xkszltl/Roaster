1af9c64b1940ca326932c04bb3fc135148b1df61
Tongliang Liao <xkszltl@gmail.com>
2019-04-20 04:19:00 -0700

    Fix incorrect rpm repo priority.

--- apply_cache.sh
+++ apply_cache.sh
@@ -18,9 +18,7 @@ for i in $(ls "$ROOT_DIR/repos/"*.repo | grep -v "$([ -f "$RPM_CACHE_REPO" ] && 
     [ -f "/etc/yum.repos.d/$(basename "$i")" ] || sudo yum-config-manager --add-repo $i
 done
 
-[ "$RPM_PRIORITY" ] || export RPM_PRIORITY=99
-
-xargs -n1 <<<$@ | sed 's/\(.*\)/\1 cache-\1/' | xargs -n1 | sed "s/\(.*\)/--save --setopt=\1\.priority=$RPM_PRIORITY/" | xargs sudo yum-config-manager
+[ "$RPM_PRIORITY" ] && xargs -n1 <<<$@ | sed 's/\(.*\)/\1 cache-\1/' | xargs -n1 | sed "s/\(.*\)/--save --setopt=\1\.priority=$RPM_PRIORITY/" | xargs sudo yum-config-manager
 xargs -n1 <<<$@ | sed "s/^/--disable $([ -f "$RPM_CACHE_REPO" ] || echo cache-)/" | xargs sudo yum-config-manager
 xargs -n1 <<<$@ | sed "s/^/--enable  $([ -f "$RPM_CACHE_REPO" ] && echo cache-)/" | xargs sudo yum-config-manager
 
