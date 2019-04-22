5a5bbacdcf912e194658358dbc7bbc333016e59f
Tongliang Liao <xkszltl@gmail.com>
2019-04-22 18:00:38 +0800

    Mirror containerd.

--- mirrors.sh
+++ mirrors.sh
@@ -61,6 +61,7 @@ BVLC/caffe,\
 caffe2/{caffe2,models},\
 catchorg/{Catch2,Clara},\
 ccache/ccache,\
+containerd/containerd,\
 cython/cython,\
 dmlc/{dlpack,dmlc-core,gluon-cv,gluon-nlp,HalideIR,tvm,xgboost},\
 dotnet/{cli,core{,-setup,clr,fx},standard},\
