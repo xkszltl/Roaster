f3040e510dbf871fb56056396a917add156baa24
Tongliang Liao <xkszltl@gmail.com>
2019-04-24 03:17:56 +0800

    sh doesn't have pushd and this issue shows up on Ubuntu when installing DEB. The reason I keep sh here (most places in this repo use bash) is to avoid having additional requirement for package consumer on a lightweight system.

--- pkgs/utils/fpm/post_install.sh
+++ pkgs/utils/fpm/post_install.sh
@@ -6,15 +6,17 @@
 
 set -xe
 
-[ "$IS_CONTAINER" ] || export IS_CONTAINER=$([ -e /proc/1/cgroup ] && [ $(sed -n 's/^[^:]*:[^:]*:\(..\)/\1/p' /proc/1/cgroup | wc -l) -gt 0 ] && echo true || echo false)
+[ "$IS_CONTAINER" ] || export IS_CONTAINER="$([ -e /proc/1/cgroup ] && [ $(sed -n 's/^[^:]*:[^:]*:\(..\)/\1/p' /proc/1/cgroup | wc -l) -gt 0 ] && echo true || echo false)"
 
 # ----------------------------------------------------------------
 # Clean up empty directory if necessary
 # ----------------------------------------------------------------
 
-pushd /etc/ld.so.conf.d
-[ -d 'roaster.conf.d' ] && echo 'include roaster.conf.d/*.conf' > /etc/ld.so.conf.d/roaster.conf || rm -f roaster.conf
-popd
+(
+    set -xe
+    cd /etc/ld.so.conf.d
+    [ -d 'roaster.conf.d' ] && echo 'include roaster.conf.d/*.conf' > /etc/ld.so.conf.d/roaster.conf || rm -f roaster.conf
+)
 
 # ----------------------------------------------------------------
 # Refresh
