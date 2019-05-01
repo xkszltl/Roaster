76dc7f2a86e964042845ab37ac5dd8c7ac4a3c62
Tongliang Liao <xkszltl@gmail.com>
2019-05-01 10:26:08 +0800

    sudo may fail and kill the entire daemon process. In this case we shuold retry as soon as possible but not hitting it too hard and trigger other issues.

--- pkgs/utils/sudo_ping_daemon.sh
+++ pkgs/utils/sudo_ping_daemon.sh
@@ -42,7 +42,7 @@ echo "Daemon sudo_ping is monitoring process $SUDO_PING_PID for zombie."
     # Append '0' because ps may not be able to found the process itself as zombie.
     while [ "_$(ps -o ppid= -p "$SUDO_PING_PID" | sed 's/[[:space:]]//g')" = "_$PPID" ]; do
         SUDO_PING_DDL="$(expr "$(date +%s)" + "$SUDO_PING_HEARTBEAT_SEC" - "$SUDO_PING_SLA_SEC")"
-        sudo -n true
+        sudo -n true || SUDO_PING_DDL="$(expr "$(date +%s)" + "$SUDO_PING_SLA_SEC")"
         while [ "_$(ps -o ppid= -p "$SUDO_PING_PID" | sed 's/[[:space:]]//g')" = "_$PPID" ] && [ "$(date +%s)" -lt "$SUDO_PING_DDL" ]; do
     	    sleep "$SUDO_PING_SLA_SEC"
         done
