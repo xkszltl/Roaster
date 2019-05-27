cadcca73d72f2e9d29874b2ad9fecca8d3b4eba6
Tongliang Liao <xkszltl@gmail.com>
2019-05-27 15:18:35 +0800

    Update privoxy.

--- docker_conf.sh
+++ docker_conf.sh
@@ -4,20 +4,45 @@ set -e
 
 sudo echo 'Permission granted.'
 
+export ROOT_DIR="$(readlink -e "$(dirname "$0")")"
+
+cd "$ROOT_DIR"
+. pkgs/env/cred.sh
+
 echo '========================================'
-echo '| Before'
+echo '| HTTP Proxy'
 echo '========================================'
 
+sudo mkdir -p '/etc/systemd/system/docker.service.d'
+
+echo "# This file is auto-generated and may be overwritten.
+
+[Service]
+Environment=\"HTTP_PROXY=$CRED_USR_PRIVOXY_ADDR:$CRED_USR_PRIVOXY_PORT\"
+Environment=\"HTTPS_PROXY=$CRED_USR_PRIVOXY_ADDR:$CRED_USR_PRIVOXY_PORT\"
+Environment=\"NO_PROXY=127.0.0.1,::1,localhost,docker.codingcafe.org,git.codingcafe.org\"
+" | sudo tee '/etc/systemd/system/docker.service.d/http-proxy.conf'
+
+sudo systemctl daemon-reload || true
+# sudo systemctl restart docker
+
+echo '========================================'
+echo '| Daemon JSON (Before)'
+echo '========================================'
+
+sudo [ -e '/etc/docker/daemon.json' ] || sudo echo '{}' > '/etc/docker/daemon.json'
 sudo cat '/etc/docker/daemon.json' | sudo tee '/etc/docker/daemon.json.bak' | jq -e '.'
 
 echo '========================================'
-echo '| After'
+echo '| Daemon JSON (After)'
 echo '========================================'
 
 sudo cat '/etc/docker/daemon.json.bak'                                                                      \
 | jq -e '. |= . + {"data-root":"/media/Scratch/docker"}'                                                    \
 | jq -e '. |= . + {"default-runtime":"nvidia"}'                                                             \
 | jq -e '. |= . + {"experimental":true}'                                                                    \
+| jq -e '. |= . + {"features":{}}'                                                                          \
+| jq -e '."features" |= ."features" + {"buildkit":true}'                                                    \
 | jq -e '. |= . + {"max-concurrent-downloads":1024}'                                                        \
 | jq -e '. |= . + {"max-concurrent-uploads":1024}'                                                          \
 | jq -e '. |= . + {"storage-driver":"devicemapper"}'                                                        \
