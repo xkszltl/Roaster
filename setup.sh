a33c7588039cd8e332dce02f8269bf6eea1f9208
Tongliang Liao <xkszltl@gmail.com>
2019-06-02 18:03:58 +0800

    Empty repo file trick doesn't work.

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 case "$DISTRO_ID" in
 "centos" | "fedora" | "rhel")
-    export RPM_CACHE_REPO="/etc/yum.repos.d/cache.repo"
+    export RPM_CACHE_REPO="/etc/yum.repos.d/codingcafe-cache.repo"
     ;;
 esac
 
