00f18b14974f4b5d01b4215f7b342a65a819f2e8
Tongliang Liao <xkszltl@gmail.com>
2019-06-04 16:04:42 +0800

    Set gtest source path for RapidJson. It was able to find GTestSrc on Ubuntu but not on CentOS. Now I force it to use our version installed in /usr/local.
    Also add some comments about what's going on for the failing valgrind_unittest.
    See https://github.com/Tencent/rapidjson/issues/1520

--- pkgs/rapidjson.sh
+++ pkgs/rapidjson.sh
@@ -8,7 +8,7 @@
     # ------------------------------------------------------------
 
     . "$ROOT_DIR/pkgs/utils/git/version.sh" Tencent/rapidjson,master
-    until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
+    until git clone --depth 1 --recursive --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd rapidjson
 
     # ------------------------------------------------------------
@@ -33,21 +33,38 @@
         mkdir -p build
         cd $_
 
-        cmake                                       \
-            -DCMAKE_BUILD_TYPE=Release              \
-            -DCMAKE_C_COMPILER="$CC"                \
-            -DCMAKE_CXX_COMPILER="$CXX"             \
-            -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
+        cmake                                           \
+            -DCMAKE_BUILD_TYPE=Release                  \
+            -DCMAKE_C_COMPILER="$CC"                    \
+            -DCMAKE_CXX_COMPILER="$CXX"                 \
+            -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache     \
             -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"   \
-            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
-            -DRAPIDJSON_HAS_STDSTRING=ON            \
-            -G"Ninja"                               \
+            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"       \
+            -DRAPIDJSON_BUILD_THIRDPARTY_GTEST=OFF      \
+            -DRAPIDJSON_HAS_STDSTRING=ON                \
+            -DGTEST_SEARCH_PATH='/usr/local/src/gtest'  \
+            -G"Ninja"                                   \
             ..
 
         time cmake --build .
-        # Valgrind unit test may fail.
+        # Valgrind unit test may fail: https://github.com/Tencent/rapidjson/issues/1520
         time cmake --build . --target test || true
         time cmake --build . --target install
+
+        # Exclude GTest files.
+        pushd "$INSTALL_ROOT"
+        for i in gtest; do
+            case "$DISTRO_ID" in
+            'centos' | 'fedora' | 'rhel')
+                [ "$(rpm -qa "roaster-$i")" ] || continue
+                rpm -ql "roaster-$i" | sed -n 's/^\//\.\//p' | xargs rm -rf
+                ;;
+            'ubuntu')
+                dpkg -l "roaster-$i" && dpkg -L "roaster-$i" | xargs -n1 | xargs -i -n1 find {} -maxdepth 0 -not -type d | sed -n 's/^\//\.\//p' | xargs rm -rf
+                ;;
+            esac
+        done
+        popd
     )
 
     "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
