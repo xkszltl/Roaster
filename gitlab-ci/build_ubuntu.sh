a52c9795700f95a35077c1dd0d911cb903b9f07c
Tongliang Liao <xkszltl@gmail.com>
2019-06-25 18:06:58 +0800

    Install docker-ce on Ubuntu.

--- gitlab-ci/build_ubuntu.sh
+++ gitlab-ci/build_ubuntu.sh
@@ -18,7 +18,7 @@ export BASE_DISTRO=ubuntu
 
 [ "$CI_COMMIT_REF_NAME" ] || export CI_COMMIT_REF_NAME=build-init
 
-for CI_JOB_STAGE in init font pkg auth infra llvm util misc dl edit finish; do
+for CI_JOB_STAGE in init repo font pkg auth infra llvm util misc dl edit finish; do
     export CI_JOB_STAGE
 
     [ "$FIRST_STAGE" ] || [ "_$(sed 's/^[^\-]*\-//' <<< "$CI_COMMIT_REF_NAME")" = "_$CI_JOB_STAGE" ] || continue
