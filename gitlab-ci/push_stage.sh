0eceed85ef655af29174a5d1a76f8980ca2ca71c
Tongliang Liao <xkszltl@gmail.com>
2019-07-17 12:43:26 +0800

    Docker login.

--- gitlab-ci/push_stage.sh
+++ gitlab-ci/push_stage.sh
@@ -8,4 +8,14 @@ set -xe
 [ "$CI_JOB_STAGE" ]
 [ "$CI_REGISTRY_IMAGE" ]
 
+set +x
+if [ "$CI_REGISTRY" ] && [ "$CI_REGISTRY_USER" ] && [ "$CI_REGISTRY_PASSWORD" ]; then
+    sudo docker login                   \
+        --password-stdin                \
+        --username "$CI_REGISTRY_USER"  \
+        "$CI_REGISTRY"                  \
+    <<< "$CI_REGISTRY_PASSWORD"
+fi
+set -x
+
 time sudo docker push "$CI_REGISTRY_IMAGE/$BASE_DISTRO:stage-$(sed 's/^[^-]*-//' <<< "$CI_JOB_STAGE")"
