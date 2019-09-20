735ea997ecdbf4f3ee6767b60ed49ed45de401ea
Tongliang Liao <xkszltl@gmail.com>
2019-09-20 16:10:57 -0700

    Redhat/CentOS has deprecated devtoolset-6. See discussions in:   * CentOS forum: https://www.centos.org/forums/viewtopic.php?t=71663&p=301567   * VFX platform community who has hard dependency on gcc-6: https://groups.google.com/forum/m/#!topic/vfx-platform-discuss/_-_CPw1fD3c   * SCL lifecycle: https://access.redhat.com/support/policy/updates/rhscl

--- pkgs/pkg.sh
+++ pkgs/pkg.sh
@@ -20,7 +20,7 @@ for i in pkg-{stable,skip,all}; do
         # ------------------------------------------------------------
         for attempt in $(seq $RPM_MAX_ATTEMPT -1 0); do
             echo "
-                [!] devtoolset-{6,7,8}{,-*}
+                [!] devtoolset-{7,8}{,-*}
                 [!] llvm-toolset-7{,-*}
 
                 qpid-cpp-client{,-*}
