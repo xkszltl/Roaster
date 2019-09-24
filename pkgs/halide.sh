1a141cf3a428a6e8cc168145e3ad47216a891a4c
Tongliang Liao <xkszltl@gmail.com>
2019-09-23 17:53:49 -0700

    Use Halide release tag instead of master since there's a recent one. This also bypass the current compile issue on master:   * https://github.com/halide/Halide/issues/4265

--- pkgs/halide.sh
+++ pkgs/halide.sh
@@ -7,7 +7,7 @@
 
     # ------------------------------------------------------------
 
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" halide/Halide,master
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" halide/Halide,release_
     until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd Halide
 
