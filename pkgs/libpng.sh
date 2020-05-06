03429c399266b400cd55384bf43eef20346b293c
Tongliang Liao <xkszltl@gmail.com>
2020-05-07 04:13:21 +0800

    Get libpng version from upstream without relying on the codingcafe mirror.

--- pkgs/libpng.sh
+++ pkgs/libpng.sh
@@ -7,15 +7,16 @@
 
     # ------------------------------------------------------------
 
-    export _GIT_MIRROR="$GIT_MIRROR"
-    export GIT_MIRROR="$GIT_MIRROR_CODINGCAFE"
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" libpng/libpng,v
-    export GIT_MIRROR="$_GIT_MIRROR"
-    env -u _GIT_MIRROR
-
     if [ "_$GIT_MIRROR" = "_$GIT_MIRROR_CODINGCAFE" ]; then
+        . "$ROOT_DIR/pkgs/utils/git/version.sh" libpng/libpng,v
         until git clone --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     else
+        export _GIT_MIRROR="$GIT_MIRROR"
+        export GIT_MIRROR="git://git.code.sf.net/p"
+        . "$ROOT_DIR/pkgs/utils/git/version.sh" libpng/code,v
+        export GIT_MIRROR="$_GIT_MIRROR"
+        env -u _GIT_MIRROR
+
         until git clone --single-branch -b "$GIT_TAG" "git://git.code.sf.net/p/libpng/code" libpng; do echo 'Retrying'; done
     fi
     cd libpng
