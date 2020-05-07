68aa77963b0a3748e97dea0b8e3075840d2f2498
Tongliang Liao <xkszltl@gmail.com>
2020-05-07 15:13:14 +0800

    Exclude pybind11 files in pytorch.

--- pkgs/pytorch.sh
+++ pkgs/pytorch.sh
@@ -165,7 +165,7 @@
 
         # Exclude GTest/MKL-DNN/ONNX/Caffe files.
         pushd "$INSTALL_ROOT"
-        for i in caffe gtest mkl-dnn onnx openblas; do
+        for i in caffe gtest mkl-dnn onnx openblas pybind11; do
             case "$DISTRO_ID" in
             'centos' | 'fedora' | 'rhel')
                 [ "$(rpm -qa "roaster-$i")" ] || continue
