9b1731a12f70a3dccc3fa9de5daa9ea7020c10d0
Tongliang Liao <xkszltl@gmail.com>
2020-07-24 02:30:31 +0800

    Build nsync on Linux.

---
+++ pkgs/nsync.sh
@@ -0,0 +1,61 @@
+# ================================================================
+# Compile Nsync
+# ================================================================
+
+[ -e $STAGE/nsync ] && ( set -xe
+    cd $SCRATCH
+
+    # ------------------------------------------------------------
+
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" google/nsync,
+    until git clone -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
+
+    cd nsync
+
+    # ------------------------------------------------------------
+
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
+
+    (
+        case "$DISTRO_ID" in
+        'centos' | 'fedora' | 'rhel')
+            set +xe
+            . scl_source enable devtoolset-8
+            set -xe
+            export CC="gcc" CXX="g++"
+            ;;
+        'ubuntu')
+            export CC="gcc-8" CXX="g++-8"
+            ;;
+        esac
+
+        . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
+
+        mkdir -p build
+        cd $_
+
+        cmake                                       \
+            -DBUILD_SHARED_LIBS=ON                  \
+            -DCMAKE_BUILD_TYPE=Release              \
+            -DCMAKE_C_COMPILER="$CC"                \
+            -DCMAKE_CXX_COMPILER="$CXX"             \
+            -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
+            -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"   \
+            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
+            -G"Ninja"                               \
+            ..
+
+        time cmake --build .
+        CTEST_PARALLEL_LEVEL="$(nproc)" time cmake --build . --target test
+        time cmake --build . --target install
+    )
+
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+
+    # ------------------------------------------------------------
+
+    cd
+    rm -rf $SCRATCH/nsync
+)
+sudo rm -vf $STAGE/nsync
+sync || true
