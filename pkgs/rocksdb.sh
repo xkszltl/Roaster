5983f50677f0cd1a58e4b06bcaa33ce4d36d9307
Tongliang Liao <xkszltl@gmail.com>
2020-09-16 20:41:31 +0800

    All 3 PR for https://github.com/facebook/rocksdb/issues/6179 has been released in 6.11.4.

--- pkgs/rocksdb.sh
+++ pkgs/rocksdb.sh
@@ -7,8 +7,7 @@
 
     # ------------------------------------------------------------
 
-    # . "$ROOT_DIR/pkgs/utils/git/version.sh" facebook/rocksdb,v
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" facebook/rocksdb,master
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" facebook/rocksdb,v
     until git clone -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd rocksdb
 
