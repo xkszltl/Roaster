7cc31ba7cf6b39b03bf08c18cbd42ffca812cabb
Tongliang Liao <xkszltl@gmail.com>
2020-09-29 09:21:09 +0800

    Expand vars in repo name because they are not supported.

--- apply_cache.sh
+++ apply_cache.sh
@@ -18,7 +18,14 @@ export ROOT_DIR="$(readlink -e "$(dirname $0)")"
 [ "$RPM_CACHE_REPO" ] || export RPM_CACHE_REPO="/etc/yum.repos.d/codingcafe-cache.repo"
 
 for i in $(ls "$ROOT_DIR/repos/"*.repo); do
-    [ -f "/etc/yum.repos.d/$(basename "$i")" ] || sudo yum-config-manager --add-repo $i
+    [ ! -f "/etc/yum.repos.d/$(basename "$i")" ] || continue
+    repo_tmp="$(mktemp "repo_tmp.XXXXXXXXXX.repo")"
+    cat "$i"                                                                                        \
+    | sed 's/^\([[:space:]]*\[.*\)\$basearch\(.*\]\)[[:space:]]*$/\1'"$(uname -i)"'\2/g'            \
+    | sed 's/^\([[:space:]]*\[.*\)\$releasever\(.*\]\)[[:space:]]*$/\1'"$DISTRO_VERSION_ID"'\2/g'   \
+    | tee "$repo_tmp"
+    sudo yum-config-manager --add-repo "$repo_tmp"
+    rm -rf "$repo_tmp"
 done
 
 [ "$RPM_PRIORITY" ] && xargs -n1 <<<$@ | sed 's/\(.*\)/\1 cache-\1/' | xargs -n1 | sed "s/\(.*\)/--save --setopt=\1\.priority=$RPM_PRIORITY/" | xargs sudo yum-config-manager
