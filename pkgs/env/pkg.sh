3ec5d4893298ee42bf1d089acd562420aa59e14b
Tongliang Liao <xkszltl@gmail.com>
2020-10-08 18:05:17 +0800

    Reformat.

--- pkgs/env/pkg.sh
+++ pkgs/env/pkg.sh
@@ -5,20 +5,16 @@
 export RPM_MAX_ATTEMPT=10
 
 # TODO: Fix the following issue:
-#       LLVM may select the wrong gcc toolchain without libgcc_s integrated.
-#       The correct choice is x86_64-redhat-linux instead of x86_64-linux-gnu.
-#
-#       libasan2 seems to contain libasan.so.3.0.0 and causing a conflict with libasan3
-#
-#       python-lexicon conflict with python2-dns-lexicon
-#       file /usr/lib/python2.7/site-packages/test/__init__.pyc conflicts between attempted installs of python2-ansible-runner-1.0.1-1.el7.noarch and python2-neomodel-3.2.8-1.el7.noarch
-#
-#       Python issues from CentOS 7.5:
-#           python2-azure-sdk conflict with python-azure-sdk
-#           python2-boto3 conflict with python-boto3
-#           python2-s3transfer conflict with python-s3transfer
-#
-#       rh-ruby26-rubygem-bundler-doc has version mismatch between 1.16/1.17.
+#   - LLVM may select the wrong gcc toolchain without libgcc_s integrated.
+#     The correct choice is x86_64-redhat-linux instead of x86_64-linux-gnu.
+#   - libasan2 seems to contain libasan.so.3.0.0 and causing a conflict with libasan3
+#   - python-lexicon conflict with python2-dns-lexicon
+#     file /usr/lib/python2.7/site-packages/test/__init__.pyc conflicts between attempted installs of python2-ansible-runner-1.0.1-1.el7.noarch and python2-neomodel-3.2.8-1.el7.noarch
+#   - Python issues from CentOS 7.5:
+#     * python2-azure-sdk conflict with python-azure-sdk
+#     * python2-boto3 conflict with python-boto3
+#     * python2-s3transfer conflict with python-s3transfer
+#   - rh-ruby26-rubygem-bundler-doc has version mismatch between 1.16/1.17.
 
 export RPM_BLACKLIST=$(echo "
     *-debuginfo
