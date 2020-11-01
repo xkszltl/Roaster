f117280ee1aee0042d22345462c2322e89223f82
Tongliang Liao <xkszltl@gmail.com>
2020-11-02 00:57:50 +0800

    Pin pybind to 2.5 until the `Python.h` not found issue got fixed. https://github.com/pybind/pybind11/issues/2632

--- pkgs/pybind.sh
+++ pkgs/pybind.sh
@@ -9,7 +9,9 @@
 
     # ------------------------------------------------------------
 
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" pybind/pybind11,v
+    # Pin to v2.5 due to Python.h not found when used by downstream.
+    # https://github.com/pybind/pybind11/issues/2632
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" pybind/pybind11,v2.5
     until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd pybind11
 
