1a82e064c1d7db1a1a0f5ebebd3957d6fb9a871f
Tongliang Liao <xkszltl@gmail.com>
2020-11-29 14:03:28 +0800

    Build LZ4. Pending issue: - About `make check -j`: https://github.com/lz4/lz4/issues/957 - About `$CFLAGS`: https://github.com/lz4/lz4/issues/958

---
+++ pkgs/lz4.sh
@@ -0,0 +1,54 @@
+# ================================================================
+# Compile LZ4
+# ================================================================
+
+[ -e $STAGE/lz4 ] && ( set -xe
+    cd $SCRATCH
+
+    # ------------------------------------------------------------
+
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" lz4/lz4,v
+    until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
+    cd lz4
+
+    # ------------------------------------------------------------
+
+    . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
+
+    (
+        case "$DISTRO_ID" in
+        'centos' | 'fedora' | 'rhel')
+            set +xe
+            . scl_source enable devtoolset-9 || exit 1
+            set -xe
+            export CC="gcc" CXX="g++"
+            ;;
+        'ubuntu')
+            export CC="gcc-8" CXX="g++-8"
+            ;;
+        esac
+
+        . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
+
+        export CC="$(which ccache) $CC" CXX="$(which ccache) $CXX"
+        export CFLAGS="$CFLAGS -O3 -fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"
+        export CXXFLAGS="$CXXFLAGS -O3 -fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"
+
+        # Explicitly inject $CFLAGS due to https://github.com/lz4/lz4/issues/958
+        make all CFLAGS="$CFLAGS" -j$(nproc)
+        # Parallel test does not work: https://github.com/lz4/lz4/issues/957
+        make check
+        # Only run quick tests (check) by default.
+        # make test
+        make PREFIX="$INSTALL_ABS" install -j
+    )
+
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+
+    # ------------------------------------------------------------
+
+    cd
+    rm -rf $SCRATCH/lz4
+)
+sudo rm -vf $STAGE/lz4
+sync || true
