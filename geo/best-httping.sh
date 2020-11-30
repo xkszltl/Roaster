d8933671deb66e871b19d5df92e8389bdf6ede96
Tongliang Liao <xkszltl@gmail.com>
2020-12-01 05:16:56 +0800

    Split httping to a general script.

---
+++ geo/best-httping.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for cmd in bc httping parallel; do
+    which "$cmd" > /dev/null
+done
+
+LINK_QUALITY="$(set -e
+    parallel --halt $(sed -n 's/\(..*\)/now,success=\1/p' <<< "$TOPK") -j0 'bash -c '"'"'
+        set -e
+        httping -Zfc10 -t3 {} 2>&1                                                                  \
+        | sed -n "s/.*[^0-9]\([0-9][0-9]*\) *ok.*time  *\([0-9][0-9]*\) *ms.*/\1\/(\2\+1)\*10^3/p"  \
+        | bc -l                                                                                     \
+        | xargs -rn1 printf "%.3f\n"                                                                \
+        | sed "s/\$/ $(sed "s/\([\\\/\.\-]\)/\\\\\1/g" <<< {})/"                                    \
+        | grep .
+    '"'" ::: "$@"   \
+    2> /dev/null    \
+    | sort -nr      \
+)"
--- geo/maven-mirror.sh
+++ geo/maven-mirror.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+cd "$(dirname "$0")"
+[ "$ROOT_DIR" ] || export ROOT_DIR="$(readlink -e ..)"
+[ "$ROOT_DIR" ]
+cd "$ROOT_DIR"
+
 for cmd in bc httping parallel xmllint; do
     which "$cmd" > /dev/null
 done
@@ -10,28 +15,17 @@ echo '----------------------------------------------------------------'
 echo '              Measure link quality to maven mirrors             '
 echo '----------------------------------------------------------------'
 
-export LINK_QUALITY="$(set -e
-    parallel --halt now,success=3 -j0 'bash -c '"'"'
-        set -e
-        httping -Zfc10 -t3 {} 2>&1                                                                  \
-        | sed -n "s/.*[^0-9]\([0-9][0-9]*\) *ok.*time  *\([0-9][0-9]*\) *ms.*/\1\/(\2\+1)\*10^3/p"  \
-        | bc -l                                                                                     \
-        | xargs -rn1 printf "%.3f\n"                                                                \
-        | sed "s/\$/ $(sed "s/\([\\\/\.\-]\)/\\\\\1/g" <<< {})/"                                    \
-        | grep .
-    '"'" :::                                                                \
-        https://maven-central-asia.storage-download.googleapis.com/maven2/  \
-        https://maven-central-eu.storage-download.googleapis.com/maven2/    \
-        https://maven-central.storage-download.googleapis.com/maven2/       \
-        https://maven-central.storage.googleapis.com/                       \
-        https://maven.aliyun.com/repository/public/                         \
-        https://mirrors.163.com/maven/repository/maven-public/              \
-        https://repo.maven.apache.org/maven2/                               \
-        https://repo1.maven.org/maven2/                                     \
-        https://repository.jboss.org/nexus/content/repositories/central/    \
-    2> /dev/null                                                            \
-    | sort -nr                                                              \
-)"
+TOPK=3 . "$ROOT_DIR/geo/best-httping.sh"                                \
+    https://maven-central-asia.storage-download.googleapis.com/maven2/  \
+    https://maven-central-eu.storage-download.googleapis.com/maven2/    \
+    https://maven-central.storage-download.googleapis.com/maven2/       \
+    https://maven-central.storage.googleapis.com/                       \
+    https://maven.aliyun.com/repository/public/                         \
+    https://mirrors.163.com/maven/repository/maven-public/              \
+    https://repo.maven.apache.org/maven2/                               \
+    https://repo1.maven.org/maven2/                                     \
+    https://repository.jboss.org/nexus/content/repositories/central/    \
+[ "$LINK_QUALITY" ]
 
 column -t <<< "$LINK_QUALITY" | sed 's/^/| /'
 
