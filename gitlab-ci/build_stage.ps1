6305b8b73b5ebd951cdf51d22abf638d6d1b55c2
Yateng Hong <yatengh@microsoft.com>
2020-12-10 10:29:41 +0800

    Support build pkgs group.

--- gitlab-ci/build_stage.ps1
+++ gitlab-ci/build_stage.ps1
@@ -1,6 +1,6 @@
 Param(
     [Parameter(Mandatory = $True)]
-    [string] $pkg,
+    [string] $stage,
 
     [Parameter(Mandatory = $True)]
     [string] $scratch
@@ -13,27 +13,30 @@ cd $PSScriptRoot/../win
 ${Env:SCRATCH} = $scratch
 mkdir "${Env:SCRATCH}"
 
-if ($pkg -ne "vsbuildtools")
+foreach ($pkg in $stage.Split(","))
 {
-    . "pkgs/env/toolchain.ps1"
-}
-
-Write-Host "Install $pkg"
-
-$path = "pkgs/$pkg.ps1"
-if ($(Test-Path $path -ErrorAction SilentlyContinue))
-{
-    & "${PSHOME}/powershell.exe" $path
-    if (-Not $?)
+    if ($pkg -ne "vsbuildtools")
     {
-        Write-Host "[Error] Failed to install `"$pkg`""
+        . "pkgs/env/toolchain.ps1"
+    }
+    
+    Write-Host "Install $pkg"
+    
+    $path = "pkgs/$pkg.ps1"
+    if ($(Test-Path $path -ErrorAction SilentlyContinue))
+    {
+        & "${PSHOME}/powershell.exe" $path
+        if (-Not $?)
+        {
+            Write-Host "[Error] Failed to install `"$pkg`""
+            exit 1
+        }
+    }
+    else
+    {
+        Write-Host "[Error] Script `"$path`" not found."
         exit 1
     }
-}
-else
-{
-    Write-Host "[Error] Script `"$path`" not found."
-    exit 1
 }
 
 # Post cleanup
