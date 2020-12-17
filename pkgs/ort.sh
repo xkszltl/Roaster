1220eda1ab65af00fecaa648bdfe23cbc334af1a
Yateng Hong <yatengh@microsoft.com>
2020-12-17 09:28:03 +0800

    Resolve comments.

--- pkgs/ort.sh
+++ pkgs/ort.sh
@@ -19,10 +19,11 @@
     cd onnxruntime
 
     # Known issues:
-    #  - CUDA test hang without GPU.
-    #    https://github.com/microsoft/onnxruntime/issues/4656
+    #   - CUDA test hang without GPU.
+    #     https://github.com/microsoft/onnxruntime/issues/4656
     git fetch origin master
     git cherry-pick 0978d2b
+    git cherry-pick 7250562
 
     git remote add patch https://github.com/xkszltl/onnxruntime.git
 
