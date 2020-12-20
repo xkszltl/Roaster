cb03fa5f178074e1f1b3ca493eeb20a7b751297b
Tongliang Liao <xkszltl@gmail.com>
2020-12-20 21:59:19 +0800

    Switch to contrib cmake for zstd, since that works much better.

--- pkgs/zstd.sh
+++ pkgs/zstd.sh
@@ -30,23 +30,48 @@
 
         . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
 
-        export CC="$(which ccache) $CC" CXX="$(which ccache) $CXX"
-        export CFLAGS="$CFLAGS -fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"
-        export CXXFLAGS="$CXXFLAGS -fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"
+        if false; then
+            export CC="$(which ccache) $CC" CXX="$(which ccache) $CXX"
+            export CFLAGS="$CFLAGS -fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"
+            export CXXFLAGS="$CXXFLAGS -fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"
 
-        # Known issues:
-        #   - Retry due to potentially broken dependency graph.
-        #     https://github.com/facebook/zstd/issues/2380
-        #   - Parallel build does not work since v1.4.8.
-        #     https://github.com/facebook/zstd/issues/2436
-        for retry in $(seq 3 -1 0); do
-            [ "$retry" -gt 0 ]
-            make allmost manual && make -C contrib/pzstd all -j$(nproc) && make -C contrib/largeNbDicts all -j$(nproc) && break
-        done
-        # Only run quick tests (check) by default.
-        # make test -j$(nproc)
-        make check -j$(nproc)
-        make PREFIX="$INSTALL_ABS" install -j
+            # Known issues:
+            #   - Retry due to potentially broken dependency graph.
+            #     https://github.com/facebook/zstd/issues/2380
+            #   - Parallel build does not work since v1.4.8.
+            #     https://github.com/facebook/zstd/issues/2436
+            for retry in $(seq 3 -1 0); do
+                [ "$retry" -gt 0 ]
+                make allmost manual && make -C contrib/pzstd all -j$(nproc) && make -C contrib/largeNbDicts all -j$(nproc) && break
+            done
+            # Only run quick tests (check) by default.
+            # make test -j$(nproc)
+            make check -j$(nproc)
+            make PREFIX="$INSTALL_ABS" install -j
+        else
+            mkdir -p build
+            cd $_
+
+            cmake                                       \
+                -DBUILD_TESTING=ON                      \
+                -DCMAKE_BUILD_TYPE=Release              \
+                -DCMAKE_C_COMPILER="$CC"                \
+                -DCMAKE_CXX_COMPILER="$CXX"             \
+                -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
+                -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"   \
+                -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
+                -DZSTD_BUILD_CONTRIB=ON                 \
+                -DZSTD_BUILD_SHARED=ON                  \
+                -DZSTD_BUILD_STATIC=ON                  \
+                -DZSTD_MULTITHREAD_SUPPORT=ON           \
+                -DZSTD_PROGRAMS_LINK_SHARED=ON          \
+                -G"Ninja"                               \
+                ../build/cmake
+
+            time cmake --build .
+            time ctest --label-exclude Medium --output-on-failure --verbose
+            time cmake --build . --target install
+        fi
     )
 
     "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
