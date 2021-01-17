23917e9d1d71a036327df18f005448bb2d70aa29
Tongliang Liao <xkszltl@gmail.com>
2021-01-17 21:50:00 +0800

    No cuDNN for CUDA 11.2.

--- win/pkgs/cudnn.ps1
+++ win/pkgs/cudnn.ps1
@@ -58,6 +58,8 @@ for ($i=8; ($i -ge 8) -and (-not (Test-Path cudnn.zip)); $i--)
             for ($l=39; ($l -ge 0) -and (-not (Test-Path cudnn.zip)); $l--)
             {
                 $cudnn_name="cudnn-$((nvcc --version) -match ' release ([0-9\.]*)' -replace '.* release ([0-9\.]*).*','${1}')-windows-x64-v${i}.${j}.${k}.${l}.zip"
+                # No cuDNN for CUDA 11.2 currently.
+                $cudnn_name=("$cudnn_name" -replace '11.2','11.1')
                 Write-Host -NoNewline '.'
                 $ErrorActionPreference="SlightlyContinue"
                 & "${Env:ProgramFiles}/CURL/bin/curl.exe" -fksSIL "${cudnn_url}/v${i}.${j}.${k}/${cudnn_name}" | Out-Null
