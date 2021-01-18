0696b3acbf14a31c0af6b5adfd9dd46681ed3ea9
Tongliang Liao <xkszltl@gmail.com>
2021-01-18 12:02:43 +0800

    Add opencv nuget update scripts.

---
+++ win/nuget/opencv-dev/update.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env powershell
+
+$ErrorActionPreference="Stop"
+
+cd $PSScriptRoot
+
+ls -Name -Path "${Env:ProgramFiles}/opencv/x64/vc16/lib/*.lib" | sort
+(ls -Name -Path ("${Env:ProgramFiles}/opencv/bin/*.pdb", "${Env:ProgramFiles}/opencv/x64/vc16/bin/*.pdb", "${Env:ProgramFiles}/opencv/x64/vc16/lib/*.lib") | sort) -Replace '^(.*)$',"<Content Include=`"`$(MSBuildThisFileDirectory)/../lib/native/lib/Release/$1`">`n  <Link>`$1</Link>`n  <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>`n</Content>"
