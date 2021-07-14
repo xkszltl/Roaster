ee4d8d61cb17540b806e5b908ae3f5797dea324b
Tongliang Liao <xkszltl@gmail.com>
2021-07-15 00:31:56 +0800

    Reduce load on github pulling of snappy.

--- pkgs/snappy.sh
+++ pkgs/snappy.sh
@@ -8,7 +8,7 @@
     # ------------------------------------------------------------
 
     . "$ROOT_DIR/pkgs/utils/git/version.sh" google/snappy,
-    until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
+    until git clone -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd snappy
 
     # Patch build error in 1.1.9.
