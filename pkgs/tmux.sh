3f1d54d51e7cbcba54bd11d24a5925adb644595c
Tongliang Liao <xkszltl@gmail.com>
2021-07-15 04:36:13 +0800

    Increase retry of tmux plugin installation to 100. This is caused by submodule to github.

--- pkgs/tmux.sh
+++ pkgs/tmux.sh
@@ -61,13 +61,13 @@ set -g mouse on
 run -b '~/.tmux/plugins/tpm/tpm'
 EOF
 
-    for attempt in $(seq 10 -1 0); do
+    for attempt in $(seq 100 -1 0); do
         [ "$attempt" -gt 0 ]
         TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins/ ~/.tmux/plugins/tpm/bin/install_plugins && break
         echo "Retrying... $(expr "$attempt" - 1) chance(s) left."
         sleep 3
     done
-    for attempt in $(seq 10 -1 0); do
+    for attempt in $(seq 100 -1 0); do
         [ "$attempt" -gt 0 ]
         TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins/ ~/.tmux/plugins/tpm/bin/update_plugins all && break
         echo "Retrying... $(expr "$attempt" - 1) chance(s) left."
