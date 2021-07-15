e529f3898c0166bccc92fc6a9ef8a73430d94342
Tongliang Liao <xkszltl@gmail.com>
2021-07-16 02:27:03 +0800

    Mirror libs from martinmoene.

--- mirror-list.sh
+++ mirror-list.sh
@@ -96,6 +96,7 @@ LMDB/lmdb,\
 lutzroeder/netron,\
 lz4/lz4{,-java},\
 madler/zlib,\
+martinmoene/{{active,any,atomic,bit,boolean,byte,expected,invoke,jthread,nonstd,optional{,-fun},ring-span,scope,span,status-value,string{,-view},type,{observer,value}-ptr,variant}-lite,clue,hamlest,kalman-estimator{,-ada},lest,nonstd-lite-project,optional-bare,PhysUnits-{CT{,-Cpp11},RT}},\
 matplotlib/{jupyter-matplotlib,matplotlib,mplcairo,pytest-mpl},\
 Maratyszcza/{confu,cpuinfo,FP16,FXdiv,NNPACK,PeachPy,psimd,pthreadpool},\
 micheles/{decorator,plac},\
