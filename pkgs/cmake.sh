dc77e693febbabf58d59baebf5c0b33be5321231
Tongliang Liao <xkszltl@gmail.com>
2021-09-29 07:35:57 +0800

    Experimental support for wider range of distros.

--- pkgs/cmake.sh
+++ pkgs/cmake.sh
@@ -16,15 +16,27 @@
     . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
 
     (
-        case "$DISTRO_ID" in
-        'centos' | 'fedora' | 'rhel')
+        case "$DISTRO_ID-$DISTRO_VERSION_ID" in
+        centos-* | fedora-* | rhel-*)
             set +xe
             . scl_source enable devtoolset-9 || exit 1
             set -xe
             export CC="gcc" CXX="g++"
             ;;
-        'ubuntu')
+        debian-10)
             export CC="gcc-8" CXX="g++-8"
+            ;;
+        debian-11)
+            export CC="gcc-10" CXX="g++-10"
+            ;;
+        ubuntu-18.* | ubuntu-19.*)
+            export CC="gcc-8" CXX="g++-8"
+            ;;
+        ubuntu-20.* | ubuntu-21.*)
+            export CC="gcc-10" CXX="g++-10"
+            ;;
+        *)
+            export CC="gcc" CXX="g++"
             ;;
         esac
 
