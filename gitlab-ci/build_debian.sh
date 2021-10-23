f8475f86ed76d3cac0a72e51e4c0abd6ba3d759f
Tongliang Liao <xkszltl@gmail.com>
2021-10-24 03:29:21 +0800

    Create Debian 11 package list based on Ubuntu 18.04 version.
    Changes and justifications:
    
    - Debian 11 only has `gcc`/`g++`/`gfortran` 9 and 10.
    - `glances` is not included in Debian 11 yet.
        - https://tracker.debian.org/pkg/glances
        - https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=990340
    - `language-pack-*` is Ubuntu-only.
    - `libfreetype6-dev` has been replaced with `libfreetype-dev` in Debian 11.
    - Debian uses `libjpeg62-turbo-dev` instead of `libjpeg-turbo8-dev`.
    - Only has LLVM 9/11 but not 10. Choose 11 for conflicting packages and install the default (11) meta-packages.
    - `prelink` is not maintained by upstream for long and is removed in Debian 11, without a separated `execstack` replacement.
        - https://tracker.debian.org/pkg/prelink
        - https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=921401
    - `qt5-default` has been removed due to the removal of Qt4 in Debian 11. `qtbase5-dev` should be the correct replacement based on the dependency graph.
    - `pxz` package has gone since Debian 10 and Ubuntu 18.10.
    
    Known issues:
    
    - Old Intel packages do not support Debian11, but Intel is rebranding them to oneAPI toolkits and would take a while to update/verify.
    - CUDA repo does not have 11.4 for Debian 11. Will need to switch to 11.5 first.
    - cuDNN/TensorRT/NCCL do not have CUDA 11.5 build yet.

---
+++ gitlab-ci/build_debian.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -xe
+
+if [ ! "$CI_REGISTRY_IMAGE" ]; then
+    echo "Please set environment variable CI_REGISTRY_IMAGE to a docker registry."
+    exit 1
+fi
+
+if [ ! "$LOG_FILE" ]; then
+    mkdir -p log
+    export LOG_FILE="$(readlink -f "$(date -u '+log/%Y-%m-%d_%H-%M-%S.log')")"
+    "$(which time)" -v "$0" 2>&1 | tee "$LOG_FILE"
+    exit 0
+fi
+
+export BASE_DISTRO=debian
+
+[ "$CI_COMMIT_REF_NAME" ] || export CI_COMMIT_REF_NAME=build-init
+
+for CI_JOB_STAGE in init repo font pkg auth infra llvm util misc dl ort edit finish; do
+    export CI_JOB_STAGE
+
+    [ "$FIRST_STAGE" ] || [ "_$(sed 's/^[^\-]*\-//' <<< "$CI_COMMIT_REF_NAME")" = "_$CI_JOB_STAGE" ] || continue
+    [ "$FIRST_STAGE" ] || FIRST_STAGE="$CI_JOB_STAGE"
+
+    case "$CI_JOB_STAGE" in
+    finish)
+        [ "$PREV_CI_JOB_STAGE" ]
+        sudo docker tag "$CI_REGISTRY_IMAGE/$BASE_DISTRO:"{"stage-$PREV_CI_JOB_STAGE",latest}
+        sudo docker push "$CI_REGISTRY_IMAGE/$BASE_DISTRO:latest"
+        sudo docker rmi "$CI_REGISTRY_IMAGE/$BASE_DISTRO:breakpoint" 2>/dev/null || true
+        ;;
+    *)
+        gitlab-ci/build_stage.sh
+        CI_JOB_NAME="push-$CI_JOB_STAGE" gitlab-ci/push_stage.sh &
+        ;;
+    esac
+
+    PREV_CI_JOB_STAGE="$CI_JOB_STAGE"
+done
+wait
+
+echo "Image \"$CI_REGISTRY_IMAGE/$BASE_DISTRO\" is ready."
