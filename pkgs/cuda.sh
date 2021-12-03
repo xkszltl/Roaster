cda3fe2ddd6481a5a45cf634119b6092f5300dba
Tongliang Liao <xkszltl@gmail.com>
2021-12-03 21:59:31 +0800

    Update NCCL mapping.

--- pkgs/cuda.sh
+++ pkgs/cuda.sh
@@ -113,7 +113,7 @@
             'centos' | 'fedora' | 'rhel')
                 $RPM_INSTALL                                                                                            \
                     libcudnn8{,-devel}"-*-*cuda$CUDA_VER_MAJOR.$CUDA_VER_MINOR"                                         \
-                    libnccl{,-devel,-static}"-*-*cuda$(sed 's/11\.[12]/11\.0/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR" | sed 's/11\.[6-9]/11\.4/')"  \
+                    libnccl{,-devel,-static}"-*-*cuda$(sed 's/11\.[1-3]/11\.0/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR" | sed 's/11\.[6-9]/11\.4/')"  \
                     libnv{infer{,-plugin},{,onnx}parsers}{8,-devel}"-8.*-*cuda$(sed 's/11\.[12]/11\.0/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR" | sed 's/11\.[5-9]/11\.4/')"
                 : || $RPM_INSTALL                                                                                       \
                     libnv{infer{,-plugin},{,onnx}parsers}{7,-devel}"-7.*-*cuda$(sed 's/11\.[2-4]/11\.1/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR")"
@@ -121,7 +121,7 @@
             'debian' | 'linuxmint' | 'ubuntu')
                 sudo DEBIAN_FRONTEND=noninteractive apt-get install --allow-downgrades -y                               \
                     libcudnn8{,-dev}"=*+cuda$CUDA_VER_MAJOR.$CUDA_VER_MINOR"                                            \
-                    libnccl{2,-dev}"=*+cuda$(sed 's/11\.[12]/11\.0/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR" | sed 's/11\.[6-9]/11\.5/')"            \
+                    libnccl{2,-dev}"=*+cuda$(sed 's/11\.[1-3]/11\.0/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR" | sed 's/11\.[6-9]/11\.5/')"            \
                     libnv{infer{,-plugin},{,onnx}parsers}{8,-dev}"=8.*+cuda$(sed 's/11\.[12]/11\.0/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR" | sed 's/11\.[5-9]/11\.4/')"
                 : || sudo DEBIAN_FRONTEND=noninteractive apt-get install --allow-downgrades -y                          \
                     libnv{infer{,-plugin},{,onnx}parsers}{7,-dev}"=7.*+cuda$(sed 's/11\.[2-4]/11\.1/' <<< "$CUDA_VER_MAJOR.$CUDA_VER_MINOR")"
