df17299dfbfc2c9fc026a69dd8468722a8df2366
Tongliang Liao <xkszltl@gmail.com>
2021-12-05 23:51:37 +0800

    Remove some fixed regressions in CentOS 7 repo. - `python2-azure-sdk` and `python2-boto3` has gone, with only their `python-` counterparts left in repo. - `libasan2` is removed from repo.

--- pkgs/env/pkg.sh
+++ pkgs/env/pkg.sh
@@ -21,23 +21,17 @@ esac
 # TODO: Fix the following issue:
 #   - LLVM may select the wrong gcc toolchain without libgcc_s integrated.
 #     The correct choice is x86_64-redhat-linux instead of x86_64-linux-gnu.
-#   - libasan2 seems to contain libasan.so.3.0.0 and causing a conflict with libasan3
 #   - python-lexicon conflict with python2-dns-lexicon
 #     file /usr/lib/python2.7/site-packages/test/__init__.pyc conflicts between attempted installs of python2-ansible-runner-1.0.1-1.el7.noarch and python2-neomodel-3.2.8-1.el7.noarch
 #   - Python issues from CentOS 7.5:
-#     * python2-azure-sdk conflict with python-azure-sdk
-#     * python2-boto3 conflict with python-boto3
 #     * python2-s3transfer conflict with python-s3transfer
 #   - rh-ruby26-rubygem-bundler-doc has version mismatch between 1.16/1.17.
 
 export RPM_BLACKLIST=$(echo "
     *-debuginfo
     git-cola
-    libasan2
     python-lexicon
     python2-ansible-runner
-    python2-azure-sdk
-    python2-boto3
     python2-s3transfer
     rh-ruby26-rubygem-bundler-doc
 " | sed -n 's/^[[:space:]]*\([^[:space:]][^[:space:]]*\).*/--exclude \1/p' | paste -s - | xargs)
