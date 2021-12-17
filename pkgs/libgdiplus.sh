9d1e33525d43c6d7b40f5176a45b69013260585e
Tongliang Liao <xkszltl@gmail.com>
2021-12-18 01:05:42 +0800

    gtest was added to libgdiplus since Oct 2019 but only released recently (Nov 2021) in 6.1

--- pkgs/libgdiplus.sh
+++ pkgs/libgdiplus.sh
@@ -11,6 +11,8 @@
     until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd libgdiplus
 
+    . "$ROOT_DIR/pkgs/utils/git/submodule.sh"
+
     # ------------------------------------------------------------
 
     . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
