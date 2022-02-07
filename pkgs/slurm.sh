3a247b3453af68ed8f4a33c0a1bbd33642f33ab4
Tongliang Liao <xkszltl@gmail.com>
2022-02-08 00:30:45 +0800

    Quote

--- pkgs/slurm.sh
+++ pkgs/slurm.sh
@@ -8,34 +8,34 @@
     until git clone --depth 1 --single-branch -b "$(git ls-remote --tags "$GIT_MIRROR/SchedMD/slurm.git" | sed -n 's/.*[[:space:]]refs\/tags\/\(slurm-[0-9\.]*\)[[:space:]]*$/\1/p' | sort -V | tail -n1)" "$GIT_MIRROR/SchedMD/slurm.git"; do echo 'Retrying'; done
     git clone --depth 1 --no-checkout --no-single-branch $GIT_MIRROR/SchedMD/slurm.git
     cd slurm
-    git checkout $(git tag | sed -n '/^slurm-[0-9\-]*$/p' | sort -V | tail -n1)
+    git checkout "$(git tag | sed -n '/^slurm-[0-9\-]*$/p' | sort -V | tail -n1)"
 
     . scl_source enable devtoolset-9 || true || exit 1
 
     for i in $(sed -n 's/^[[:space:]]*\(.*:\).* META .*/\1/p' slurm.spec); do
-        sed -i "s/^\([[:space:]]*$i[[:space:]]*\).* META .*/\1"$(sed -n "s/[[:space:]]*$i[[:space:]]*\(.*\)/\1/p" META | head -n1)"/" slurm.spec
+        sed -i "s/^\([[:space:]]*$i[[:space:]]*\).* META .*/\1"$(sed -n "s/[[:space:]]*$i[[:space:]]*\(.*\)/\1/p" META | head -n1)"/" 'slurm.spec'
     done
 
-    export SLURM_NAME=$(for i in Name Version Release; do
+    export SLURM_NAME="$(for i in Name Version Release; do
         sed -n "s/^[[:space:]]*$i:[[:space:]]*\(.*\)/\1/p" META | head -n1
-    done | xargs | sed 's/[[:space:]][[:space:]]*/-/g')
+    done | xargs | sed 's/[[:space:]][[:space:]]*/-/g')"
 
-    export SLURM_EXT=$(sed -n "s/^[[:space:]]*Source:[^\.]*\(.*\)/\1/p" slurm.spec | head -n1)
+    export SLURM_EXT="$(sed -n "s/^[[:space:]]*Source:[^\.]*\(.*\)/\1/p" 'slurm.spec' | head -n1)"
 
-    export SLURM_TAR=$SLURM_NAME$SLURM_EXT
+    export SLURM_TAR="$SLURM_NAME$SLURM_EXT"
 
     cd ..
-    mkdir -p $SLURM_NAME
-    cp -rf slurm/* $_/
-    tar -acvf $SLURM_TAR $SLURM_NAME
-    rm -rf $SCRATCH/$SLURM_NAME
+    mkdir -p "$SLURM_NAME"
+    cp -rf 'slurm/'* "$_/"
+    tar -acvf "$SLURM_TAR" "$SLURM_NAME"
+    rm -rf "$SCRATCH/$SLURM_NAME"
 
-    rpmbuild -ta $SLURM_TAR --with lua --with multiple_slurmd --with mysql --with openssl
+    rpmbuild -ta "$SLURM_TAR" --with lua --with multiple_slurmd --with mysql --with openssl
 
-    rm -rf $SCRATCH/slurm*
+    rm -rf "$SCRATCH/slurm"*
 
-    dnf install $HOME/rpmbuild/RPMS/$(uname -i)/slurm{,-*}.rpm
-    rm -rf $HOME/rpmbuild
+    "$RPM_INSTALL" "$HOME/rpmbuild/RPMS/$(uname -i)/slurm"{,-*}'.rpm'
+    rm -rf "$HOME/rpmbuild"
 )
 sudo rm -vf $STAGE/slurm
 sync || true
