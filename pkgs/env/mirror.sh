ae339ad9f965cf2afd9cd2b53d2a39b1daec851c
Tongliang Liao <xkszltl@gmail.com>
2022-02-07 23:59:41 +0800

    Show IP addr for git mirrors.

--- pkgs/env/mirror.sh
+++ pkgs/env/mirror.sh
@@ -8,17 +8,19 @@ export GIT_MIRROR_CODINGCAFE="500,https://git.codingcafe.org/Mirrors"
 # ----------------------------------------------------------------
 
 case "$DISTRO_ID" in
-'centos' | 'rhel')
-    which bc     > /dev/null 2> /dev/null || sudo yum install -y bc
-    which column > /dev/null 2> /dev/null || sudo yum install -y util-linux
-    which paste  > /dev/null 2> /dev/null || sudo yum install -y coreutils
-    which ping   > /dev/null 2> /dev/null || sudo yum install -y iputils
-    which sed    > /dev/null 2> /dev/null || sudo yum install -y sed
-    which xargs  > /dev/null 2> /dev/null || sudo yum install -y findutils
+'centos' | 'fedora' | 'rhel')
+    which bc     > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y bc
+    which column > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y util-linux
+    which dig    > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y bind-utils
+    which paste  > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y coreutils
+    which ping   > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y iputils
+    which sed    > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y sed
+    which xargs  > /dev/null 2> /dev/null || sudo "$(which dnf >/dev/null 2>&1 && echo 'dnf' || echo 'yum')" install -y findutils
     ;;
 'debian' | 'linuxmint' | "ubuntu")
     which bc     > /dev/null 2> /dev/null || sudo apt-get install -y bc
     which column > /dev/null 2> /dev/null || sudo apt-get install -y bsdmainutils
+    which dig    > /dev/null 2> /dev/null || sudo apt-get install -y dnsutils
     which paste  > /dev/null 2> /dev/null || sudo apt-get install -y coreutils
     which ping   > /dev/null 2> /dev/null || sudo apt-get install -y iputils-ping
     which sed    > /dev/null 2> /dev/null || sudo apt-get install -y sed
@@ -34,7 +36,8 @@ export LINK_QUALITY="$(
     for i in $(env | sed -n 's/^GIT_MIRROR_[^=]*=//p'); do :
         price="$(cut -d',' -f1 <<< "$i,")"
         url="$(cut -d',' -f2 <<< "$i,")"
-        sudo ping -W 1 -n $PING_ROUND $(sed 's/.*:\/\///' <<<"$i" | sed 's/\/.*//')                 \
+        fqdn="$(sed 's/.*:\/\///' <<<"$url" | sed 's/\/.*//')"
+        sudo ping -W 1 -n $PING_ROUND "$fqdn"                                                       \
         | sed -n '/ms$/p'                                                                           \
         | sed 's/.*[^0-9]\([0-9]*\)%.*[^0-9\.]\([0-9\.]*\).*ms/\1 \2/'                              \
         | sed 's/.*ewma.*\/\([0-9\.]*\).*/\1/'                                                      \
@@ -42,13 +45,13 @@ export LINK_QUALITY="$(
         | sed "s/\([0-9\.][0-9\.]*\).*[[:space:]]\([0-9\.][0-9\.]*\).*/\2\*\(\1\*10+1\)\*$price/"   \
         | sed 's/^$/10\^20/'                                                                        \
         | bc
-        echo "$url"
+        printf '%s %s\n' "$url" "$(dig '+noall' '+short' -t A "$fqdn" | grep '[^\.]$' | paste -sd' ' -)"
     done | paste - - | sort -n | column -t
 )"
 
 sed 's/^/| /' <<< "$LINK_QUALITY"
 
-[ "$GIT_MIRROR" ] || GIT_MIRROR="$(head -n1 <<< "$LINK_QUALITY" | xargs -n1 | tail -n1)"
+[ "$GIT_MIRROR" ] || GIT_MIRROR="$(head -n1 <<< "$LINK_QUALITY" | xargs -n1 | tail -n2 | head -n1)"
 echo '----------------------------------------------------------------'
 echo "| GIT_MIRROR | $GIT_MIRROR"
 echo '----------------------------------------------------------------'
