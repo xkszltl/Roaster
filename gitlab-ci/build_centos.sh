2886d285e8a12f61e6b809e5bef1b2a1b5bf06a1
Roaster Project <xkszltl@gmail.com>
2022-04-08 16:53:00 +0800

    Split auth stage for CentOS.

--- gitlab-ci/build_centos.sh
+++ gitlab-ci/build_centos.sh
@@ -18,7 +18,7 @@ export BASE_DISTRO=centos
 
 [ "$CI_COMMIT_REF_NAME" ] || export CI_COMMIT_REF_NAME=build-init
 
-for CI_JOB_STAGE in init repo font pkg-{stable,skip,all} tex ss intel infra llvm util misc dl ort edit finish; do
+for CI_JOB_STAGE in init repo font pkg-{stable,skip,all} auth tex ss intel infra llvm util misc dl ort edit finish; do
     export CI_JOB_STAGE
 
     [ "$FIRST_STAGE" ] || [ "_$(sed 's/^[^\-]*\-//' <<< "$CI_COMMIT_REF_NAME")" = "_$CI_JOB_STAGE" ] || continue
