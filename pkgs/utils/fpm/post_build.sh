ce0237989425f0db5260e302ad529132d1508e19
Tongliang Liao <xkszltl@gmail.com>
2022-04-18 18:17:37 +0800

    List target triple dir in ldconfig.

--- pkgs/utils/fpm/post_build.sh
+++ pkgs/utils/fpm/post_build.sh
@@ -16,7 +16,19 @@ find "./$INSTALL_PREFIX" -name '*.pc' -type f | xargs -r sed -i "s/$(sed 's/\/*$
 export PKG_LD_CONF_DIR="/etc/ld.so.conf.d/roaster.conf.d"
 
 mkdir -p "./$PKG_LD_CONF_DIR"
-find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*' | sed 's/\.//' | sed 's/\/\/*/\//g' > "./$PKG_LD_CONF_DIR/$(basename $(cd ../.. && pwd)).conf"
+find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*'   \
+| sed 's/\/\/*/\//g'                                        \
+| sed 's/^\.\//\//'                                         \
+>> "./$PKG_LD_CONF_DIR/$(basename $(cd ../.. && pwd)).conf"
+
+# Add target triple.
+#   - Vendor can be anything, including "unknown".
+#   - GCC/LLVM uses "linux"/"linux-gnu" in target triple.
+find "./$INSTALL_PREFIX" -maxdepth 1 -type d -name 'lib*'                                                                       \
+| xargs -rI{} find {} -mindepth 1 -maxdepth 1 -type d '(' -name "$(uname -m)-*-linux" -o -name "$(uname -m)-*-linux-gnu" ')'    \
+| sed 's/\/\/*/\//g'                                                                                                            \
+| sed 's/^\.\//\//'                                                                                                             \
+>> "./$PKG_LD_CONF_DIR/$(basename $(cd ../.. && pwd)).conf"
 
 du -sh .{,{,/usr{,/local}}/*}
 
