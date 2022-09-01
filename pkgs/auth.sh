1dac1c05b84bf6999ebcb940a63f41923c76f31f
Roaster Project <xkszltl@gmail.com>
2022-09-01 19:20:51 +0800

    Setup sssd on Debian.

--- pkgs/auth.sh
+++ pkgs/auth.sh
@@ -58,6 +58,53 @@
                 sudo systemctl start $i || $IS_CONTAINER
             done
             ;;
+        'debian' | 'linuxmint' | 'ubuntu')
+            pushd '/etc/ldap'
+            for i in 'BASE' 'URI' 'TLS_CACERT' 'TLS_REQCERT'; do :
+                if [ "$(grep "^[[:space:]#]*$i[[:space:]]" 'ldap.conf' | wc -l)" -ne 1 ]; then
+                    sed "s/^[[:space:]#]*$i[[:space:]].*//" 'ldap.conf' > "$SCRATCH/.ldap.conf"
+                    sudo echo "# $i " >> "$SCRATCH/.ldap.conf"
+                    sudo mv -f {"$SCRATCH/.",}'ldap.conf'
+                fi
+            done
+            sudo cat 'ldap.conf'                                                                                        \
+            | sed 's/^[[:space:]#]*\(BASE[[:space:]][[:space:]]*\).*/\1dc=codingcafe,dc=org/'                           \
+            | sed 's/^[[:space:]#]*\(URI[[:space:]][[:space:]]*\).*/\1ldap:\/\/ldap.codingcafe.org/'                    \
+            | sed 's/^[[:space:]#]*\(TLS_CACERT[[:space:]][[:space:]]*\).*/\1\/etc\/pki\/tls\/certs\/ca-bundle.crt/'    \
+            | sed 's/^[[:space:]#]*\(TLS_REQCERT[[:space:]][[:space:]]*\).*/\1demand/'                                  \
+            > "$SCRATCH/.ldap.conf"
+            sudo mv -f {"$SCRATCH/.",}'ldap.conf'
+            popd
+
+            printf '\n# Automatically added by "%s".\n\nsession required pam_mkhomedir.so\n' "$0" | sudo tee -a '/etc/pam.d/common-session'
+
+            cat << ____________EOF | sed 's/^                //' | sudo tee '/etc/sssd/sssd.conf'
+                [sssd]
+
+                config_file_version = 2
+                domains = codingcafe.org
+                services = autofs, nss, pam, ssh, sudo
+
+                [domain/codingcafe.org]
+
+                auth_provider = ldap
+                cache_credentials = True
+                chpass_provider = ldap
+                enumerate = True
+                id_provider = ldap
+                ldap_id_use_start_tls = True
+                ldap_search_base = dc=codingcafe,dc=org
+                ldap_sudo_full_refresh_interval = 3600
+                ldap_sudo_search_base = ou=SUDOers,dc=codingcafe,dc=org
+                ldap_sudo_smart_refresh_interval = 600
+                ldap_uri = ldap://ldap.codingcafe.org
+                sudo_provider = ldap
+____________EOF
+            sudo chmod 600 '/etc/sssd/sssd.conf'
+
+            sudo systemctl daemon-reload || $IS_CONTAINER
+            sudo systemctl restart sssd  || $IS_CONTAINER
+            ;;
         *)
             echo "Unsupported distro \"DISTRO_ID\" for ldap configuration, skipped."
             ;;
