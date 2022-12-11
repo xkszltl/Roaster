ea405bdb00e89a454507a2e66e6168b87376d1d0
Roaster Project <xkszltl@gmail.com>
2022-12-12 03:40:44 +0800

    Mirror tree.

--- mirror-list.sh
+++ mirror-list.sh
@@ -221,6 +221,7 @@ zserge/jsmn,\
 git@gitlab.com:\ /\ {\
 libeigen/eigen,\
 NVIDIA/{cloud-native/{cnt-docs,go-nvlib,k8s-driver-manager,mig-parted},container-{images/{caffe,cuda{,-ppc64le,gl},digits,dockerhub-manifests,driver,l4t-base,mps,opencl,opengl,samples,video-codec-sdk,vulkan},infrastructure/{aws-kube-ci,github-app},toolkit/{container-{config,runtime,toolkit},gpu-monitoring-tools,libnvidia-container,nvidia-docker}},dlsyseng/{slurm,spank_fstab},headers/{cuda,cudnn},hpc/slurm-mig-discovery,kubernetes/{device-plugin,gpu-{feature-discovery,operator}},cuda},\
+OldManProgrammer/unix-tree,\
 pycqa/flake8,\
 },\
 git@gitlab.alpinelinux.org:\ /\ {\
