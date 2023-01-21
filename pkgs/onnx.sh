16e265c1065923a520f0def9c325a9510faecd7b
Tongliang Liao <xkszltl@gmail.com>
2023-01-22 03:13:04 +0800

    Use SCL python 3.8 for onnx 1.13.0. Remove the python->python3 aliasing patch since the shebang issue has been fixed, years after initial filing: - https://github.com/onnx/onnx/issues/1952

--- pkgs/onnx.sh
+++ pkgs/onnx.sh
@@ -82,56 +82,69 @@
         . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
         . "$ROOT_DIR/pkgs/utils/fpm/distro_cc.sh"
 
-        # --------------------------------------------------------
-        # Hack for protoc-gen-mypy bug:
-        #     https://github.com/onnx/onnx/issues/1952
-        # --------------------------------------------------------
-
-        if which python3; then
-            PY_DIR="$(readlink -f "$INSTALL_ROOT/../python")"
-            mkdir -p "$PY_DIR"
-            ln -sf "$(which python3)" "$PY_DIR/python"
-            export PATH="$PY_DIR:$PATH"
-        fi
-
-        CMAKE_ARGS="$CMAKE_ARGS
-            -DBUILD_SHARED_LIBS=ON
-            -DCMAKE_BUILD_TYPE=Release
-            -DCMAKE_C_COMPILER='$(which "$CC")'
-            -DCMAKE_C_COMPILER_LAUNCHER=ccache
-            -DCMAKE_CXX_COMPILER='$(which "$CXX")'
-            -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-            -DONNX_BUILD_TESTS=ON
-            -DONNX_GEN_PB_TYPE_STUBS=ON
-            -DONNX_USE_PROTOBUF_SHARED_LIBS=ON
-            -DONNXIFI_ENABLE_EXT=ON
-        " ONNX_ML=1 "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" .
-
         mkdir -p build
-        cd $_
+        pushd $_
 
-        "$TOOLCHAIN/cmake"                          \
-            -DBENCHMARK_ENABLE_LTO=ON               \
-            -DBUILD_ONNX_PYTHON=ON                  \
-            -DBUILD_SHARED_LIBS=ON                  \
-            -DCMAKE_BUILD_TYPE=Release              \
-            -DCMAKE_C_COMPILER="$CC"                \
-            -DCMAKE_CXX_COMPILER="$CXX"             \
-            -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
-            -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"   \
-            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
-            -DONNX_BUILD_BENCHMARKS=ON              \
-            -DONNX_BUILD_TESTS=ON                   \
-            -DONNX_GEN_PB_TYPE_STUBS=ON             \
-            -DONNX_ML=ON                            \
-            -DONNX_USE_PROTOBUF_SHARED_LIBS=ON      \
-            -DONNXIFI_ENABLE_EXT=ON                 \
-            -G"Ninja"                               \
-            ..
+        (
+            set -e
+            # Known issues:
+            # - ONNX 1.13.0 requires Protobuf 3.20.2 incompatible with Python 3.6.
+            case "$DISTRO_ID-$DISTRO_VERSION_ID" in
+            'centos-'* | 'fedora-'* | 'rhel-'* | 'scientific-'*)
+                set +xe
+                . scl_source enable rh-python38 || exit 1
+                set -xe
+                ;;
+            esac
 
-        time "$TOOLCHAIN/cmake" --build .
-        time ./onnx_gtests
-        time "$TOOLCHAIN/cmake" --build . --target install
+            "$TOOLCHAIN/cmake"                          \
+                -DBENCHMARK_ENABLE_LTO=ON               \
+                -DBUILD_ONNX_PYTHON=ON                  \
+                -DBUILD_SHARED_LIBS=ON                  \
+                -DCMAKE_BUILD_TYPE=Release              \
+                -DCMAKE_C_COMPILER="$CC"                \
+                -DCMAKE_CXX_COMPILER="$CXX"             \
+                -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
+                -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g"   \
+                -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
+                -DONNX_BUILD_BENCHMARKS=ON              \
+                -DONNX_BUILD_TESTS=ON                   \
+                -DONNX_GEN_PB_TYPE_STUBS=ON             \
+                -DONNX_ML=ON                            \
+                -DONNX_USE_PROTOBUF_SHARED_LIBS=ON      \
+                -DONNXIFI_ENABLE_EXT=ON                 \
+                -G"Ninja"                               \
+                ..
+
+            time "$TOOLCHAIN/cmake" --build .
+            time ./onnx_gtests
+            time "$TOOLCHAIN/cmake" --build . --target install
+        )
+
+        popd
+
+        (
+            set -e
+            export PY_VER='^3\.[7-9],^3\.[1-6][0-9]'
+            case "$DISTRO_ID-$DISTRO_VERSION_ID" in
+            'debian-'* | 'linuxmint-'* | 'ubuntu-'*)
+                # Already pinned to a Python3.6-compatible version.
+                ! python3 --version | cut -d' ' -f2 | grep '^3\.[0-6]\.' >/dev/null || export PY_VER=''
+                ;;
+            esac
+            CMAKE_ARGS="$CMAKE_ARGS
+                -DBUILD_SHARED_LIBS=ON
+                -DCMAKE_BUILD_TYPE=Release
+                -DCMAKE_C_COMPILER='$(which "$CC")'
+                -DCMAKE_C_COMPILER_LAUNCHER=ccache
+                -DCMAKE_CXX_COMPILER='$(which "$CXX")'
+                -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+                -DONNX_BUILD_TESTS=ON
+                -DONNX_GEN_PB_TYPE_STUBS=ON
+                -DONNX_USE_PROTOBUF_SHARED_LIBS=ON
+                -DONNXIFI_ENABLE_EXT=ON
+            " ONNX_ML=1 "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" .
+        )
     )
 
     "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
