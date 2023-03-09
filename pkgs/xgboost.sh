5238656adf48a54dba0fac5975c7a0b85fd0583f
Tongliang Liao <xkszltl@gmail.com>
2023-03-10 06:52:00 +0800

    Fix typo.

--- pkgs/xgboost.sh
+++ pkgs/xgboost.sh
@@ -9,7 +9,7 @@
 
     . "$ROOT_DIR/pkgs/utils/git/version.sh" dmlc/xgboost,v
     until git clone --depth 1 --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
-    cd gflags
+    cd xgboost
 
     # ------------------------------------------------------------
 
