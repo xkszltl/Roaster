043ca4e9ec3f053549a1afe5df88f479b876c006
Tongliang Liao <xkszltl@gmail.com>
2023-04-21 02:32:42 +0800

    Add missing cmd checks and make it POSIX.

--- geo/best-httping.sh
+++ geo/best-httping.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-for cmd in bc httping parallel; do
+for cmd in bc grep httping parallel sed xargs; do
     ! which "$cmd" >/dev/null || continue
     printf '\033[31m[ERROR] Missing command "%s".\033[0m\n' "$cmd" >&2
     exit 1
 done
 
 LINK_QUALITY="$(set -e
-    parallel $(sed -n 's/\(..*\)/\-\-halt now,success=\1/p' <<< "$TOPK") -j0 'bash -c '"'"'
+    parallel $(sed -n 's/\(..*\)/\-\-halt now,success=\1/p' <<< "$TOPK") -j0 'sh -c '"'"'
         set -e
         httping -Zfc10 -t3 {} 2>&1                                                                  \
         | sed -n "s/.*[^0-9]\([0-9][0-9]*\) *ok.*time  *\([0-9][0-9]*\) *ms.*/\1\/(\2\+1)\*10^3/p"  \
