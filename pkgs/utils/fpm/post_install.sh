6c7fa6da93e101fe933dd5a3f2fc9c479d44baf9
Tongliang Liao <xkszltl@gmail.com>
2024-06-14 23:25:36 -0700

    Detect buildkit.

--- pkgs/utils/fpm/post_install.sh
+++ pkgs/utils/fpm/post_install.sh
@@ -51,6 +51,13 @@ ldconfig
     | grep -q '/docker/containers/[0-9A-Fa-f][0-9A-Fa-f]*/resolv\.conf' \
     || exit 0
 
+    [ ! -e '/proc/1/mountinfo' ]                                        \
+    || ! cut -d' ' -f4 '/proc/1/mountinfo'                              \
+    | grep -q '/docker/buildkit/executor/hosts'                         \
+    || ! cut -d' ' -f4 '/proc/1/mountinfo'                              \
+    | grep -q '/docker/buildkit/executor/resolv\.conf'                  \
+    || exit 0
+
     # Leaked by docker/podman.
     # They can also exist on host, if leaked out from previous bind mount.
     # Use with caution.
