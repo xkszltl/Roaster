94918f53865a86805fb2bd371513457f28f50477
Tongliang Liao <xkszltl@gmail.com>
2024-06-14 17:11:43 -0700

    Update CC.

--- pkgs/utils/fpm/distro_cc.sh
+++ pkgs/utils/fpm/distro_cc.sh
@@ -15,10 +15,14 @@ case "$DISTRO_ID-$DISTRO_VERSION_ID" in
 'debian-11' | 'ubuntu-20.'* | 'ubuntu-21.'*)
     export CC="gcc-10" CXX="g++-10" FC="gfortran-10" AR='gcc-ar-10' RANLIB='gcc-ranlib-10'
     ;;
-'ubuntu-22.'* | 'ubuntu-23.'*)
-    # Ubuntu 22.04 has GCC 12 but CUDA 11.8 only supports up to GCC 11.
+'debian-12')
+    export CC="gcc-12" CXX="g++-12" FC="gfortran-12" AR='gcc-ar-12' RANLIB='gcc-ranlib-12'
+    ;;
+'ubuntu-22.'*)
     export CC="gcc-11" CXX="g++-11" FC="gfortran-11" AR='gcc-ar-11' RANLIB='gcc-ranlib-11'
     ;;
+'ubuntu-23.'* | 'ubuntu-24.'*)
+    export CC="gcc-13" CXX="g++-13" FC="gfortran-13" AR='gcc-ar-13' RANLIB='gcc-ranlib-13'
 *)
     export CC="gcc" CXX="g++" GC="gfortran" AR='gcc-ar' RANLIB='gcc-ranlib'
     ;;
