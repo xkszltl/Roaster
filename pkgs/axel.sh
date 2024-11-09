5d10676a71727ba3427fcc123a3cd9eca2f16406
Roaster Project <xkszltl@gmail.com>
2024-11-09 20:48:01 +0800

    Pin axel to 2.17.13 due to excessive autoconf requirement. - https://github.com/axel-download-accelerator/axel/issues/435 - https://github.com/axel-download-accelerator/axel/commit/1179ed90553a1a448a7d30d282895faf236abd7d

--- pkgs/axel.sh
+++ pkgs/axel.sh
@@ -7,7 +7,11 @@
 
     # ------------------------------------------------------------
 
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" axel-download-accelerator/axel,v
+    # Known issues:
+    # - Axel 2.17.14 requires autoconf 2.72 not yet available in Debian 12 as of Nov 2024.
+    #   https://github.com/axel-download-accelerator/axel/issues/435
+    #   https://github.com/axel-download-accelerator/axel/commit/1179ed90553a1a448a7d30d282895faf236abd7d
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" axel-download-accelerator/axel,v2.17.13
     until git clone -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd axel
 
