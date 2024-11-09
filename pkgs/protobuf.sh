dd9cd9281521fb701850583c442e089b3272b581
Roaster Project <xkszltl@gmail.com>
2024-11-10 01:12:35 +0800

    Upgrade to protobuf 4/5.

--- pkgs/protobuf.sh
+++ pkgs/protobuf.sh
@@ -11,7 +11,9 @@
 
     # Known issues:
     # - Protobuf 3.21 is re-versioned as 4.21 in Python with breaking changes.
-    . "$ROOT_DIR/pkgs/utils/git/version.sh" protocolbuffers/protobuf,v3.20.
+    # - Protobuf has tags without major version like v28.3 for v5.28.3.
+    #   Pin major version to 5 until we have more clarity on their versioning scheme.
+    . "$ROOT_DIR/pkgs/utils/git/version.sh" protocolbuffers/protobuf,v5.
     until git clone --single-branch -b "$GIT_TAG" "$GIT_REPO"; do echo 'Retrying'; done
     cd protobuf
 
@@ -29,54 +31,76 @@
     . "$ROOT_DIR/pkgs/utils/fpm/pre_build.sh"
 
     (
+        set -e
+
         . "$ROOT_DIR/pkgs/utils/fpm/toolchain.sh"
         . "$ROOT_DIR/pkgs/utils/fpm/distro_cc.sh"
 
-        if false; then
-            export CC="ccache $CC"
-            export CXX="ccache $CXX"
-            export C{,XX}FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -fPIC -O3 -g -DDPYTHON_PROTO2_CPP_IMPL_V2"
+        mkdir -p build
+        cd $_
 
-            ./autogen.sh
-            ./configure --prefix="$INSTALL_ABS"
-            make -j$(nproc)
-            make check -j$(nproc)
-            make install -j
-        else
-            mkdir -p build
-            cd $_
+        "$TOOLCHAIN/cmake"                          \
+            -DBUILD_SHARED_LIBS=ON                  \
+            -DCMAKE_BUILD_TYPE=Release              \
+            -DCMAKE_C_COMPILER="$CC"                \
+            -DCMAKE_CXX_COMPILER="$CXX"             \
+            -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
+            -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g -DPYTHON_PROTO2_CPP_IMPL_V2"   \
+            -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
+            -Dprotobuf_BUILD_CONFORMANCE=ON         \
+            -Dprotobuf_BUILD_EXAMPLES=ON            \
+            -Dprotobuf_BUILD_LIBPROTOC=ON           \
+            -Dprotobuf_BUILD_LIBUPB=ON              \
+            -Dprotobuf_INSTALL_EXAMPLES=ON          \
+            -Dprotobuf_USE_EXTERNAL_GTEST=ON        \
+            -G"Ninja"                               \
+            ..
 
-            "$TOOLCHAIN/cmake"                          \
-                -DBUILD_SHARED_LIBS=ON                  \
-                -DCMAKE_BUILD_TYPE=Release              \
-                -DCMAKE_C_COMPILER="$CC"                \
-                -DCMAKE_CXX_COMPILER="$CXX"             \
-                -DCMAKE_C{,XX}_COMPILER_LAUNCHER=ccache \
-                -DCMAKE_C{,XX}_FLAGS="-fdebug-prefix-map='$SCRATCH'='$INSTALL_PREFIX/src' -g -DPYTHON_PROTO2_CPP_IMPL_V2"   \
-                -DCMAKE_INSTALL_PREFIX="$INSTALL_ABS"   \
-                -Dprotobuf_BUILD_EXAMPLES=ON            \
-                -Dprotobuf_INSTALL_EXAMPLES=ON          \
-                -G"Ninja"                               \
-                ../cmake
-
-            time "$TOOLCHAIN/cmake" --build .
-            time "$TOOLCHAIN/cmake" --build . --target check
-            time "$TOOLCHAIN/cmake" --build . --target install
-        fi
+        time "$TOOLCHAIN/cmake" --build .
+        time "$TOOLCHAIN/ctest" --output-on-failure -j"$(nproc)"
+        time "$TOOLCHAIN/cmake" --build . --target install
     )
 
-    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
+    # Protobuf 3.22 introduces jsoncpp dependency.
+    # - https://github.com/protocolbuffers/protobuf/pull/10739
+    # - https://github.com/protocolbuffers/protobuf/commit/5308cf0aa67881ff0fc5518e02144e93bc342e83
+    # Exclude jsoncpp files.
+    pushd "$INSTALL_ROOT"
+    for i in jsoncpp; do
+        case "$DISTRO_ID" in
+        'centos' | 'fedora' | 'rhel' | 'scientific')
+            [ "$(rpm -qa "roaster-$i")" ] || continue
+            rpm -ql "roaster-$i" | sed -n 's/^\//\.\//p' | xargs rm -rf
+            ;;
+        'debian' | 'linuxmint' | 'ubuntu')
+            dpkg -l "roaster-$i" && dpkg -L "roaster-$i" | xargs -n1 | xargs -i -n1 find {} -maxdepth 0 -not -type d | sed -n 's/^\//\.\//p' | xargs rm -rf
+            ;;
+        esac
+    done
+    popd
 
-    # Fake makefile output for CMake build due to hard-coded path in "setup.py".
-    [ ! -d 'build' ] || ln -sf '../build' 'src/.libs'
+    "$ROOT_DIR/pkgs/utils/fpm/install_from_git.sh"
 
     # Known issues:
     # - Protobuf 3.20 drops Python 3.6 support.
     #   https://github.com/protocolbuffers/protobuf/pull/9480
     #   https://github.com/protocolbuffers/protobuf/commit/301d315dc4674d1bc799446644e88eff0af1ac86
+    # - Python Protobuf 4.25 drops Python 3.7 support.
     # - Protobuf 3.21 is re-versioned as 4.21 in Python with breaking changes.
-    PROTOC="$(realpath -e 'build/protoc')" "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" \
-        protocolbuffers/protobuf/./python,v3.20.[3.6=v3.19.]
+    # - Python Protobuf 5.26 removes setup.py and only supports bazel for wheel build.
+    #   https://github.com/protocolbuffers/protobuf/pull/15708
+    #   https://github.com/protocolbuffers/protobuf/commit/5722aeffcad72e9a335a3ec7985858dfa31477be
+    #   https://github.com/protocolbuffers/protobuf/pull/15671
+    #   https://github.com/protocolbuffers/protobuf/commit/8135fca851c76344392c7888e4eb647013b40479
+    if [ -e "python/setup.py" ]; then
+        # Fake makefile output for CMake build due to hard-coded path in "setup.py".
+        [ ! -d 'build' ] || ln -sf '../build' 'src/.libs'
+
+        PROTOC="$(realpath -e 'build/protoc')" "$ROOT_DIR/pkgs/utils/pip_install_from_git.sh" \
+            protocolbuffers/protobuf/./python,v5.[3.6=v3.19.,3.7=v4.24.]
+    else
+        "$ROOT_DIR/pkgs/utils/pip_install_from_wheel.sh" "protobuf==$GIT_TAG_VER"
+    fi
 
     # ------------------------------------------------------------
 
--- setup.sh
+++ setup.sh
@@ -139,7 +139,7 @@ pushd "$SCRATCH"
     sudo rm -rvf "$STAGE"
     sudo mkdir -p "$(dirname "$STAGE")/.$(basename "$STAGE")"
     cd $_
-    [ $# -gt 0 ] && sudo touch $@ || sudo touch repo font pkg-stable pkg-skip pkg-all fpm auth vim tmux tex ss trojan intel nasm lm-sensors lz4 zstd cmake hiredis ccache c-ares axel ipt python-3.{7,8,9,10,11} cuda gdrcopy ucx ompi llvm-{gcc,clang} boost jemalloc eigen openblas gtest benchmark gflags glog snappy protobuf nsync grpc catch2 jsoncpp rapidjson simdjson utf8proc pugixml pybind libpng x264 x265 mkl-dnn ispc halide xgboost sentencepiece opencv leveldb rocksdb lmdb nvcodec ffmpeg onnx pytorch torchvision apex ort
+    [ $# -gt 0 ] && sudo touch $@ || sudo touch repo font pkg-stable pkg-skip pkg-all fpm auth vim tmux tex ss trojan intel nasm lm-sensors lz4 zstd cmake hiredis ccache c-ares axel ipt python-3.{7,8,9,10,11} cuda gdrcopy ucx ompi llvm-{gcc,clang} boost jemalloc eigen openblas gtest benchmark gflags glog snappy jsoncpp rapidjson simdjson utf8proc pugixml protobuf nsync grpc catch2 pybind libpng x264 x265 mkl-dnn ispc halide xgboost sentencepiece opencv leveldb rocksdb lmdb nvcodec ffmpeg onnx pytorch torchvision apex ort
     sync || true
     cd "$SCRATCH"
     sudo mv -vf "$(dirname "$STAGE")/.$(basename "$STAGE")" $STAGE
@@ -197,14 +197,14 @@ for i in $(echo "
     gflags
     glog
     snappy
-    protobuf
-    nsync
-    catch2
     jsoncpp
     rapidjson
     simdjson
     utf8proc
     pugixml
+    protobuf
+    nsync
+    catch2
     pybind
     grpc
     libpng
