d22f16c992481a4a775834103d0cbc7c40c05b79
Tongliang Liao <xkszltl@gmail.com>
2024-11-16 18:20:42 -0800

    Monitoring plot.

---
+++ mon-plot.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+set -e
+
+for cmd in grep hostname journalctl python3 sed; do
+    ! which "$cmd" >/dev/null || continue
+    printf '\033[31m[ERROR] Missing command "$s".\033[0m\n' "$cmd" >&2
+    exit 1
+done
+
+for pkg in matplotlib numpy; do
+    ! python3 -m pip show "$pkg" || continue
+    printf '\033[31m[ERROR] Missing python lib "%s".\033[0m\n' "$pkg" >&2
+    exit 1
+done
+
+[ "$win" ] || win='1h'
+
+journalctl --no-pager --since "-$win" --until now -o short-unix -u{sensors,ipmi}-mon                \
+| sed 's/[[:space:]][[:space:]]*/ /g'                                                               \
+| grep '^[^ ][^ ]* [^ ][^ ]* sh\[\([1-9][0-9]*\)\]: '                                               \
+| sed -n 's/^\([0-9][0-9]*\)\.\([0-9][0-9][0-9][0-9][0-9][0-9]\) /\1 - '"$(date +'%s')"'; /p'       \
+| grep -i                                                                                           \
+    -e'; '{'power1','Sensor 2'}                                                                     \
+    -e'; '{'MB','System'}' Temp'                                                                    \
+    -e'; '{{'CPU','VRMCpu'}{,'2'},'Vcpu'{,'2'}'VRM'}' Temp'                                         \
+    -e'; '{'DDR4_E',{,'P2-'}'DIMM'{'E1','E~H'}}' Temp'                                              \
+    -e'; ''12V'                                                                                     \
+| sed 's/ *[|:] */; /'                                                                              \
+| cut -d'|' -f1                                                                                     \
+| tr -d '+'                                                                                         \
+| sed 's/°C.*//'                                                                                    \
+| sed 's/ W.*//'                                                                                    \
+| sed 's/[Nn]\/[Aa].*/nan/'                                                                         \
+| sed 's/^ *//'                                                                                     \
+| sed 's/ *$//'                                                                                     \
+| sed -n 's/^\([^;]*\); \([^;]*\); \([^;]*\)$/stat\.setdefault("\2", \[\]).append(\[\1, \3\])/p'    \
+| tr 'A-Z' 'a-z'                                                                                    \
+| sed 's/inf/0/g'                                                                                   \
+| sed 's/nan/0/g'                                                                                   \
+| sed 's/"power[0-9]"/"Power"/'                                                                     \
+| sed 's/"sensor 2"/"T_ssd"/'                                                                       \
+| sed 's/"cpu[0-9]* temp"/"T_cpu"/'                                                                 \
+| sed 's/"vrmcpu[0-9]* temp"/"T_vrm"/'                                                              \
+| sed 's/"vcpu[0-9]*vrm temp"/"T_vrm"/'                                                             \
+| sed 's/"system temp"/"T_sys"/'                                                                    \
+| sed 's/"mb temp"/"T_sys"/'                                                                        \
+| sed 's/"p[0-9][_\-]dimm[a-z][^ ]* temp"/"T_mem"/'                                                 \
+| sed 's/"dimm[a-z][^ ]* temp"/"T_mem"/'                                                            \
+| sed 's/"12v"/"12V"/'                                                                              \
+| paste -sd';' -                                                                                    \
+| sed 's/^/stat = {};/'                                                                             \
+| sed 's/$/\n'"$(set -e +x >&2
+        printf '%s\n' '
+                import matplotlib
+                import matplotlib.pyplot
+                import numpy as np
+
+                max_sample = 2000
+                dat = {
+                    k: np.flip(np.flip(np.array(v, dtype=np.float64))[::(len(v) + max_sample - 1) // max_sample]).transpose()
+                    for (k, v) in stat.items()
+                }
+                matplotlib.rcParams["lines.linewidth"] = min(1e3 / max(i.shape[1] for i in dat.values()), 1)
+                (fig, ax) = matplotlib.pyplot.subplots(figsize=(16, 9))
+                ax.set_title("'"$win System Monitoring ($(hostname))"'")
+                ax.set_xlabel("time [d]")
+                ax.set_ylabel("temperature [℃]")
+                y_v = ax.secondary_yaxis(1.0, functions=(lambda x: x / 5, lambda x: x * 5))
+                y_v.set_ylabel("voltage (V)")
+                y_w = ax.secondary_yaxis(1.05, functions=(lambda x: x * 10, lambda x: x / 10))
+                y_w.set_ylabel("wattage (W)")
+                for (i, k) in (
+                    ("Power", .1),
+                    ("T_ssd", 1.),
+                    ("T_cpu", 1.),
+                    ("T_sys", 1.),
+                    ("T_vrm", 1.),
+                    ("T_mem", 1.),
+                    ("12V",   5.),
+                ):
+                    if i in dat:
+                        ax.plot(dat[i][0] / 86400, dat[i][1] * k, label=i)
+                ax.legend(loc="upper left", bbox_to_anchor=(1.1, 1.0))
+                fig.savefig("'"mon-$win-$(hostname).pdf"'", bbox_inches="tight")
+            '                               \
+        | sed 's/^    //'                   \
+        | sed 's/^    //'                   \
+        | sed 's/^    //'                   \
+        | sed 's/^    //'                   \
+        | grep .                            \
+        | sed 's/\([\[\\\/\.\-]\)/\\\1/g'   \
+        | paste -sd'\v' -                   \
+        | sed 's/'"$(printf '\v')"'/\\n/g'
+    )"'/'                                                                                           \
+| python3
