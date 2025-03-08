3c894df5a91db012b55e738eeb8840fe6da5c7a2
Tongliang Liao <xkszltl@gmail.com>
2025-03-08 00:23:47 -0800

    Recent version of firewalld on Debian 12 throws naming conflict when trying to re-add built-in services.

--- pkgs/firewall.sh
+++ pkgs/firewall.sh
@@ -9,28 +9,35 @@
         sudo systemctl enable firewalld || $IS_CONTAINER
         sudo systemctl --no-pager status firewalld || sudo systemctl start firewalld || $IS_CONTAINER
     fi
+    if ! sudo firewall-cmd --state; then
+        if $IS_CONTAINER; then
+            printf '\033[36m[INFO] Skip firewalld setup in container.\033[0m\n' >&2
+            exit 0
+        else
+            printf '\033[31m[ERROR] Failed to start firewalld.\033[0m\n' >&2
+            exit 1
+        fi
+    fi
 
     sudo firewall-cmd --permanent --delete-service=afp || true
-    if sudo firewall-cmd --permanent --new-service=afp; then
+    if ! sudo firewall-cmd --permanent --get-services | xargs -n1 | grep -q '^afp$'; then
+        sudo firewall-cmd --permanent --new-service=afp
         sudo firewall-cmd --permanent --service=afp --set-short='Apple Filing Protocol'
         sudo firewall-cmd --permanent --service=afp --set-description='The Apple Filing Protocol (AFP), formerly AppleTalk Filing Protocol, is a proprietary network protocol, and part of the Apple File Service (AFS), that offers file services for macOS and the classic Mac OS.'
         sudo firewall-cmd --permanent --service=afp --add-port=548/{tcp,udp}
-    else
-        $IS_CONTAINER
+        sudo firewall-cmd --reload
     fi
-    sudo firewall-cmd --reload || $IS_CONTAINER
 
     sudo firewall-cmd --permanent --delete-service=rdp || true
-    if sudo firewall-cmd --permanent --new-service=rdp; then
+    if ! sudo firewall-cmd --permanent --get-services | xargs -n1 | grep -q '^rdp$'; then
+        sudo firewall-cmd --permanent --new-service=rdp
         sudo firewall-cmd --permanent --service=rdp --set-short='Remote Desktop Protocol'
         sudo firewall-cmd --permanent --service=rdp --set-description='RDP is based on, and is an extension of, the T-120 family of protocol standards.'
         sudo firewall-cmd --permanent --service=rdp --add-port=3389/tcp
-    else
-        $IS_CONTAINER
+        sudo firewall-cmd --reload
     fi
-    sudo firewall-cmd --reload || $IS_CONTAINER
 
-    sudo firewall-cmd --list-all || $IS_CONTAINER
+    sudo firewall-cmd --list-all
 )
 sudo rm -vf $STAGE/firewall
 sync "$STAGE" || true
