950e013cfca9033d9d3eb25ef77fe51402faf656
Tongliang Liao <xkszltl@gmail.com>
2025-12-21 01:23:19 -0800

    rsync with 100 chunks.

--- prsync.sh
+++ prsync.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-for cmd in parallel rsync; do
+for cmd in parallel rsync xargs; do
     ! command -v "$cmd" >/dev/null || continue
     printf '\033[31m[ERROR] Missing command "%s".\033[0m\n' "$cmd" >&2
     exit 1
 done
 
-seq 0 9                                         \
-| parallel --lb -j10 -q sh -c 'set -ex
+seq 0 99                                        \
+| xargs printf '%02d\n'                         \
+| parallel --lb -j20 -q sh -c 'set -ex
     for i in $(seq 5); do
         ! rsync --exclude "**/*[^{}].*" "$@"    \
         || break
