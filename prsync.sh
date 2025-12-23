e8eed58b595795710a6e59a839a874d656365e4b
Tongliang Liao <xkszltl@gmail.com>
2025-12-23 02:22:26 -0800

    File-based chunking in prsync.

--- prsync.sh
+++ prsync.sh
@@ -8,14 +8,22 @@ for cmd in parallel rsync xargs; do
     exit 1
 done
 
-seq 0 99                                        \
-| xargs printf '%02d\n'                         \
+seq 100                                                                                 \
 | parallel --lb -j20 -q sh -c 'set -ex
+    tid={}
     for i in $(seq 5); do
-        ! rsync --exclude "**/*[^{}].*" "$@"    \
+        ! find "$'$(expr "$#" - 1 || true)'/././././" -not -type d                      \
+        | sed "s/.*\\/\\.\\/\\.\\/\\.\\/\\.\\//**\\//"                                  \
+        | paste -d "$(printf "\v")" '"$(seq 100 | sed 's/..*/\-/' | paste -sd' ' -)"'   \
+        | cut -d "$(printf "\v")" -f "$tid"                                             \
+        | rsync --include-from - --include "**/" --exclude "*" "$@"                     \
         || break
+        sleep "0.$tid"
     done
 ' -- "$@"
 
 rsync "$@"
+
+printf '\033[36m[INFO] Verify remote content.\033[0m\n' >&2
 rsync -c "$@"
+printf '\033[32m[INFO] Rsync completed.\033[0m\n' >&2
